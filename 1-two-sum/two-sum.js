77acfd8b1ecaddd7f2a9f901a1f02a883e841a8e
Xavier <xavierh93@hotmail.com>
2024-04-15 11:32:16 -0400

    Time: 60 ms (68.19%) | Memory: 51.7 MB (19.72%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+        if (map.has(complement)) {
+            return [i, map.get(complement)]
+        }
+        map.set(nums[i], i)
+    }
+    return null
+};
