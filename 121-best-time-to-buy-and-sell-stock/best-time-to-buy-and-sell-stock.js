556d9d4fab4c00329cb95721076c462a35d3bc22
Xavier <xavierh93@hotmail.com>
2024-04-15 11:44:44 -0400

    Time: 79 ms (34.84%) | Memory: 58.9 MB (63.06%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (prices === null || prices.length <= 1) return 0
+
+    let profit = 0
+    let minBuy = prices[0]
+
+    for (let i = 0; i < prices.length; i++) {
+        minBuy = Math.min(minBuy, prices[i])
+        profit = Math.max(profit, prices[i] - minBuy)
+    }
+    return profit
+};
