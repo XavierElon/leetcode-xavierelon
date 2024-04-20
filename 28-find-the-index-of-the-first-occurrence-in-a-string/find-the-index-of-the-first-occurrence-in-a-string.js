ca9a3c587c4ddf4a38996c922115249cd757ab9c
Xavier <xavierh93@hotmail.com>
2024-04-20 11:05:28 -0400

    Time: 49 ms (74.77%) | Memory: 49.9 MB (9.40%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (needle === '' || needle === haystack) return 0
+    if (needle == '' || haystack == '') return 0
     if (haystack.length < needle.length) return -1
 
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        if (haystack[i] === needle[0]) {
-            for (let j = 0; j < needle.length; j++) {
-                if (needle[j] !== haystack[i + j]) {
-                    break
-                } else if (j === needle.length - 1) {
-                    return i
-                }
+    for (let windowStart = 0; windowStart < haystack.length - needle.length + 1; windowStart++) {
+        if (haystack[windowStart] === needle[0])
+        for (let j = 0; j < needle.length ; j++) {
+            if (needle[j] !== haystack[windowStart + j]) {
+                break
+            } else if (j === needle.length - 1) {
+                return windowStart
             }
         }
     }
