0a8aff535718abe2960de8f5bcad0a60e84f6eea
Xavier <xavierh93@hotmail.com>
2024-04-20 11:22:51 -0400

    Time: 162 ms (56.87%) | Memory: 64.9 MB (72.86%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -4,26 +4,24 @@
  */
 var threeSum = function(nums) {
     nums.sort((a, b) => a - b)
-    const res = []
+    const result = []
 
     for (let i = 0; i < nums.length; i++) {
-        let low = i + 1, high = nums.length - 1, sum = 0
+        let low = i + 1, high = nums.length -1, sum = 0;
 
         while (low < high) {
             sum = nums[i] + nums[low] + nums[high]
+
             if (sum === 0) {
-                res.push([nums[i], nums[low], nums[high]])
+                result.push([nums[i], nums[low], nums[high]])
                 while(nums[low+1] === nums[low]) low++
                 while(nums[high-1] === nums[high]) high--
                 low++
                 high--
-            } else if (sum < 0) {
-                low++
-            } else {
-                high--
-            }
-            while (nums[i+1] === nums[i]) i++
+            } else if (sum < 0) low++
+            else high--
         }
+        while(nums[i+1] === nums[i]) i++
     }
-    return res
+    return result
 };
