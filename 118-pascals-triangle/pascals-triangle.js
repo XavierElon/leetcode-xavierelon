b41f4113fc522fdc39b3a124fda29167d1922617
Xavier <xavierh93@hotmail.com>
2024-04-20 09:17:28 -0400

    Time: 60 ms (43.97%) | Memory: 49.9 MB (64.04%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -5,13 +5,13 @@
 var generate = function(numRows) {
     const triangle = []
 
-    for (let rowNumber = 0; rowNumber < numRows; rowNumber++) {
-        let row = new Array(rowNumber + 1)
+    for (let rowNum = 0; rowNum < numRows; rowNum++) {
+        const row = new Array(rowNum + 1)
         row[0] = 1
-        row[row.length - 1] = 1
+        row[rowNum] = 1
 
-        for (let j = 1; j < row.length-1; j++) {
-            row[j] = triangle[rowNumber - 1][j-1] + triangle[rowNumber - 1][j]
+        for (let j = 1; j < row.length -1; j++) {
+            row[j] = triangle[rowNum - 1][j - 1] + triangle[rowNum - 1][j]
         }
 
         triangle.push(row)
