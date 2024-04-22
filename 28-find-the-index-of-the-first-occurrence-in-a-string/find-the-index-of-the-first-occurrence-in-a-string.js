8b2ffcc7822542c52213f3029072f5fdb36c864e
Xavier <xavierh93@hotmail.com>
2024-04-22 08:59:36 -0400

    Time: 48 ms (79.27%) | Memory: 49.3 MB (18.17%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -8,14 +8,16 @@ var strStr = function(haystack, needle) {
     if (haystack.length < needle.length) return -1
 
     for (let windowStart = 0; windowStart < haystack.length - needle.length + 1; windowStart++) {
-        if (haystack[windowStart] === needle[0])
-        for (let j = 0; j < needle.length ; j++) {
-            if (needle[j] !== haystack[windowStart + j]) {
-                break
-            } else if (j === needle.length - 1) {
-                return windowStart
+        if (haystack[windowStart] === needle[0]) {
+            for (let j = 0; j < needle.length; j++) {
+                if (needle[j] !== haystack[windowStart + j]) {
+                    break
+                } else if (j === needle.length - 1) {
+                    return windowStart
+                }
             }
         }
     }
-    return -1
-};
+    return -1;
+
+}
