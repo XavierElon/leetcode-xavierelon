d1f06ed5145443986190de82a0e110f55ebbb65c
Xavier <xavierh93@hotmail.com>
2024-04-23 10:41:42 -0400

    Time: 61 ms (15.56%) | Memory: 48.7 MB (67.43%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -3,13 +3,12 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    let res = new Array(rowIndex + 1).fill(1)
+    let res = new Array(rowIndex+1).fill(1)
 
     for (let i = 0; i < rowIndex; i++) {
         for (let j = i; j > 0; j--) {
             res[j] = res[j] + res[j-1]
         }
     }
-
     return res
 };
