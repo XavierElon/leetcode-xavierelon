3f75e34d91dad03d2463cb5ae66eedc503023f84
Xavier <xavierh93@hotmail.com>
2024-04-24 09:50:38 -0400

    Time: 76 ms (45.24%) | Memory: 57 MB (31.68%) - LeetSync

---
+++ 207-course-schedule/course-schedule.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const indegree = new Array(numCourses).fill(0)
+    const adjList = new Array(numCourses).fill(null).map(() => [])
+
+    for (const [course, prereq] of prerequisites) {
+        indegree[course]++
+        adjList[prereq].push(course)
+    }
+    console.log(adjList)
+
+    const queue = []
+    for (let i = 0; i < numCourses; i++) {
+        if (indegree[i] === 0) {
+            queue.push(i)
+        }
+    }
+
+    let count = 0
+    while (queue.length > 0) {
+        const course = queue.shift()
+        count++
+        for (const nextCourse of adjList[course]) {
+            indegree[nextCourse]--
+            if (indegree[nextCourse] === 0) {
+                queue.push(nextCourse)
+            }
+        }
+    }
+    return count === numCourses
+};
