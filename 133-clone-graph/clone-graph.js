4156a14d28bc7c9ebe65bfa67878b912657f580a
Xavier <xavierh93@hotmail.com>
2024-04-25 09:42:58 -0400

    Time: 67 ms (20.21%) | Memory: 51.4 MB (67.91%) - LeetSync

--- 133-clone-graph/clone-graph.js
+++ 133-clone-graph/clone-graph.js
@@ -11,25 +11,16 @@
  * @return {Node}
  */
 var cloneGraph = function(node) {
-    if (!node) return
+    if (node === null) return
     const map = new Map()
 
-    const clonedNode = new Node(node.val, [])
-    map.set(node, clonedNode)
-
-    const queue = [node]
-
-    while (queue.length > 0) {
-        const current = queue.shift()
-
-        for (const neighbor of current.neighbors) {
-            if (!map.has(neighbor)) {
-                map.set(neighbor, new Node(neighbor.val, []))
-                queue.push(neighbor)
-            }
-                    map.get(current).neighbors.push(map.get(neighbor))
-
+    const clone = root => {
+        if (!map.has(root.val)) {
+            map.set(root.val, new Node(root.val))
+            map.get(root.val).neighbors = root.neighbors.map(clone)
         }
+        return map.get(root.val)
     }
-    return clonedNode
+
+    return clone(node)
 };
