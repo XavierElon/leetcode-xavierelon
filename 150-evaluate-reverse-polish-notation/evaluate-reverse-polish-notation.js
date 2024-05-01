0564e69eab4182641db91359f6704748c64999f8
Xavier <xavierh93@hotmail.com>
2024-05-01 11:31:38 -0400

    Time: 68 ms (35.44%) | Memory: 51.3 MB (91.12%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const ops = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => ~~(a / b)
+  };
+
+  const stack = [];
+
+  for (let n of tokens) {
+    if (ops[n] != null) {
+      const b = stack.pop();
+      const a = stack.pop();
+      stack.push(ops[n](a, b));
+    } else {
+      stack.push(Number(n));
+    }
+  }
+
+  return stack[0];
+};
