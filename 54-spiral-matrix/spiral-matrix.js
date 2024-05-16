d78082d134e57eb989748316ab115e4c11643922
Xavier <xavierh93@hotmail.com>
2024-05-16 14:30:37 -0400

    Time: 60 ms (11.43%) | Memory: 48.6 MB (61.66%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    const result = []
+    if (matrix.length === 0) return result
+
+    let rowBegin = 0
+    let rowEnd = matrix.length - 1
+    let colBegin = 0
+    let colEnd = matrix[0].length - 1
+
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        for (let i = colBegin; i <= colEnd; i++) {
+            result.push(matrix[rowBegin][i])
+        }
+        rowBegin++
+        
+        for (let i = rowBegin; i <= rowEnd; i++) {
+            result.push(matrix[i][colEnd])
+        }
+        colEnd--
+
+        if (rowBegin <= rowEnd) {
+            for (let i = colEnd; i >= colBegin; i--) {
+                result.push(matrix[rowEnd][i])
+            }
+            rowEnd--
+        }
+        
+
+        if (colBegin <= colEnd) {
+            for (let i = rowEnd; i >= rowBegin; i--) {
+                result.push(matrix[i][colBegin])
+            }
+            colBegin++
+        }
+        
+    }
+    return result
+};
