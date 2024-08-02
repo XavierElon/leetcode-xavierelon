8fa0eb512e0f75e141ae82111019f8339caab0d1
Xavier <xavierh93@hotmail.com>
2024-08-02 08:30:28 -0400

    Time: 70 ms (56.27%) | Memory: 57.3 MB (19.92%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const freqMap = new Map()
+    for (const num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1)
+    }
+
+    const buckets = Array(nums.length + 1).fill().map(() => [])
+    for (const [num, freq] of freqMap) {
+        buckets[freq].push(num)
+    }
+
+    const result = []
+    for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+        result.push(...buckets[i])
+    }
+
+    return result.slice(0, k)
+};
