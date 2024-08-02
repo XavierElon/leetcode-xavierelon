2cb8c61471feabb7938f5e9a31bb3bf2bca90a68
Xavier <xavierh93@hotmail.com>
2024-08-02 10:11:38 -0400

    Time: 66 ms (66.20%) | Memory: 53.8 MB (29.43%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.js
@@ -0,0 +1,28 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    const rows = Array(9).fill().map(() => new Set())
+    const cols = Array(9).fill().map(() => new Set())
+    const boxes = Array(9).fill().map(() => new Set())
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const num = board[i][j]
+            if (num === '.') continue
+
+            const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3)
+
+            if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) {
+                return false
+            }
+
+            rows[i].add(num)
+            cols[j].add(num)
+            boxes[boxIndex].add(num)
+        }
+    }
+
+    return true
+};
