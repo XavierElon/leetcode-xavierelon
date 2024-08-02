1fcbb4d208669de8d3fce0145c8484ec92e8a563
Xavier <xavierh93@hotmail.com>
2024-08-02 08:42:24 -0400

    Time: 86 ms (17.38%) | Memory: 57.3 MB (19.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -18,6 +18,7 @@ var topKFrequent = function(nums, k) {
     for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
         result.push(...buckets[i])
     }
+    console.log(result)
 
-    return result.slice(0, k)
+    return result
 };
