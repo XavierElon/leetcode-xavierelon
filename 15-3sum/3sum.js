ffe9f0b1c9ab00141e8027d3a9e13c594ad9e73a
Xavier <xavierh93@hotmail.com>
2024-08-03 15:56:26 -0400

    Time: 147 ms (84.20%) | Memory: 64.2 MB (92.26%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,24 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b)
-    const res = []
+    nums.sort((a, b) => a - b);
+    const result = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        let low = i + 1, high = nums.length - 1, sum = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) continue;
 
-        while (low < high) {
-            sum = nums[i] + nums[low] + nums[high]
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
             if (sum === 0) {
-                res.push([nums[i], nums[low], nums[high]])
-                while (nums[low] === nums[low+1]) low++
-                while(nums[high] === nums[high-1]) high--
-                low++
-                high--
-            } else if (sum < 0) low++
-            else high--
+                result.push([nums[i], nums[left], nums[right]]);
+
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                
+                left++;
+                right--;
+            }
+            else if (sum < 0) {
+                left++;
+            } 
+            else {
+                right--;
+            }
         }
-        while (nums[i+1] === nums[i]) i++
     }
-    return res
+    return result;
 };
