86ee4daebb8168df0b0a2d954f584ad09a328f0e
Xavier <xavierh93@hotmail.com>
2024-08-03 13:34:56 -0400

    Time: 1225 ms (20.98%) | Memory: 72.3 MB (72.37%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums)
+    console.log(numSet)
+
+    let maxSequence = 0
+
+    for (const num of nums) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num
+            let currentSequence = 1
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum++
+                currentSequence++
+            }
+
+            maxSequence = Math.max(currentSequence, maxSequence)
+        }
+    }
+    return maxSequence
+};
