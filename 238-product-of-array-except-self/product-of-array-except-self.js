580d2c0ca911c74901dff0e7ca792dee105d7b7a
Xavier <xavierh93@hotmail.com>
2024-08-03 13:18:11 -0400

    Time: 101 ms (66.75%) | Memory: 61.4 MB (91.32%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const length = nums.length
-    const answer = new Array(length).fill(1)
+    const result = new Array(nums.length).fill(1)
 
     let leftProduct = 1
-    for (let i = 0; i < length; i++) {
-        answer[i] = leftProduct
+    let rightProduct = 1
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] *= leftProduct
         leftProduct *= nums[i]
     }
 
-    let rightProduct = 1
-    for (let i = length - 1; i >= 0; i--) {
-        answer[i] *= rightProduct
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= rightProduct
         rightProduct *= nums[i]
     }
 
-    return answer
+    return result
 };
