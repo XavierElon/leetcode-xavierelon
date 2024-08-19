709cbca4503421fc408713a78acd12bfcbe1ce1c
Xavier <xavierh93@hotmail.com>
2024-08-19 15:20:58 -0400

    Time: 208 ms (68.95%) | Memory: 72.5 MB (80.69%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -7,12 +7,11 @@ var dailyTemperatures = function(temperatures) {
     const stack = []
 
     for (let i = 0; i < temperatures.length; i++) {
-        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length-1]]) {
             let prevIndex = stack.pop()
             res[prevIndex] = i - prevIndex
         }
         stack.push(i)
     }
-
     return res
 };
