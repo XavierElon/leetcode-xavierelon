499873e147f1ad6e400bfa31d664020a4eab0c3a
Xavier <xavierh93@hotmail.com>
2024-08-21 11:45:50 -0400

    Time: 75 ms (25.81%) | Memory: 52.9 MB (62.58%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.ts
@@ -0,0 +1,21 @@
+function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+    const stack: number[] = [];
+    const nextGreater: Map<number, number> = new Map();
+    
+    for (let i = nums2.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && stack[stack.length - 1] <= nums2[i]) {
+            stack.pop()
+        }
+
+        if (stack.length > 0) {
+            nextGreater.set(nums2[i], stack[stack.length - 1])
+        } else {
+            nextGreater.set(nums2[i], -1)
+        }
+
+        stack.push(nums2[i])
+    }
+    console.log(nextGreater)
+
+    return nums1.map(num => nextGreater.get(num))
+}
