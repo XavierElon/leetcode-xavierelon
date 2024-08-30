dcacc8d45bd2b986df1a3b7ac802dc50da35f830
Xavier <xavierh93@hotmail.com>
2024-08-30 12:26:32 -0400

    Time: 46 ms (94.03%) | Memory: 50.9 MB (61.19%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.ts
@@ -0,0 +1,13 @@
+    function sortSentence(s: string): string {
+        const words = s.split(' ')
+
+        const sortedWords: string[] = new Array(words.length)
+
+        for (const word of words) {
+            const position = parseInt(word[word.length - 1])
+
+            sortedWords[position - 1] = word.slice(0, -1)
+        }
+
+        return sortedWords.join(' ')
+    };
