1afc69f7b73423c5961180054cb084f5a8a893d1
Xavier <xavierh93@hotmail.com>
2024-08-30 15:01:22 -0400

    Time: 55 ms (60.81%) | Memory: 49.9 MB (80.88%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.ts
@@ -0,0 +1,20 @@
+function summaryRanges(nums: number[]): string[] {
+    const result: string[] = []
+    let i: number = 0
+
+    while (i < nums.length) {
+        let start: number = i
+
+        while (i + 1 < nums.length && nums[i+1] === nums[i] + 1) {
+            i++
+        }
+
+        if (start === i) {
+            result.push(`${nums[start]}`)
+        } else {
+            result.push(`${nums[start]}->${nums[i]}`)
+        }
+        i++
+    }
+    return result
+};
