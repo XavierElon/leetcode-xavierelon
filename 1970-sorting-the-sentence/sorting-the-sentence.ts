0ef074752052361d44f06a0b7aa90c7335bcb867
Xavier <xavierh93@hotmail.com>
2024-08-30 12:33:39 -0400

    Time: 59 ms (34.33%) | Memory: 50.4 MB (61.19%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.ts
+++ 1970-sorting-the-sentence/sorting-the-sentence.ts
@@ -1,13 +1,13 @@
-    function sortSentence(s: string): string {
-        const words = s.split(' ')
+function sortSentence(s: string): string {
+    const words: string[] = s.split(' ')
 
-        const sortedWords: string[] = new Array(words.length)
+    const sortedWords: string[] = new Array(words.length)
 
-        for (const word of words) {
-            const position = parseInt(word[word.length - 1])
+    for (const word of words) {
+        const position = parseInt(word[word.length-1])
 
-            sortedWords[position - 1] = word.slice(0, -1)
-        }
+        sortedWords[position-1] = word.slice(0, -1)
+    }
 
-        return sortedWords.join(' ')
-    };
+    return sortedWords.join(' ')
+};
