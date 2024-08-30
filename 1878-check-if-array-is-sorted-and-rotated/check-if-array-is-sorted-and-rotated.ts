b087bbba8a9cd93bd37e9844929587a616192323
Xavier <xavierh93@hotmail.com>
2024-08-30 10:41:50 -0400

    Time: 53 ms (74.70%) | Memory: 51.3 MB (53.01%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.ts
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.ts
@@ -1,9 +1,9 @@
 function check(nums: number[]): boolean {
     let dropCount: number = 0
     const length: number = nums.length
-    
+
     for (let i = 0; i < length; i++) {
-        if (nums[i] > nums[(i + 1) % length]) dropCount++
+        if (nums[i] > nums[(i+1) % length]) dropCount++
 
         if (dropCount > 1) return false
     }
