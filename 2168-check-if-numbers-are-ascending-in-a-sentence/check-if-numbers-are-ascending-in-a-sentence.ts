238fa01c5f8ffd56b841963a82f826c809c25caa
Xavier <xavierh93@hotmail.com>
2024-08-30 11:29:22 -0400

    Time: 66 ms (13.95%) | Memory: 51.8 MB (6.98%) - LeetSync

--- 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.ts
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.ts
@@ -1,5 +1,5 @@
 function areNumbersAscending(s: string): boolean {
-    let tokens: string[] = s.split(' ')
+    const tokens: string[] = s.split(' ')
     let previousNumber: number = -1
 
     for (const token of tokens) {
