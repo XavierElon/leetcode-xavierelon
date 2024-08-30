8ba7a45f8aac3f537124824479363be61290d389
Xavier <xavierh93@hotmail.com>
2024-08-30 14:46:02 -0400

    Time: 64 ms (28.09%) | Memory: 51.9 MB (33.71%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.ts
@@ -0,0 +1,17 @@
+function findMin(nums: number[]): number {
+    let left: number = 0
+    let right: number = nums.length - 1
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1
+        } else if (nums[mid] < nums[right]) {
+            right = mid
+        } else {
+            right--
+        }
+    }
+    return nums[left]
+};
