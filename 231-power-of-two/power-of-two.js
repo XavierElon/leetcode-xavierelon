36da66d021f9d83d30bf7d10c04638dec4ccdf20
Xavier <xavierh93@hotmail.com>
2024-09-02 15:52:53 -0400

    Time: 79 ms (40.66%) | Memory: 53.1 MB (59.20%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,8 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n === 0) return false
-    while (n % 2 === 0) n /= 2
-
-    return n === 1
+    return n > 0 ? !(n & n-1) : false
 };
