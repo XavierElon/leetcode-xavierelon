82d9651da81190c8c2aea0e89c23b186896ac11a
Xavier <xavierh93@hotmail.com>
2024-09-03 10:28:10 -0400

    Time: 224 ms (82.03%) | Memory: 71.7 MB (68.75%) - LeetSync

---
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function getTargetCopy(original: TreeNode | null, cloned: TreeNode | null, target: TreeNode | null): TreeNode | null {
+    if (original === null || cloned === null || target === null) return null
+
+    if (cloned.val === target.val) return cloned
+
+    return getTargetCopy(original.left, cloned.left, target) || getTargetCopy(original.right, cloned.right, target)
+};
