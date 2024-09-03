34398a5b661d4da5437cbe2fbda53c9ac666461f
Xavier <xavierh93@hotmail.com>
2024-09-03 11:23:37 -0400

    Time: 87 ms (35.71%) | Memory: 55.9 MB (60.71%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     children: _Node[]
+ *     
+ *     constructor(v: number) {
+ *         this.val = v;
+ *         this.children = [];
+ *     }
+ * }
+ */
+
+
+function levelOrder(root: _Node | null): number[][] {
+	if (!root) return []
+
+    const result: number[][] = []
+    const queue: _Node[] = [root]
+
+    while (queue.length > 0) {
+        const currentSize = queue.length
+        const level: number[] = []
+
+        for (let i = 0; i < currentSize; i++) {
+            const currentNode: _Node = queue.shift()
+            level.push(currentNode.val)
+
+            for (const child of currentNode.children) {
+                queue.push(child)
+            }
+        }
+        result.push(level)
+    }
+    return result
+};
