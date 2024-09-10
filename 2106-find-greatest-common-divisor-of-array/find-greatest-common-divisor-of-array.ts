fee9e5657a8464bd4c5dd63e9dc5eebefd30cbe1
Xavier <xavierh93@hotmail.com>
2024-09-10 11:59:56 -0400

    Time: 58 ms (61.43%) | Memory: 51.1 MB (98.57%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.ts
@@ -0,0 +1,9 @@
+function gcd(a: number, b: number): number {
+    return b === 0 ? a : gcd(b, a % b)
+}
+
+function findGCD(nums: number[]): number {
+    const min = Math.min(...nums)
+    const max = Math.max(...nums)
+    return gcd(min, max)
+};
