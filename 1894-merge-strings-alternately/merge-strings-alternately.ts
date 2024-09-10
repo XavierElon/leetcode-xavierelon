80901053561950d919505c238272cfe305525c06
Xavier <xavierh93@hotmail.com>
2024-09-10 11:36:27 -0400

    Time: 49 ms (93.80%) | Memory: 52.3 MB (37.55%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/merge-strings-alternately.ts
@@ -1,20 +1,16 @@
 function mergeAlternately(word1: string, word2: string): string {
-    const merged: string[] = []
+    let merged: string[] = []
+    let len1 = word1.length
+    let len2 = word2.length
 
-    const length1: number = word1.length
-    const length2: number = word2.length
-    // const maxLength: number = length1 > length2 ? length1 : length2
-
-    for (let i = 0; i < Math.max(length1, length2); i++) {
-        if (i < length1) {
-                    merged.push(word1[i])
-
+    for (let i = 0; i < Math.max(len1, len2); i++) {
+        if (i < len1) {
+            merged.push(word1[i])
         }
 
-        if (i < length2) {
+        if (i < len2) {
             merged.push(word2[i])
         }
     }
-
     return merged.join('')
 };
