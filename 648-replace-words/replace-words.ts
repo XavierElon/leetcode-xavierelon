fb1e55d741aae52a4836053064c48e95ddc3c364
Xavier <xavierh93@hotmail.com>
2024-09-11 09:16:24 -0400

    Time: 412 ms (42.00%) | Memory: 58.3 MB (98.00%) - LeetSync

--- 648-replace-words/replace-words.ts
+++ 648-replace-words/replace-words.ts
@@ -1,13 +1,12 @@
 function replaceWords(dictionary: string[], sentence: string): string {
     const newSentence = sentence.split(' ')
 
-    for (let j = 0; j < dictionary.length; j++) {
-        for (let i = 0; i < newSentence.length; i++) {
+    for (let i = 0; i < newSentence.length; i++) {
+        for (let j = 0; j < dictionary.length; j++) {
             if (newSentence[i].startsWith(dictionary[j])) {
                 newSentence[i] = dictionary[j]
             }
         }
     }
-
     return newSentence.join(' ')
 };
