1a6a11f527763e48d88b4ccda458ddfce7fb2f06
Xavier <xavierh93@hotmail.com>
2024-09-11 07:09:26 -0400

    Time: 51 ms (79.50%) | Memory: 50.1 MB (89.44%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.ts
+++ 2556-convert-the-temperature/convert-the-temperature.ts
@@ -1,5 +1,5 @@
 function convertTemperature(celsius: number): number[] {
-    const kelvin = celsius + 273.15;
-    const fahrenheit = celsius * 1.80 + 32.00;
-    return [kelvin, fahrenheit];
+  const kelvin: number = celsius + 273.15
+  const fahrenheit: number = celsius * 1.80 + 32.00
+  return [kelvin, fahrenheit]  
 };
