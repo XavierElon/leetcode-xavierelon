b143fe91e8474e59f0935d3365c3da6d4c96088f
Xavier <xavierh93@hotmail.com>
2024-09-12 16:34:43 -0400

    Time: 58 ms (80.48%) | Memory: 52 MB (72.00%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.ts
@@ -0,0 +1,18 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    let count: number = 0
+    const length: number = flowerbed.length
+
+    for (let i = 0; i < length; i++) {
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i-1] === 0) &&
+            (i === length-1 || flowerbed[i+1] === 0)) {
+                flowerbed[i] = 1;
+                count++
+                
+                if (count >= n) {
+                    return true
+                }
+            }
+    }
+    return count >= n
+
+};
