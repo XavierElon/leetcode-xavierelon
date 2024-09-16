80743b373bb4873c6096c06e08097c2f9e5b41d8
Xavier <xavierh93@hotmail.com>
2024-09-16 16:39:14 -0400

    Time: 64 ms (30.06%) | Memory: 52.1 MB (45.04%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/merge-strings-alternately.ts
@@ -1,13 +1,12 @@
 function mergeAlternately(word1: string, word2: string): string {
     let merged: string[] = []
-    let len1 = word1.length
-    let len2 = word2.length
+    let len1: number = word1.length
+    let len2: number = word2.length
 
     for (let i = 0; i < Math.max(len1, len2); i++) {
         if (i < len1) {
             merged.push(word1[i])
         }
-
         if (i < len2) {
             merged.push(word2[i])
         }
