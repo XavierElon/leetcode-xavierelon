052603d800923fba25faa6e740e7d21d4136adb2
Xavier <xavierh93@hotmail.com>
2024-09-19 16:08:01 -0400

    Time: 68 ms (31.66%) | Memory: 51.2 MB (99.84%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.ts
+++ 605-can-place-flowers/can-place-flowers.ts
@@ -3,15 +3,14 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
     const length: number = flowerbed.length
 
     for (let i = 0; i < length; i++) {
-        if (flowerbed[i] === 0 && ((i === 0 || flowerbed[i-1] === 0) && 
-        (i === length-1 || flowerbed[i+1] === 0))) {
+        if (flowerbed[i] === 0 && ((i === 0 || flowerbed[i-1] === 0) && (i === length - 1 || flowerbed[i+1] === 0))) {
             flowerbed[i] = 1
             count++
 
             if (count >= n) {
                 return true
             }
-        }
+         }
     }
     return count >= n
 };
