186519022d6c2faac095138e0a248913af5d98d9
Xavier <xavierh93@hotmail.com>
2024-09-24 16:41:06 -0400

    Time: 72 ms (5.91%) | Memory: 51.4 MB (30.74%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.ts
@@ -0,0 +1,11 @@
+function isSubsequence(s: string, t: string): boolean {
+    let i: number = 0, j = 0
+
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
+            i++
+        }
+        j++
+    }
+    return i === s.length
+};
