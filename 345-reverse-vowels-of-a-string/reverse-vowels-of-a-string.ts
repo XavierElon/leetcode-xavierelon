d938d9432eab035a7ab2cbde087b20f2218ebb83
Xavier <xavierh93@hotmail.com>
2024-09-24 11:45:07 -0400

    Time: 65 ms (83.81%) | Memory: 55.5 MB (90.48%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.ts
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.ts
@@ -1,17 +1,18 @@
 function reverseVowels(s: string): string {
-    const vowels: Set<string> = new Set(['a', 'e', 'i', 'o','u', 'A', 'E', 'I', 'O', 'U'])
+    const vowels: Set<string> = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
     let arr: string[] = s.split('')
     let left: number = 0
-    let right: number = s.length-1
+    let right: number = s.length - 1
 
     while (left < right) {
-        if (!vowels.has(arr[left])) left++
+        if (!vowels.has(arr[left])) left++;
         else if (!vowels.has(arr[right])) right--
         else {
             [arr[left], arr[right]] = [arr[right], arr[left]]
-            left++
             right--
+            left++
         }
     }
     return arr.join('')
+
 };
