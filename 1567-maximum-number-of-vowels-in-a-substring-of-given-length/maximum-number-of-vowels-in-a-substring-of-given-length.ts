7a503c31204eeb92defab13107902ce509ea2500
Xavier <xavierh93@hotmail.com>
2024-09-25 15:16:07 -0400

    Time: 91 ms (19.48%) | Memory: 54.3 MB (32.93%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -0,0 +1,25 @@
+function maxVowels(s: string, k: number): number {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    let maxVowelCount: number = 0
+    let currentVowelCount: number = 0
+
+    for (let i = 0; i < k; i++) {
+        if (vowels.has(s[i])) {
+            currentVowelCount++
+        }
+    }
+
+    maxVowelCount = currentVowelCount
+
+    for (let i = k; i < s.length; i++) {
+        if (vowels.has(s[i])) {
+            currentVowelCount++
+        }
+        if (vowels.has(s[i - k])) {
+            currentVowelCount--
+        }
+        maxVowelCount = Math.max(maxVowelCount, currentVowelCount)
+    }
+
+    return maxVowelCount
+};
