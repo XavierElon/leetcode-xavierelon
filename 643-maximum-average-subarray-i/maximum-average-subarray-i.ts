5ce329f6b199910170438199a43820551622f107
Xavier <xavierh93@hotmail.com>
2024-09-25 10:06:23 -0400

    Time: 89 ms (44.22%) | Memory: 64 MB (55.17%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.ts
@@ -0,0 +1,15 @@
+function findMaxAverage(nums: number[], k: number): number {
+    let currentSum: number = 0
+    for (let i = 0; i < k; i++) {
+        currentSum += nums[i]
+    }
+
+    let maxSum: number = currentSum;
+
+    for (let i = k; i < nums.length; i++) {
+        currentSum += nums[i] - nums[i - k]
+        maxSum = Math.max(maxSum, currentSum)
+    }
+
+    return maxSum/k
+};
