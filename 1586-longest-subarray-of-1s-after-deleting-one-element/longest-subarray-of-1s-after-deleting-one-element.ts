9becf9b482f96aac2ed6e84e74f6356fe70e1cdd
Xavier <xavierh93@hotmail.com>
2024-09-25 16:55:22 -0400

    Time: 72 ms (41.63%) | Memory: 57.6 MB (49.26%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.ts
@@ -0,0 +1,20 @@
+function longestSubarray(nums: number[]): number {
+    let left: number = 0
+    let zeroCount: number = 0
+    let maxLength: number = 0
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            zeroCount++
+        }
+
+        while (zeroCount > 1) {
+            if (nums[left] === 0) {
+                zeroCount--
+            }
+            left++
+        }
+        maxLength = Math.max(maxLength, right - left)
+    }
+    return maxLength
+};
