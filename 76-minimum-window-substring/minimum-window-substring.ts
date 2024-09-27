4166eeff65960a5f2bc02d11db1fd28beff045af
Xavier <xavierh93@hotmail.com>
2024-09-27 08:02:47 -0400

    Time: 107 ms (33.45%) | Memory: 55.1 MB (60.82%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.ts
+++ 76-minimum-window-substring/minimum-window-substring.ts
@@ -1,39 +1,37 @@
 function minWindow(s: string, t: string): string {
     if (t.length > s.length) return ""
 
-    const tFreq: Record<string, number> = {}
-    const windowFreq: Record<string, number> = {}
+    const tFreq: Map<string, number> = new Map()
+    const windowFreq: Map<string, number> = new Map()
 
     for (const char of t) {
-        tFreq[char] = (tFreq[char] || 0) + 1
+        tFreq.set(char, (tFreq.get(char) || 0) + 1)
     }
 
-    let left: number = 0, right = 0
-    let required = Object.keys(tFreq).length
-    console.log(required)
-    let formed: number = 0
-    let minLen: number = Infinity
+    let left: number = 0, right = 0, formed = 0
+    let required: number = tFreq.size
+    let minLength: number = Infinity
     let result: string = ""
 
     const isWindowValid = () => formed === required
 
     while (right < s.length) {
         const charRight = s[right]
-        windowFreq[charRight] = (windowFreq[charRight] || 0) + 1
+        windowFreq.set(charRight, (windowFreq.get(charRight) || 0) + 1)
 
-        if (tFreq[charRight] && windowFreq[charRight] === tFreq[charRight]) {
+        if (tFreq.has(charRight) && windowFreq.get(charRight) === tFreq.get(charRight)) {
             formed++
         }
 
         while (isWindowValid() && left <= right) {
-            if (right - left + 1 < minLen) {
-                minLen = right - left + 1
+            if (right - left + 1 < minLength) {
+                minLength = right - left + 1
                 result = s.substring(left, right + 1)
             }
 
             const charLeft: string = s[left]
-            windowFreq[charLeft]--
-            if (tFreq[charLeft] && windowFreq[charLeft] < tFreq[charLeft]) {
+            windowFreq.set(charLeft, windowFreq.get(charLeft) - 1)
+            if (tFreq.has(charLeft) && windowFreq.get(charLeft) < tFreq.get(charLeft)) {
                 formed--
             }
             left++
