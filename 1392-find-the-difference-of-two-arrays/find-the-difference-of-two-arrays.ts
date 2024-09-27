6bb14bda86611d03c370561a3394d326a0236353
Xavier <xavierh93@hotmail.com>
2024-09-27 13:52:13 -0400

    Time: 97 ms (33.00%) | Memory: 57.8 MB (16.58%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
@@ -2,20 +2,20 @@ function findDifference(nums1: number[], nums2: number[]): number[][] {
     const set1 = new Set(nums1)
     const set2 = new Set(nums2)
 
-    const onlyInNums1: number[] = []
-    const onlyInNums2: number[] = []
+    const isOnlyInSet1: number[] = []
+    const isOnlyInSet2: number[] = []
 
     for (const num of set1) {
         if (!set2.has(num)) {
-            onlyInNums1.push(num)
+            isOnlyInSet1.push(num)
         }
     }
 
     for (const num of set2) {
         if (!set1.has(num)) {
-            onlyInNums2.push(num)
+            isOnlyInSet2.push(num)
         }
     }
 
-    return [onlyInNums1, onlyInNums2]
- };
+    return [isOnlyInSet1, isOnlyInSet2]
+};
