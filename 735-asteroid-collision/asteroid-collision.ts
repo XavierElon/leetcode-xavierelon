370c0f5362986ada46d1388b4d0536bf30e51812
Xavier <xavierh93@hotmail.com>
2024-09-29 16:12:04 -0400

    Time: 64 ms (74.17%) | Memory: 52.5 MB (73.22%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.ts
@@ -0,0 +1,27 @@
+function asteroidCollision(asteroids: number[]): number[] {
+    const stack: number[] = []
+
+    for (const asteroid of asteroids) {
+        let collision = false
+
+        while (asteroid < 0 && stack.length > 0 && stack[stack.length - 1] > 0) {
+            const top = stack[stack.length - 1]
+            if (top < -asteroid) {
+                stack.pop()
+                continue
+            } else if (top === -asteroid) {
+                stack.pop()
+                collision = true
+                break
+            } else {
+                collision = true
+                break
+            }
+        }
+        if (!collision) {
+            stack.push(asteroid)
+        }
+
+    }
+    return stack
+};
