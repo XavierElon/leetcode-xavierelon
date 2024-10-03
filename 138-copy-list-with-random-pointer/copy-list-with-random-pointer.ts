f2b0e3652decd97a2b55d7193430eadd48a32ee8
Xavier <xavierh93@hotmail.com>
2024-10-03 09:20:37 -0400

    Time: 68 ms (31.49%) | Memory: 52.2 MB (69.70%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     next: _Node | null
+ *     random: _Node | null
+ * 
+ *     constructor(val?: number, next?: _Node, random?: _Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *         this.random = (random===undefined ? null : random)
+ *     }
+ * }
+ */
+
+
+function copyRandomList(head: _Node | null): _Node | null {
+    if (!head) return null
+
+    let current: _Node | null = head
+
+    while (current !== null) {
+        const newNode: _Node = new _Node(current.val, current.next, null)
+        current.next = newNode
+        current = newNode.next
+    }
+
+    current = head
+    while (current !== null) {
+        if (current.random !== null) {
+            current.next.random = current.random.next
+        }
+        current = current.next.next
+    }
+
+    current = head
+    const copyHead: _Node | null = head.next
+    let copyCurrent: _Node | null = copyHead
+
+    while (current !== null) {
+        current.next = current.next.next
+        if (copyCurrent.next !== null) {
+            copyCurrent.next = copyCurrent.next.next
+        }
+        current = current.next
+        copyCurrent = copyCurrent.next
+    }
+    return copyHead
+}; 
