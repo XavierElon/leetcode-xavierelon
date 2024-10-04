e838a3657277b89b8a594ef78dca30d0f16cc2ac
Xavier <xavierh93@hotmail.com>
2024-10-04 17:47:26 -0400

    Time: 69 ms (74.32%) | Memory: 56.9 MB (66.77%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
@@ -17,5 +17,6 @@ function searchBST(root: TreeNode | null, val: number): TreeNode | null {
 
     if (root.val === val) return root
 
-    return (searchBST(root.left, val)) || (searchBST(root.right, val))
+    if (val < root.val) return searchBST(root.left, val)
+    else return searchBST(root.right, val)
 };
