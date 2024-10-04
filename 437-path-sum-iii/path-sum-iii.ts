f7d4a8eb9eff2ea9dbfc7380271aed96fa04895c
Xavier <xavierh93@hotmail.com>
2024-10-04 12:03:59 -0400

    Time: 53 ms (99.19%) | Memory: 55.1 MB (83.81%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root: TreeNode | null, targetSum: number): number {
+    const prefixSums: Map<number, number> = new Map()
+
+    prefixSums.set(0, 1)
+
+    let count: number = 0
+
+    function dfs(node: TreeNode | null, currentSum: number): void {
+        if (node === null) return
+
+        currentSum += node.val
+
+        const neededSum: number = currentSum - targetSum
+        if (prefixSums.has(neededSum)) {
+            count += prefixSums.get(neededSum)
+        }
+
+        prefixSums.set(currentSum, (prefixSums.get(currentSum) || 0) + 1)
+
+        if (node.left !== null) dfs(node.left, currentSum)
+        if (node.right !== null) dfs(node.right, currentSum)
+
+        prefixSums.set(currentSum, prefixSums.get(currentSum) - 1)
+     }
+
+    dfs(root, 0)
+    return count
+};
