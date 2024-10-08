59b6fbda5a3ae8950443c3df5a662a92526ed20a
Xavier <xavierh93@hotmail.com>
2024-10-08 08:16:56 -0400

    Time: 92 ms (66.44%) | Memory: 56.8 MB (55.01%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.ts
+++ 6-zigzag-conversion/zigzag-conversion.ts
@@ -1,18 +1,18 @@
 function convert(s: string, numRows: number): string {
     if (numRows === 1) return s
 
-    const rows: string[] = new Array((numRows)).fill('')
+    const rows: string[] = new Array(numRows).fill('')
     let currentRow: number = 0
     let goingDown: boolean = false
 
     for (const char of s) {
         rows[currentRow] += char
 
-        if (currentRow === 0 || currentRow === numRows -1) {
+        if (currentRow === 0 || currentRow === numRows - 1) {
             goingDown = !goingDown
         }
 
-        currentRow += goingDown ? 1 : -1
+        currentRow += goingDown ? 1 : - 1
     }
 
     return rows.join('')
