28fb0bad0e80892c4b26aa447bcf5ea053c4875a
Xavier <xavierh93@hotmail.com>
2024-10-08 19:18:39 -0400

    Time: 86 ms (84.48%) | Memory: 61.4 MB (89.11%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -0,0 +1,15 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const seen: Set<number> = new Set()
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) return true
+
+        seen.add(nums[i])
+
+        if (seen.size > k) {
+            seen.delete(nums[i - k])
+        } 
+    }
+
+    return false
+};
