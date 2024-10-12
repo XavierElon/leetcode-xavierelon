77f2f64b53a461fea70333854d28d04a8e4a9be2
Xavier <xavierh93@hotmail.com>
2024-10-12 19:50:45 -0400

    Time: 85 ms (59.46%) | Memory: 58.9 MB (48.65%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.ts
@@ -0,0 +1,20 @@
+function removeDuplicates(s: string, k: number): string {
+    const stack: [string, number][] = []
+
+    for (let char of s) {
+        if (stack.length > 0 && stack[stack.length - 1][0] === char) {
+            stack[stack.length - 1][1]++
+            if (stack[stack.length - 1][1] === k) {
+                stack.pop()
+            }
+        } else {
+            stack.push([char, 1])
+        }
+    }
+
+    let result: string = ''
+    for (const[char, count] of stack) {
+        result += char.repeat(count)
+    }
+    return result
+};
