4373d8120d2321bf7508c7c00eaf84fe78d80275
Xavier <xavierh93@hotmail.com>
2024-10-12 12:26:14 -0400

    Time: 67 ms (24.20%) | Memory: 51.7 MB (39.49%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
@@ -0,0 +1,17 @@
+function search(nums: number[], target: number): boolean {
+        let left = 0, right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((right + left) / 2); // lower mid
+
+        if (nums[mid] == nums[right]) right--; // the only difference between Q.33
+        else if (nums[mid] < nums[right]) { // right part sorted
+            if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+            else right = mid
+        } else{
+            if (target > nums[mid] || target < nums[left]) left = mid + 1;
+            else right = mid;
+        } 
+    }
+    return nums[left]==target;
+};
