0f1ad6b6248e54a92afa0192454182363813349e
Xavier <xavierh93@hotmail.com>
2024-10-12 12:30:00 -0400

    Time: 57 ms (75.16%) | Memory: 51.7 MB (52.23%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
@@ -1,9 +1,11 @@
 function search(nums: number[], target: number): boolean {
-        let left = 0, right = nums.length - 1;
+        let left: number = 0
+        let right: number = nums.length - 1;
 
     while (left < right) {
         let mid = Math.floor((right + left) / 2); // lower mid
-
+        if (nums[mid] === target) return true
+        if (nums[mid] === nums[left]) left++
         if (nums[mid] == nums[right]) right--; // the only difference between Q.33
         else if (nums[mid] < nums[right]) { // right part sorted
             if (target > nums[mid] && target <= nums[right]) left = mid + 1;
