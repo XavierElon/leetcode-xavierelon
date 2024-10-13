b149c853492b3fe59150616da08b8472f8c9c9be
Xavier <xavierh93@hotmail.com>
2024-10-13 15:19:46 -0400

    Time: 163 ms (57.83%) | Memory: 74.8 MB (63.45%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.ts
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.ts
@@ -1,14 +1,12 @@
 function arrayRankTransform(arr: number[]): number[] {
     if (arr.length === 0) return []
 
-    const sortedUnique = Array.from(new Set([...arr])).sort((a, b) => a - b)
+    const uniqueSorted: number[] = Array.from(new Set([...arr])).sort((a, b) => a - b)
 
     const rankMap: Map<number, number> = new Map()
-    sortedUnique.forEach((num, index) => {
-        rankMap.set(num, index + 1)
-    })
+    uniqueSorted.forEach((num, index) => rankMap.set(num, index + 1))
 
-    const rankedArr: number[] = arr.map(num => rankMap.get(num))
+    const result: number[] = arr.map(num => rankMap.get(num))
 
-    return rankedArr
+    return result
 };
