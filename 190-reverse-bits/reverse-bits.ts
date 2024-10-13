abd626fb26fa17a45959520a896d1748f4cded07
Xavier <xavierh93@hotmail.com>
2024-10-13 11:48:36 -0400

    Time: 69 ms (32.94%) | Memory: 51.9 MB (58.31%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.ts
@@ -0,0 +1,11 @@
+function reverseBits(n: number): number {
+	  var result = 0;
+  var count = 32;
+
+  while (count--) {
+    result *= 2;
+    result += n & 1;
+    n = n >> 1;
+  }
+  return result;
+};
