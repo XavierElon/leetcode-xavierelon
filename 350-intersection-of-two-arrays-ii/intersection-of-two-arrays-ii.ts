99531788132cef2511fa2bac1b4314b751b84c2e
Xavier <xavierh93@hotmail.com>
2024-10-13 09:01:43 -0400

    Time: 63 ms (54.90%) | Memory: 52.3 MB (47.17%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
@@ -1,17 +1,20 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
-    const countMap: { [key: number]: number } = {}
+    nums1.sort((a, b) => a - b)
+    nums2.sort((a, b) => a - b)
+    let i: number = 0
+    let j: number = 0
     const result: number[] = []
 
-    for (const num of nums1) {
-        countMap[num] = (countMap[num] || 0) + 1
-    }
-
-    for (const num of nums2) {
-        if (countMap[num] && countMap[num] > 0) {
-            result.push(num)
-            countMap[num]--
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            result.push(nums1[i])
+            i++
+            j++
+        } else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
         }
     }
-
     return result
 };
