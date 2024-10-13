fc31f69c16a6e7ef56dde0d92bad8df59e7788e8
Xavier <xavierh93@hotmail.com>
2024-10-13 14:52:38 -0400

    Time: 153 ms (75.91%) | Memory: 73.4 MB (76.90%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.ts
@@ -0,0 +1,15 @@
+function arrayRankTransform(arr: number[]): number[] {
+    if (arr.length === 0) return []
+
+    const sortedUnique = Array.from(new Set([...arr])).sort((a, b) => a - b)
+
+    const rankMap: Map<number, number> = new Map()
+
+    sortedUnique.forEach((num, index) => {
+        rankMap.set(num, index + 1)
+    })
+
+    const rankedArr: number[] = arr.map(num => rankMap.get(num))
+
+    return rankedArr
+};
