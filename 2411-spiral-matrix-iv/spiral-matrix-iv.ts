f357c8bdd61a87189dbe8a9f358806d6cf1b1359
Xavier <xavierh93@hotmail.com>
2024-10-14 17:50:13 -0400

    Time: 450 ms (62.66%) | Memory: 86.2 MB (86.82%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.ts
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function spiralMatrix(m: number, n: number, head: ListNode | null): number[][] {
+    const matrix: number[][] = Array.from({ length: m}, () => Array(n).fill(-1))
+    let top: number = 0
+    let bottom: number = m - 1
+    let left: number = 0
+    let right: number = n - 1
+
+    let current = head
+
+    while (top <= bottom && left <= right && current !== null) {
+        //Right
+        for (let i = left; i <= right && current; i++) {
+            matrix[top][i] = current.val
+            current = current.next
+        }
+        top++
+
+        // Bottom
+        for (let i = top; i <= bottom && current; i++) {
+            matrix[i][right] = current.val
+            current = current.next
+        }
+        right--
+
+        if (top <= bottom) {
+            for (let i = right; i >= left && current; i--) {
+                matrix[bottom][i] = current.val
+                current = current.next
+            }
+            bottom--
+        }
+
+        if (left <= right) {
+            for (let i = bottom; i >= top && current; i--) {
+                matrix[i][left] = current.val
+                current = current.next
+            }
+            left++
+        }
+    }
+
+    return matrix
+};
