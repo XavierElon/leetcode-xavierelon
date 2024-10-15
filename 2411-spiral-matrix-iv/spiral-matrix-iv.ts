2605857cfb84829b948179c18100af407ec8b611
Xavier <xavierh93@hotmail.com>
2024-10-15 09:16:26 -0400

    Time: 463 ms (51.03%) | Memory: 87.1 MB (72.48%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.ts
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.ts
@@ -12,29 +12,29 @@
 
 function spiralMatrix(m: number, n: number, head: ListNode | null): number[][] {
     const matrix: number[][] = Array.from({ length: m}, () => Array(n).fill(-1))
-    let top: number = 0
-    let bottom: number = m - 1
     let left: number = 0
     let right: number = n - 1
-
-    let current = head
+    let top: number = 0
+    let bottom: number = m - 1
+    let current: ListNode = head
 
     while (top <= bottom && left <= right && current !== null) {
-        //Right
+        // Right
         for (let i = left; i <= right && current; i++) {
             matrix[top][i] = current.val
             current = current.next
         }
         top++
 
-        // Bottom
+        // Down
         for (let i = top; i <= bottom && current; i++) {
             matrix[i][right] = current.val
             current = current.next
         }
         right--
 
-        if (top <= bottom) {
+        // Left
+        if (bottom >= top) {
             for (let i = right; i >= left && current; i--) {
                 matrix[bottom][i] = current.val
                 current = current.next
@@ -42,6 +42,7 @@ function spiralMatrix(m: number, n: number, head: ListNode | null): number[][] {
             bottom--
         }
 
+        // Up
         if (left <= right) {
             for (let i = bottom; i >= top && current; i--) {
                 matrix[i][left] = current.val
@@ -50,6 +51,5 @@ function spiralMatrix(m: number, n: number, head: ListNode | null): number[][] {
             left++
         }
     }
-
     return matrix
 };
