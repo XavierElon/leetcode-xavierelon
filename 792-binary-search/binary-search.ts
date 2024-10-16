21d4ecfbe0cae68e7d285c36fac8e96ef52540b8
Xavier <xavierelon93@gmail.com>
2024-10-16 12:02:50 -0400

    Time: 63 ms (47.84%) | Memory: 53.7 MB (44.50%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+    let left: number = 0
+    let right: number = nums.length - 1
+
+    while (left <= right) {
+        const mid: number = Math.floor((left + right) / 2)
+
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return -1
+};
