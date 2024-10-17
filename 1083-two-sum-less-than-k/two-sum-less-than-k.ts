422cc2a93f66ea460edaf8d92b24c199d65edb5b
Xavier <xavierelon93@gmail.com>
2024-10-17 11:59:47 -0400

    Time: 5 ms (100.00%) | Memory: 52.1 MB (5.88%) - LeetSync

--- 1083-two-sum-less-than-k/two-sum-less-than-k.ts
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.ts
@@ -1,11 +1,25 @@
 function twoSumLessThanK(nums: number[], k: number): number {
-    let maxSum: number = -Infinity
+    nums.sort((a, b) => a - b)
+    let maxSum: number = -1
 
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] < k) maxSum = Math.max(maxSum, nums[i] + nums[j])
+    function binarySearch(left: number, right: number, target: number): number {
+        while (left <= right) {
+            const mid: number = Math.floor((left + right) / 2)
+            
+            if (nums[mid] < target) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
         }
+        return right
     }
 
-    return maxSum === -Infinity ? -1 : maxSum
+    for (let i = 0; i < nums.length - 1; i++) {
+        const j: number = binarySearch(i + 1, nums.length - 1, k - nums[i])
+        if (j !== i) {
+            maxSum = Math.max(maxSum, nums[i] + nums[j])
+        }
+    }
+    return maxSum
 };
