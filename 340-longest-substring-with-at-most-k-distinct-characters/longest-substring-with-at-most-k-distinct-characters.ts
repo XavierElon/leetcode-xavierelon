2c2953a1cf3640810065bc1845da0803c5763af3
Xavier <xavierelon93@gmail.com>
2024-10-17 10:25:05 -0400

    Time: 15 ms (100.00%) | Memory: 53.2 MB (31.48%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.ts
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.ts
@@ -1,25 +1,24 @@
 function lengthOfLongestSubstringKDistinct(s: string, k: number): number {
     if (k === 0) return 0
-    const n: number = s.length
-    if (n === 0) return 0
+    const length: number = s.length
+    if (length === 0) return 0
 
-    let left: number = 0
-    let maxLen: number = 0
     const charMap: Map<string, number> = new Map()
+    let left: number = 0
+    let maxLength: number = 0
 
-    for (let right = 0; right < n; right++) {
+    for (let right = 0; right < length; right++) {
         const currentChar: string = s[right]
         charMap.set(currentChar, (charMap.get(currentChar) || 0) + 1)
-        
         while (charMap.size > k) {
-            const leftChar: string = s[left]
+            const leftChar: string = s[left++]
             charMap.set(leftChar, charMap.get(leftChar) - 1)
             if (charMap.get(leftChar) === 0) {
                 charMap.delete(leftChar)
             }
-            left++
         }
-        maxLen = Math.max(maxLen, right - left + 1)
+        maxLength = Math.max(maxLength, right - left + 1)
     }
-    return maxLen
+
+    return maxLength
 };
