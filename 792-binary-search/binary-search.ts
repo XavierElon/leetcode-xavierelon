66817b7d263f6f4f554a75dacf8d879224cdb85e
Xavier <xavierelon93@gmail.com>
2024-10-18 08:04:08 -0400

    Time: 0 ms (100.00%) | Memory: 53.2 MB (92.12%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -3,15 +3,12 @@ function search(nums: number[], target: number): number {
     let right: number = nums.length - 1
 
     while (left <= right) {
-        const mid: number = Math.floor((left + right) / 2) 
+        const mid: number = Math.floor((left + right) / 2)
 
-        if (nums[mid] === target) {
-            return mid
-        } else if (nums[mid] < target) {
-            left = mid + 1
-        } else {
-            right = mid - 1
-        }
+        if (nums[mid] === target) return mid
+        else if (nums[mid] < target) left = mid + 1
+        else right = mid - 1
     }
+
     return -1
 };
