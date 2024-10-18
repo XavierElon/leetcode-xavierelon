b0e97c64446ee8ec5102f10e48d50d4b08cbff32
Xavier <xavierelon93@gmail.com>
2024-10-18 09:29:25 -0400

    Time: 3 ms (100.00%) | Memory: 52 MB (99.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.ts
+++ 1036-rotting-oranges/rotting-oranges.ts
@@ -1,47 +1,47 @@
 function orangesRotting(grid: number[][]): number {
     let freshCount: number = 0
-    let rottenOranges: number[] = []
-    let rowCount: number = grid.length
-    let colCount: number = grid[0].length
+    let rottingOranges: number[] = []
+    let rows: number = grid.length
+    let cols: number = grid[0].length
     let min: number = 0
 
-    for (let i = 0; i < rowCount; i++) {
-        for (let j = 0; j < colCount; j++) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
             if (grid[i][j] === 1) freshCount++
-            else if (grid[i][j] === 2) rottenOranges.push(i * colCount + j)
+            else if (grid[i][j] === 2) rottingOranges.push(i * cols + j)
         }
     }
 
-    while (freshCount !== 0 && rottenOranges.length !== 0) {
-        const size: number = rottenOranges.length
+    while (freshCount !== 0 && rottingOranges.length !== 0) {
+        const size: number = rottingOranges.length
 
         for (let i = 0; i < size; i++) {
-            const rottenPos: number = rottenOranges.shift()
-            const row: number = Math.floor(rottenPos / colCount)
-            const col: number = rottenPos % colCount
+            const rottenPos: number = rottingOranges.shift()
+            const row: number =  Math.floor(rottenPos / cols)
+            const col: number = rottenPos % cols
 
             if (row - 1 >= 0 && grid[row-1][col] === 1) {
-                rottenOranges.push((row - 1) * colCount + col)
-                grid[row-1][col] = 2
+                rottingOranges.push((row - 1) * cols + col)
                 freshCount--
+                grid[row-1][col] = 2
             }
 
-            if (row + 1 < rowCount && grid[row+1][col] === 1) {
-                rottenOranges.push((row+1) * colCount + col)
-                grid[row+1][col] = 2
+            if (row + 1 < rows && grid[row+1][col] === 1) {
+                rottingOranges.push((row + 1) * cols + col)
                 freshCount--
+                grid[row+1][col] = 2
             }
 
             if (col - 1 >= 0 && grid[row][col-1] === 1) {
-                rottenOranges.push(row * colCount + (col - 1))
-                grid[row][col-1] = 2
+                rottingOranges.push(row * cols + (col - 1))
                 freshCount--
+                grid[row][col-1] = 2
             }
 
-            if (col + 1 < colCount && grid[row][col+1] === 1) {
-                rottenOranges.push(row * colCount + (col + 1))
-                grid[row][col+1] = 2
+            if (col + 1 < cols && grid[row][col+1] === 1) {
+                rottingOranges.push(row * cols + (col + 1))
                 freshCount--
+                grid[row][col+1] = 2
             }
         }
         min++
