9023d6f7061c0162fb0405158ad5a83699bf557e
Xavier <xavierelon93@gmail.com>
2024-10-23 09:54:46 -0400

    Time: 19 ms (96.15%) | Memory: 57.2 MB (30.69%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -0,0 +1,26 @@
+function topKFrequent(nums: number[], k: number): number[] {
+    const frequencyMap: Map<number, number> = new Map()
+
+    for (const num of nums) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1)
+    }
+
+    const bucket: number[][] = Array.from({ length: nums.length + 1 }, () => [])
+
+    for (const [num, freq] of frequencyMap.entries()) {
+        bucket[freq].push(num)
+    }
+
+    const result: number[] = []
+
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (bucket[i].length > 0) {
+            for (const num of bucket[i]) {
+                result.push(num)
+                if (result.length === k) {
+                    return result
+                }
+            }
+        }
+    }
+};
