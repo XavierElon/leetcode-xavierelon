058d5b3db210c5fe5980e65cb8e44011eba4a96c
Xavier <xavierelon93@gmail.com>
2024-10-23 12:15:38 -0400

    Time: 0 ms (100.00%) | Memory: 52.6 MB (78.59%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -0,0 +1,26 @@
+function trap(height: number[]): number {
+    let left: number = 0
+    let right: number = height.length - 1
+    let leftMax: number = 0
+    let rightMax: number = 0
+    let water: number = 0
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            if (height[left] > leftMax) {
+                leftMax = height[left]
+            } else {
+                water += leftMax - height[left]
+            }
+            left++
+        } else {
+            if (height[right] > rightMax) {
+                rightMax = height[right]
+            } else {
+                water += rightMax - height[right]
+            }
+            right--
+        }
+    }
+    return water
+};
