04d842be346b57f5746570ea99151f0150e1faa6
Xavier <xavierelon93@gmail.com>
2024-10-24 10:37:27 -0400

    Time: 7 ms (100.00%) | Memory: 55.4 MB (82.17%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.ts
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.ts
@@ -2,13 +2,14 @@ function frequencySort(s: string): string {
     const map: Map<string, number> = new Map()
 
     for (const char of s) {
-        map.set(char, (map.get(char) || 0) + 1) 
+        map.set(char, (map.get(char) || 0) + 1)
     }
 
-    const sortedCharacters = Array.from(map.entries()).sort((a, b) => b[1] - a[1])
+    const sortedFrequencies = Array.from(map.entries()).sort((a, b) => b[1] - a[1])
 
     let result: string = ''
-    for (const [char, freq] of sortedCharacters) {
+
+    for (const [char, freq] of sortedFrequencies) {
         result += char.repeat(freq)
     }
 
