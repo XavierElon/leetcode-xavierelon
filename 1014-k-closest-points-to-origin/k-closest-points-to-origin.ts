f375b8d320a1fdec658f25db3e512a4ac5620b03
Xavier <xavierelon93@gmail.com>
2024-10-24 11:15:46 -0400

    Time: 36 ms (99.76%) | Memory: 62.9 MB (88.41%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.ts
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.ts
@@ -1,12 +1,11 @@
 function kClosest(points: number[][], k: number): number[][] {
     points.sort((a, b) => {
-     
-        return squaredDistance(a) - squaredDistance(b)
-    })   
+        return squaredDist(a) - squaredDist(b)
+    })
 
     return points.slice(0, k)
 };
 
-const squaredDistance = (point: number[]): number => {
+const squaredDist = (point: number[]): number => {
     return point[0] * point[0] + point[1] * point[1]
 }
