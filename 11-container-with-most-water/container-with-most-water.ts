5351b61f7e1394354d6db6d64ea035f4c83682aa
Xavier <xavierelon93@gmail.com>
2024-10-24 12:11:21 -0400

    Time: 3 ms (97.07%) | Memory: 59.5 MB (25.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,12 +1,12 @@
 function maxArea(height: number[]): number {
     let left: number = 0
-    let right: number = height.length -1
+    let right: number = height.length - 1
     let maxWater: number = 0
 
     while (left < right) {
         const width: number = right - left
         const containerHeight: number = Math.min(height[left], height[right])
-        maxWater = Math.max(maxWater, width * containerHeight)
+        maxWater = Math.max(maxWater, containerHeight * width)
 
         if (height[left] < height[right]) {
             left++
