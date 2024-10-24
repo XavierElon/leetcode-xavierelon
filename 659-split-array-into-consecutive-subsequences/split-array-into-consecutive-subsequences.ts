133192704e3429df1d31698d9783935ff3cf1be2
Xavier <xavierelon93@gmail.com>
2024-10-24 11:08:11 -0400

    Time: 31 ms (100.00%) | Memory: 61.7 MB (25.00%) - LeetSync

--- 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.ts
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.ts
@@ -14,12 +14,12 @@ function isPossible(nums: number[]): boolean {
         if (tails.get(num - 1) > 0) {
             tails.set(num - 1, tails.get(num - 1) - 1)
             tails.set(num, (tails.get(num) || 0) + 1)
-            freqMap.set(num, freqMap.get(num) - 1)
-        } else if ((freqMap.get(num + 1)) > 0 && (freqMap.get(num + 2)) > 0) {
-            freqMap.set(num + 1, freqMap.get(num + 1) - 1)
-            freqMap.set(num + 2, freqMap.get(num + 2) - 1)
+            freqMap.set(num, (freqMap.get(num) || 0) - 1) 
+        } else if (freqMap.get(num + 1) > 0 && freqMap.get(num + 2) > 0) {
             tails.set(num + 2, (tails.get(num + 2) || 0) + 1)
-            freqMap.set(num, freqMap.get(num) - 1)
+            freqMap.set(num, (freqMap.get(num) || 0) - 1)
+            freqMap.set(num + 1, (freqMap.get(num + 1) || 0) - 1)
+            freqMap.set(num + 2, (freqMap.get(num + 2) || 0) - 1) 
         } else {
             return false
         }
