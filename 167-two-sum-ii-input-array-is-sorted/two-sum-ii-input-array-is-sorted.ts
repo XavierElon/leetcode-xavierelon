1f0a96225364e51cbea04b608741a27fa097d1bf
Xavier <xavierelon93@gmail.com>
2024-10-24 11:46:59 -0400

    Time: 3 ms (96.90%) | Memory: 51.6 MB (74.03%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,19 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let left: number = 0
+    let right: number = numbers.length - 1
+
+    while (left < right) {
+        const sum: number = numbers[left] + numbers[right]
+
+        if (sum === target) {
+            return [left + 1, right + 1]
+        }
+
+        if (sum < target) {
+            left++
+        } else {
+            right--
+        }
+
+    }
+};
