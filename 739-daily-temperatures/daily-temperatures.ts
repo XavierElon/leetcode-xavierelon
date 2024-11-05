783fa0ee24ca58fc0a485e243a8165936b4ea3d1
Xavier <xavierelon93@gmail.com>
2024-11-05 12:29:34 -0500

    Time: 17 ms (91.08%) | Memory: 71.2 MB (92.28%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.ts
@@ -0,0 +1,15 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    const n: number = temperatures.length
+    const answer: number[] = new Array(n).fill(0)
+    const stack: number[] = []
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const prevIndex: number = stack.pop()
+            answer[prevIndex] = i - prevIndex
+        }
+        stack.push(i)
+    }
+
+    return answer
+};
