7fdd16005276fa7eeec21de2396bfb0517e94e47
Xavier <xavierelon93@gmail.com>
2024-11-05 13:16:48 -0500

    Time: 156 ms (30.61%) | Memory: 80.5 MB (15.25%) - LeetSync

--- 883-car-fleet/car-fleet.ts
+++ 883-car-fleet/car-fleet.ts
@@ -1,19 +1,18 @@
 interface Car {
-    pos: number
     speed: number
+    position: number
 }
 
 function carFleet(target: number, position: number[], speed: number[]): number {
-    const cars: Car[] = position.map((pos, index) => ({ pos, speed: speed[index]}))
-
-    cars.sort((a, b) => b.pos - a.pos)
-
     const stack: number[] = []
+    const cars: Car[] = position.map((pos, index) => ({ position: pos, speed: speed[index] }))
+
+    cars.sort((a, b) => b.position - a.position)
 
     for (const car of cars) {
-        const time = (target - car.pos) / car.speed
+        const time: number = (target - car.position) / car.speed
 
-        if (stack.length === 0 || time > stack[stack.length - 1]) stack.push(time)
+        if (stack.length === 0 || time > stack[stack.length-1]) stack.push(time)
     }
 
     return stack.length
