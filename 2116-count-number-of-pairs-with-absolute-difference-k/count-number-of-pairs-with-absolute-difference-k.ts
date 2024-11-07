601e8dc4db34182480cd3cd4fa2adc429056899d
Xavier <xavierelon93@gmail.com>
2024-11-07 15:50:05 -0500

    Time: 6 ms (16.67%) | Memory: 52.6 MB (16.95%) - LeetSync

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.ts
@@ -0,0 +1,18 @@
+function countKDifference(nums: number[], k: number): number {
+    const frequencyMap: Map<number, number> = new Map()
+    let count: number = 0
+
+    for (const num of nums) {
+        if (frequencyMap.has(num + k)) {
+            count += frequencyMap.get(num + k)
+        }
+
+        if (k !== 0 && frequencyMap.has(num - k)) {
+            count += frequencyMap.get(num - k)
+        }
+
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1)
+    }
+
+    return count
+};
