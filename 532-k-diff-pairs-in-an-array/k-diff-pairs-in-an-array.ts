a5d10b6bbee6b5108db9d5777b984cac2c919a17
Xavier <xavierelon93@gmail.com>
2024-11-07 17:39:59 -0500

    Time: 8 ms (73.68%) | Memory: 54.9 MB (65.71%) - LeetSync

---
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.ts
@@ -0,0 +1,27 @@
+function findPairs(nums: number[], k: number): number {
+    if (k < 0) return 0
+
+    const frequencyMap: Map<number, number> = new Map()
+
+    for (const num of nums) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1)
+    }
+
+    let count: number = 0
+
+    if (k === 0) {
+        for (const [num, freq] of frequencyMap.entries()) {
+            if (freq >= 2) {
+                count++
+            }
+        }
+    } else {
+        for (const num of frequencyMap.keys()) {
+            if (frequencyMap.has(num + k)) {
+                count++
+            }
+        }
+    }
+
+    return count
+};
