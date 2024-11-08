c5088d525db6da7ff7144ad597d1c77835f94582
Xavier <xavierelon93@gmail.com>
2024-11-08 10:53:50 -0800

    Time: 3 ms (30.95%) | Memory: 51.5 MB (76.12%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.ts
+++ 410-split-array-largest-sum/split-array-largest-sum.ts
@@ -1,6 +1,6 @@
 function splitArray(nums: number[], k: number): number {
-
-    const canSplit = (capacity: number): boolean => {
+    
+    const isSplittable = (capacity: number): boolean => {
         let requiredSubarrays: number = 1
         let currentSum: number = 0
 
@@ -18,19 +18,17 @@ function splitArray(nums: number[], k: number): number {
         }
         return true
     }
-    
+
     let left: number = Math.max(...nums)
-    let right: number = nums.reduce((acc, cur) => acc + cur, 0)
+    let right: number = nums.reduce((acc, num) => acc + num, 0)
 
     while (left < right) {
         const mid: number = Math.floor((left + right) / 2)
-
-        if (canSplit(mid)) {
+        if (isSplittable(mid)) {
             right = mid
         } else {
             left = mid + 1
         }
     }
-
-    return left
+    return right
 };
