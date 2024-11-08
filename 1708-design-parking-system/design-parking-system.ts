dc84a97d92a9a4298bd6bc1d3459606cb5e6e7ea
Xavier <xavierelon93@gmail.com>
2024-11-08 08:43:59 -0800

    Time: 19 ms (26.92%) | Memory: 59.8 MB (17.95%) - LeetSync

---
+++ 1708-design-parking-system/design-parking-system.ts
@@ -0,0 +1,28 @@
+class ParkingSystem {
+    private parkingSlots: number[]
+
+    constructor(big: number, medium: number, small: number) {
+        this.parkingSlots = [big, medium, small]
+    }
+
+    addCar(carType: number): boolean {
+        const index: number = carType - 1
+
+        if (index < 0 || index >= this.parkingSlots.length) {
+            throw new Error("Invalid Car Type")
+        }
+
+        if (this.parkingSlots[index] > 0) {
+            this.parkingSlots[index]--
+            return true
+        }
+
+        return false
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
