08d9617f1875f7fbffe4a2df74f4312b1127597b
Xavier <xavierelon93@gmail.com>
2024-11-08 10:09:06 -0800

    Time: 99 ms (70.83%) | Memory: 59.6 MB (54.55%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.ts
+++ 348-design-tic-tac-toe/design-tic-tac-toe.ts
@@ -1,43 +1,37 @@
 class TicTacToe {
-    rows: number[];
-    cols: number[];
-    diagonal: number;
-    antiDiagonal: number;
+    private rows: number[]
+    private cols: number[]
+    private diagonal: number
+    private antiDiagonal: number
 
     constructor(n: number) {
-        this.rows = Array(n).fill(0);
-        this.cols = Array(n).fill(0);
-        this.diagonal = 0;
-        this.antiDiagonal = 0;
+        this.rows = Array(n).fill(0)
+        this.cols = Array(n).fill(0)
+        this.diagonal = 0
+        this.antiDiagonal = 0
     }
 
     move(row: number, col: number, player: number): number {
-        const currentPlayer = player === 1 ? 1 : -1;
-        // update currentPlayer in rows an cols array
-        this.rows[row] += currentPlayer;
-        this.cols[col] += currentPlayer;
+        const currentPlayer = player === 1 ? 1 : -1
+
+        this.rows[row] += currentPlayer
+        this.cols[col] += currentPlayer
 
         if (row === col) {
-            this.diagonal += currentPlayer;
+            this.diagonal += currentPlayer
         }
 
         if (col === (this.cols.length - row - 1)) {
-            this.antiDiagonal += currentPlayer;
+            this.antiDiagonal += currentPlayer
         }
 
-        const n = this.rows.length;
+        const n: number = this.rows.length
 
-        // check if current player wins
-        if(Math.abs(this.rows[row]) === n  || 
-            Math.abs(this.cols[col]) === n ||
-            Math.abs(this.diagonal) === n  ||
-            Math.abs(this.antiDiagonal) === n
-            ) {
-                return player;
-            }
+        if (Math.abs(this.rows[row]) === n || Math.abs(this.cols[col]) === n || Math.abs(this.diagonal) === n || Math.abs(this.antiDiagonal) === n) {
+            return player
+        }
 
-            // no one wins
-            return 0;
+        return 0
     }
 }
 
