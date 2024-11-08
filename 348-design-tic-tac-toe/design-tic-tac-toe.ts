6603ecb63e3ddfdc26e6844ff179bf425e85a5fe
Xavier <xavierelon93@gmail.com>
2024-11-08 10:21:07 -0800

    Time: 109 ms (20.83%) | Memory: 59.5 MB (63.64%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.ts
+++ 348-design-tic-tac-toe/design-tic-tac-toe.ts
@@ -1,33 +1,28 @@
 class TicTacToe {
     private rows: number[]
     private cols: number[]
-    private diagonal: number
-    private antiDiagonal: number
+    private diagonals: number
+    private antiDiagonals: number
 
     constructor(n: number) {
-        this.rows = Array(n).fill(0)
-        this.cols = Array(n).fill(0)
-        this.diagonal = 0
-        this.antiDiagonal = 0
+        this.rows = new Array(n).fill(0)
+        this.cols = new Array(n).fill(0)
+        this.diagonals = 0
+        this.antiDiagonals = 0
     }
 
     move(row: number, col: number, player: number): number {
-        const currentPlayer = player === 1 ? 1 : -1
+        const currentPlayer: number = player === 1 ? 1 : -1
 
         this.rows[row] += currentPlayer
         this.cols[col] += currentPlayer
 
-        if (row === col) {
-            this.diagonal += currentPlayer
-        }
-
-        if (col === (this.cols.length - row - 1)) {
-            this.antiDiagonal += currentPlayer
-        }
+        if (row === col) this.diagonals += currentPlayer
+        if (col === (this.cols.length - row - 1)) this.antiDiagonals += currentPlayer
 
         const n: number = this.rows.length
 
-        if (Math.abs(this.rows[row]) === n || Math.abs(this.cols[col]) === n || Math.abs(this.diagonal) === n || Math.abs(this.antiDiagonal) === n) {
+        if (Math.abs(this.rows[row]) === n || Math.abs(this.cols[col]) === n || Math.abs(this.diagonals) === n || Math.abs(this.antiDiagonals) === n) {
             return player
         }
 
