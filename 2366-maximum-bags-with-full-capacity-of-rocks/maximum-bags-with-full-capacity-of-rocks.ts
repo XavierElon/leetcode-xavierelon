747d01a60e2fc7e2220cd6c7af122fa8824a392b
Xavier <xavierelon93@gmail.com>
2024-11-08 08:15:11 -0800

    Time: 835 ms (14.29%) | Memory: 74.4 MB (10.00%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.ts
@@ -0,0 +1,19 @@
+function maximumBags(capacity: number[], rocks: number[], additionalRocks: number): number {
+    let arr = []
+    let ans: number = 0
+
+    for (const rock in rocks) {
+        console.log(rock)
+        arr.push(capacity[rock] - rocks[rock])
+    }
+
+    arr.sort((a, b) => a - b)
+
+    for (let val of arr) {
+        additionalRocks -= val
+        if (additionalRocks < 0) break
+        ans += 1
+    }
+
+    return ans
+};
