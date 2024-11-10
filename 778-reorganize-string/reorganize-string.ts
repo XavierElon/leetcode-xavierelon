925a5260b196069e557110741e77c6c66b3d3024
Xavier <xavierelon93@gmail.com>
2024-11-09 22:28:26 -0500

    Time: 0 ms (100.00%) | Memory: 52.6 MB (80.72%) - LeetSync

--- 778-reorganize-string/reorganize-string.ts
+++ 778-reorganize-string/reorganize-string.ts
@@ -5,28 +5,17 @@ function reorganizeString(s: string): string {
         frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1)
     }
 
-    const sortedArray = Array.from(frequencyMap.entries()).sort((a, b) => b[1] - a[1])
+    const sortedArray: Array<[string, number]> = Array.from(frequencyMap.entries()).sort((a, b) => b[1] - a[1])
 
-    const maxCount: number = sortedArray[0][1]
+    const maxCount = sortedArray[0][1]
     if (maxCount > Math.ceil(s.length / 2)) return ""
 
-    const ans: string[] = new Array(s.length)
+    const ans: string[] = []
     let index: number = 0
 
-    const [mostFreqChar, mostFreq] = sortedArray[0]
-
-    for (let i = 0; i < mostFreq; i++) {
-        ans[index] = mostFreqChar
-        index += 2
-    }
-
-    for (let i = 1; i < sortedArray.length; i++) {
-        const [char, count] = sortedArray[i]
-        for (let j = 0; j < count; j++) {
-            if (index >= s.length) {
-                index = 1
-            }
-   
+    for (const [char, count] of sortedArray) {
+        for (let i = 0; i < count; i++) {
+            if (index >= s.length) index = 1
             ans[index] = char
             index += 2
         }
