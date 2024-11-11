278438367830f90e5e66d3072658111d1015d756
Xavier <xavierelon93@gmail.com>
2024-11-11 11:33:23 -0500

    Time: 7 ms (73.53%) | Memory: 54.3 MB (31.17%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.ts
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.ts
@@ -1,16 +1,14 @@
 function maximumUnits(boxTypes: number[][], truckSize: number): number {
     boxTypes.sort((a, b) => b[1] - a[1])
-    let remainingTruckSize: number = truckSize
+    let remainingBoxes: number = truckSize
     let totalUnits: number = 0
 
     for (const [numberOfBoxes, numberOfUnitsPerBox] of boxTypes) {
-        if (remainingTruckSize === 0) {
-            break
-        }
-
-        const minBoxes: number = Math.min(remainingTruckSize, numberOfBoxes)
+        const minBoxes: number = Math.min(numberOfBoxes, remainingBoxes) 
         totalUnits += minBoxes * numberOfUnitsPerBox
-        remainingTruckSize -= minBoxes
+        remainingBoxes -= minBoxes
+        if (remainingBoxes === 0) break
     }
+
     return totalUnits
 };
