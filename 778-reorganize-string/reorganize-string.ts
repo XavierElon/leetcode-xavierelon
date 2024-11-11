23fbd99de3b9a98a07e028b95559e3ff2db727f7
Xavier <xavierelon93@gmail.com>
2024-11-11 16:24:31 -0500

    Time: 2 ms (82.86%) | Memory: 52.1 MB (97.67%) - LeetSync

--- 778-reorganize-string/reorganize-string.ts
+++ 778-reorganize-string/reorganize-string.ts
@@ -1,38 +1,25 @@
 function reorganizeString(s: string): string {
-    const charCounts: number[] = new Array(26).fill(0)
+    const frequencyMap: Map<string, number> = new Map()
+
     for (const char of s) {
-        charCounts[char.charCodeAt(0) - 'a'.charCodeAt(0)]++
+        frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1)
     }
 
-    let maxCount: number = 0
-    let letterIndex: number = 0
-    for (let i = 0; i < 26; i++) {
-        if (charCounts[i] > maxCount) {
-            maxCount = charCounts[i]
-            letterIndex = i
-        }
-    }
+    const sortedArray: Array<[string, number]> = Array.from(frequencyMap.entries()).sort((a, b) => b[1] - a[1])
 
+    const maxCount: number = sortedArray[0][1]
     if (maxCount > Math.ceil(s.length / 2)) return ''
 
-    const ans: string[] = new Array(s.length)
+    const ans: string[] = []
     let index: number = 0
 
-    while (charCounts[letterIndex] > 0) {
-        ans[index] = String.fromCharCode(letterIndex + 'a'.charCodeAt(0))
-        index += 2
-        charCounts[letterIndex]--
-    }
-
-    for (let i = 0; i < 26; i++) {
-        while (charCounts[i] > 0) {
-            if (index >= s.length) {
-                index = 1
-            }
-            ans[index] = String.fromCharCode(i + 'a'.charCodeAt(0))
+    for (const [char, count] of sortedArray) {
+        for (let i = 0; i < count; i++) {
+            if (index >= s.length) index = 1
+            ans[index] = char
             index += 2
-            charCounts[i]--
         }
     }
+
     return ans.join('')
 };
