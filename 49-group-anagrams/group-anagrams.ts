3e70c3b8fa4a021995c9f95c57cda1f34b72f6a8
Xavier <xavierelon93@gmail.com>
2024-11-12 09:31:49 -0500

    Time: 35 ms (58.08%) | Memory: 63.1 MB (73.10%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,15 +1,13 @@
 function groupAnagrams(strs: string[]): string[][] {
-    const map = {}
+    const map: Map<string, string[]> = new Map()
 
     for (const str of strs) {
         const sortedStr: string = str.split('').sort().join('')
-
-        if (!map[sortedStr]) {
-            map[sortedStr] = [str]
+        if (map.has(sortedStr)) {
+            map.get(sortedStr).push(str)
         } else {
-            map[sortedStr].push(str)
+            map.set(sortedStr, [str])
         }
     }
-
-    return Object.values(map)
+    return Array.from(map.values())
 };
