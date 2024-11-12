b9dc508a9fd30ce63c657ea07e69be04be16f021
Xavier <xavierelon93@gmail.com>
2024-11-12 11:30:57 -0500

    Time: 84 ms (56.82%) | Memory: 57.6 MB (53.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.ts
+++ 200-number-of-islands/number-of-islands.ts
@@ -2,26 +2,25 @@ function numIslands(grid: string[][]): number {
     if (grid.length === 0) return 0
     const numRows: number = grid.length
     const numCols: number = grid[0].length
-    let islandCount: number = 0
+    let numIslands: number = 0
 
     const directions: number[][] = [
         [-1, 0], // Up
         [1, 0], // Down
         [0, -1], // Left
-        [0, 1] // Right
+        [0, 1] //Right
     ]
 
     const bfs = (startRow: number, startCol: number) => {
         const queue: number[][] = []
         queue.push([startRow, startCol])
-        grid[startRow][startCol] = '0'
+        grid[startRow][startCol]
 
-        while (queue.length > 0) {
-            const [currentRow, currentCol] = queue.shift()
-
+        while (queue.length !== 0) {
+            const [currentRow, currentCol]: number[] = queue.shift()
             for (const [dRow, dCol] of directions) {
-                const newRow: number = currentRow + dRow
-                const newCol: number = currentCol + dCol
+                const newRow: number = dRow + currentRow
+                const newCol: number = dCol + currentCol
 
                 if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols && grid[newRow][newCol] === '1') {
                     queue.push([newRow, newCol])
@@ -34,11 +33,11 @@ function numIslands(grid: string[][]): number {
     for (let row = 0; row < numRows; row++) {
         for (let col = 0; col < numCols; col++) {
             if (grid[row][col] === '1') {
-                islandCount++
+                numIslands++
                 bfs(row, col)
             }
         }
     }
 
-    return islandCount
+    return numIslands
 };
