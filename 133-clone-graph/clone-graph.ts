979404ac33a2112b2d78afbf30f303ea38da4890
Xavier <xavierelon93@gmail.com>
2024-11-11 19:49:44 -0500

    Time: 57 ms (86.05%) | Memory: 51.5 MB (93.22%) - LeetSync

--- 133-clone-graph/clone-graph.ts
+++ 133-clone-graph/clone-graph.ts
@@ -1,34 +1,23 @@
-/**
- * Definition for _Node.
- * class _Node {
- *     val: number
- *     neighbors: _Node[]
- * 
- *     constructor(val?: number, neighbors?: _Node[]) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.neighbors = (neighbors===undefined ? [] : neighbors)
- *     }
- * }
- * 
- */
-
-
 function cloneGraph(node: _Node | null): _Node | null {
-	if (node === null) return
+    const visited = new Map<_Node, _Node>();
+    return clone(node, visited);
+};
 
-    const map: Map<_Node, _Node> = new Map();
-
-    const dfs = (n: _Node): _Node => {
-        if (map.has(n)) return map.get(n)
-
-        const clone: _Node = new _Node(n.val)
-        map.set(n, clone)
-
-        for (const neighbor of n.neighbors) {
-            clone.neighbors.push(dfs(neighbor))
-        }
-        return clone
+function clone(node: _Node | null, visited: Map<_Node, _Node>): _Node | null {
+    if(node === null){
+        return node;
     }
 
-    return dfs(node)
-};
+    if(visited.has(node)){
+        return visited.get(node);
+    }
+
+    const copy = new Node(node.val);
+    visited.set(node, copy);
+
+    for(const neighbor of node.neighbors){
+        copy.neighbors.push(clone(neighbor, visited));
+    }
+    
+    return copy;
+}
