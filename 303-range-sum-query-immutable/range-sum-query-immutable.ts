776883bae2777e76fb32ff8e9b9f6035f616e464
Xavier <xavierelon93@gmail.com>
2024-11-15 10:39:45 -0800

    Time: 6 ms (83.19%) | Memory: 59.4 MB (68.60%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.ts
@@ -0,0 +1,22 @@
+class NumArray {
+    private prefixSums: number[]
+
+    constructor(nums: number[]) {
+        this.prefixSums = new Array(nums.length + 1).fill(0)
+
+        for (let i = 0; i < nums.length; i++) {
+            this.prefixSums[i+1] = this.prefixSums[i] + nums[i]
+        }
+
+    }
+
+    sumRange(left: number, right: number): number {
+        return this.prefixSums[right+1] - this.prefixSums[left]
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
