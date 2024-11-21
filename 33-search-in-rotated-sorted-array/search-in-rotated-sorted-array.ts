19fcf45172819db84eb4098ad448ecb426066387
Xavier <xavierelon93@gmail.com>
2024-11-21 16:48:04 -0500

    Time: 0 ms (100.00%) | Memory: 51.7 MB (32.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -3,7 +3,7 @@ function search(nums: number[], target: number): number {
     let right: number = nums.length - 1
 
     while (left <= right) {
-        let mid: number = Math.floor((left + right) / 2)
+        const mid: number = Math.floor((left + right) / 2)
 
         if (nums[mid] === target) return mid
 
@@ -14,12 +14,12 @@ function search(nums: number[], target: number): number {
                 left = mid + 1
             }
         } else {
-            if (nums[mid] < target && target <= nums[right]) {
+            if (nums[mid] < target && nums[right] >= target) {
                 left = mid + 1
             } else {
                 right = mid - 1
             }
         }
-    }
+    } 
     return -1
 };
