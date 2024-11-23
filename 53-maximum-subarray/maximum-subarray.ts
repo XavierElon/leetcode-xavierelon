6bcf8944b0cbf1a78e6f1818fbb281c94c84e12b
Xavier <xavierelon93@gmail.com>
2024-11-23 14:44:43 -0500

    Time: 6 ms (13.41%) | Memory: 64 MB (5.15%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -0,0 +1,10 @@
+function maxSubArray(nums: number[]): number {
+    const dp = []
+    dp.push(nums[0])
+    
+    for (let i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i])
+    }
+    
+    return Math.max(...dp)
+};
