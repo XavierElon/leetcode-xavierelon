90132857f53c1817a8f2e7ced07cbc9b63f6ecff
Xavier <xavierelon93@gmail.com>
2024-11-23 14:47:59 -0500

    Time: 1 ms (82.49%) | Memory: 51.1 MB (37.97%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.js
@@ -0,0 +1,25 @@
+var customSortString = function(order, s) {
+    const charCount = {};
+    for (const char of order) {
+        charCount[char] = 0;
+    }
+
+    for (const char of s) {
+        if (charCount[char] !== undefined) {
+            charCount[char]++;
+        }
+    }
+
+    let sortedS = '';
+    for (const char of order) {
+        sortedS += char.repeat(charCount[char]);
+    }
+
+    for (const char of s) {
+        if (!order.includes(char)) {
+            sortedS += char;
+        }
+    }
+
+    return sortedS;
+};
