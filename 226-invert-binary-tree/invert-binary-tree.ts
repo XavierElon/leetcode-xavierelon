a4a3cd7194dcc5c644598634a8dd58189ee72958
Xavier <xavierelon93@gmail.com>
2024-11-24 11:00:06 -0500

    Time: 0 ms (100.00%) | Memory: 51.7 MB (58.44%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.ts
+++ 226-invert-binary-tree/invert-binary-tree.ts
@@ -19,5 +19,6 @@ function invertTree(root: TreeNode | null): TreeNode | null {
     root.right = temp
     if (root.left) invertTree(root.left)
     if (root.right) invertTree(root.right)
+
     return root
 };
