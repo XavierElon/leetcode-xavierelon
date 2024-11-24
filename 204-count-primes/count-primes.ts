292cbe79f1f3f0248ac0fbedb8efbe24b75fe7b5
Xavier <xavierelon93@gmail.com>
2024-11-24 11:26:38 -0500

    Time: 216 ms (62.12%) | Memory: 98 MB (35.20%) - LeetSync

--- 204-count-primes/count-primes.ts
+++ 204-count-primes/count-primes.ts
@@ -1,11 +1,11 @@
 function countPrimes(n: number): number {
     if (n <= 2) return 0
-    let isPrime: boolean[] = new Array(n).fill(true)
+    const isPrime: boolean[] = new Array(n).fill(true)
+
+    const sqrtN: number = Math.floor(Math.sqrt(n))
 
     isPrime[0] = false
     isPrime[1] = false
-
-    const sqrtN = Math.floor(Math.sqrt(n))
 
     for (let i = 2; i <= sqrtN; i++) {
         if (isPrime[i]) {
