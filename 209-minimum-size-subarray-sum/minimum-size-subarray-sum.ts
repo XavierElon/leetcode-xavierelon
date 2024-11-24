b6a4895e94bd993c0805f6ec96a410622fe21437
Xavier <xavierelon93@gmail.com>
2024-11-24 13:20:00 -0500

    Time: 5 ms (23.98%) | Memory: 55.2 MB (73.56%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.ts
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.ts
@@ -6,7 +6,7 @@ function minSubArrayLen(target: number, nums: number[]): number {
     for (let right = 0; right < nums.length; right++) {
         sum += nums[right]
 
-        while (sum >= target) {
+        while(sum >= target) {
             minLength = Math.min(minLength, right - left + 1)
             sum -= nums[left++]
         }
