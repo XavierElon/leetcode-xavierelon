03826637269f8ad092a4acc55a9e1355514ba0a6
Xavier <xavierelon93@gmail.com>
2024-11-25 11:09:04 -0500

    Time: 308 ms (5.00%) | Memory: 59.5 MB (5.09%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,11 +1,16 @@
 function missingNumber(nums: number[]): number {
-    const set: Set<number> = new Set(nums)
-    let expectedNumCount: number = nums.length + 1
+    let n = nums.length
+    if (n === 0) return 0
+    nums.sort((a, b) => a - b)
+    console.log(nums)
+    if (nums[0] !== 0) return 0
+    let prev = nums[0] 
     
-    for (let i = 0; i < expectedNumCount; i++) {
-        if (!set.has(i)) {
-            return i
-        }
+    for (let i = 1; i < nums.length; i++) {
+        console.log(prev)
+        console.log(nums[i])
+        if (nums[i] !== prev + 1) return prev + 1
+        prev = nums[i]
     }
-    return -1
+    return n
 }; 
