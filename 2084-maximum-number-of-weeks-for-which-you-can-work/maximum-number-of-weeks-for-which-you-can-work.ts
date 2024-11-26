7acb2e3fcc69987efc6590eca733eaadb0ad3beb
Xavier <xavierelon93@gmail.com>
2024-11-25 19:55:21 -0500

    Time: 10 ms (100.00%) | Memory: 63.3 MB (50.00%) - LeetSync

--- 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.ts
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.ts
@@ -1,13 +1,9 @@
 function numberOfWeeks(milestones: number[]): number {
-    let numberOfWeeks: number = 0
-    let maxMilestones: number = Math.max(...milestones)
-    
-    milestones.sort((a, b) => b - a)
-    let sumOthers: number = milestones.reduce((acc, curr) => acc + curr, 0)
-    sumOthers -= milestones[0]
-
-    if (maxMilestones > sumOthers + 1) numberOfWeeks = sumOthers * 2 + 1
-    else numberOfWeeks = sumOthers += milestones[0]
-    
-    return numberOfWeeks
+    let maxNumberOfWeeks = 0
+    let maxItem = -Infinity
+    milestones.forEach((milestone) => {
+        maxNumberOfWeeks += milestone
+        maxItem = Math.max(maxItem, milestone)
+    })
+    return Math.min(maxNumberOfWeeks, (maxNumberOfWeeks - maxItem) * 2 + 1)
 };
