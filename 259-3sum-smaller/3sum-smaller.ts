3cf036fdecad380f49b9952fa3cc1e0e69032cd2
Xavier <xavierelon93@gmail.com>
2024-11-28 16:51:12 -0500

    Time: 21 ms (71.43%) | Memory: 52.1 MB (44.00%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.ts
+++ 259-3sum-smaller/3sum-smaller.ts
@@ -1,24 +1,21 @@
 function threeSumSmaller(nums: number[], target: number): number {
     let count: number = 0
-    const n: number = nums.length
-
     nums.sort((a, b) => a - b)
-
-    for (let i = 0; i < n - 2; i++) {
+ 
+    for (let i = 0; i < nums.length - 2; i++) {
         let left: number = i + 1
-        let right = n - 1
+        let right: number = nums.length - 1
 
         while (left < right) {
             const sum: number = nums[i] + nums[left] + nums[right]
 
             if (sum < target) {
-                count += right - left
-                left++
+                 count += right - left
+                 left++
             } else {
                 right--
             }
         }
     }
-
     return count
 };
