508227982eb945e527e7569f506bf99fbf3ebe6c
Xavier <xavierelon93@gmail.com>
2024-12-01 12:37:18 -0500

    Time: 0 ms (100.00%) | Memory: 49.5 MB (51.74%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return ""
+
+    let prefix = strs[0]
+
+    for (let i = 1; i < strs.length; i++) {
+        const currentString = strs[i]
+
+        while (currentString.indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+            if (prefix === '') return ''
+        }
+    }
+
+    return prefix
+};
