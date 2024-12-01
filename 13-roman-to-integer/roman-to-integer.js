61d8a4095ce9a6aba029f080511844cf4fd7c77b
Xavier <xavierelon93@gmail.com>
2024-12-01 13:21:15 -0500

    Time: 8 ms (43.12%) | Memory: 55 MB (62.06%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let sum = 0
+    
+    for (let i = 0; i < s.length; i++) {
+        if (i > 0 && s.charAt(i-1) === 'I' && (s.charAt(i) === 'V' || s.charAt(i) === 'X')) {
+            sum -= 2
+        }
+        if (i > 0 && s.charAt(i-1) === 'X' && (s.charAt(i) === 'L' || s.charAt(i) === 'C')) {
+            sum -= 20
+        }
+        if (i > 0 && s.charAt(i-1) === 'C' && (s.charAt(i) === 'D' || s.charAt(i) === 'M')) {
+            sum -= 200
+        }
+        sum += map[s.charAt(i)]
+    }
+    return sum
+};
