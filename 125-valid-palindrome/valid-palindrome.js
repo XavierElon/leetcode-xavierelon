4cededb14ff81f55971a7c60197b65ca5b61dab9
Xavier <xavierelon93@gmail.com>
2024-12-01 18:15:43 -0500

    Time: 4 ms (73.06%) | Memory: 53.6 MB (38.03%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,18 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase().split('').filter(char => (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')).join('')
-
-    let left = 0
-    let right = s.length - 1
-
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            return false
-        }
-        left++
-        right--
+    const filterNonAlphabet = (str) => {
+        return str.replace(/[^0-9a-zA-Z]/g, '')
     }
 
-    return true
+    s = s.trim().toLowerCase()
+
+    s = filterNonAlphabet(s)
+
+    let reversedStr = s.split('').reverse().join('')
+
+    return reversedStr === s
 };
