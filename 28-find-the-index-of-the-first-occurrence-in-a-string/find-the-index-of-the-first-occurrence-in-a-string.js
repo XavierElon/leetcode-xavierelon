a2a20712a7996e8a568a5d59be25721ec8f23ec6
Xavier <xavierelon93@gmail.com>
2024-12-01 14:16:08 -0500

    Time: 0 ms (100.00%) | Memory: 49.8 MB (11.01%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,20 +4,19 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (needle == '' || haystack == '') return 0
     if (haystack.length < needle.length) return -1
+    if (haystack.length === '' || needle.length === '') return 0
 
-    for (let windowStart = 0; windowStart <  haystack.length - needle.length + 1; windowStart++) {
+    for (let windowStart = 0; windowStart < haystack.length - needle.length + 1; windowStart++) {
         if (haystack[windowStart] === needle[0]) {
             for (let i = 0; i < needle.length; i++) {
                 if (needle[i] !== haystack[windowStart + i]) {
                     break
-                } else if (i === needle.length -1) {
+                } else if (i === needle.length - 1) {
                     return windowStart
                 }
             }
         }
     }
     return -1
-
-}
+};
