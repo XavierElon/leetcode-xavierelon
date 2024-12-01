abf203463a21ca6f3ef516bc8aa4edf679b55fbd
Xavier <xavierelon93@gmail.com>
2024-12-01 14:48:38 -0500

    Time: 0 ms (100.00%) | Memory: 49.7 MB (44.14%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -5,12 +5,12 @@
 var rotate = function(matrix) {
     const n = matrix.length
 
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i + 1; j < matrix.length; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
     }
-    
+
     for (let i = 0; i < n; i++) {
         matrix[i].reverse()
     }
