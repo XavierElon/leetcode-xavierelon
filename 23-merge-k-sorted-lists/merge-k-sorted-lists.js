61534d6df389568348e15762b3e625ad8da32441
Xavier <xavierelon93@gmail.com>
2024-12-02 14:06:46 -0500

    Time: 11 ms (66.73%) | Memory: 55.2 MB (87.52%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -9,39 +9,42 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-function mergeLists(a, b) {
-    const dummy = new ListNode(0);
-    let temp = dummy;
-     while (a !== null && b !== null) {
-         if (a.val < b.val) {
-             temp.next = a;
-             a = a.next;
-         } else {
-             temp.next = b;
-             b = b.next;
-         }
-         temp = temp.next;
-     }
-    if (a !== null) {
-        temp.next = a;
-    }
-    if (b !== null) {
-        temp.next = b;
-    }
-    return dummy.next;
-}
-
 var mergeKLists = function(lists) {
-    if (lists.length === 0 ) {
-        return null;
+    if (lists.length === 0) {
+        return null
     }
-    // two two
-    // priority queue
+
     while (lists.length > 1) {
-        let a = lists.shift(); // the head will contains the "less" length list
-        let b = lists.shift(); // acturally, we can use the linkedlist to replace it, the while loop will be the while( list.header.next !== null || lists.length > 0)
-        const h = mergeLists(a, b);
-        lists.push(h);
+        let l1 = lists.shift()
+        let l2 = lists.shift()
+        let merged = mergeLists(l1, l2)
+        lists.push(merged)
     }
-    return lists[0];
+    return lists[0]
 };
+
+function mergeLists(l1, l2) {
+    const dummy = new ListNode(0)
+    let current = dummy
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            current.next = l1
+            l1 = l1.next
+        } else {
+            current.next = l2
+            l2 = l2.next
+        }
+        current = current.next
+    }
+
+    if (l1) {
+        current.next = l1
+    }
+
+    if (l2) {
+        current.next = l2
+    }
+
+    return dummy.next
+}
