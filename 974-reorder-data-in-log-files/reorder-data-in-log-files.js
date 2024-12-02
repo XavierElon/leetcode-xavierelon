e72b6e5a87b07b1e7e72deb94931fee7f78406f5
Xavier <xavierelon93@gmail.com>
2024-12-01 19:19:11 -0500

    Time: 9 ms (49.07%) | Memory: 53.5 MB (60.07%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.js
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.js
@@ -3,29 +3,40 @@
  * @return {string[]}
  */
 var reorderLogFiles = function(logs) {
-    const textLogs = []
+    const letterLogs = []
     const digitLogs = []
 
+    const isDigit = (char) => /\d/.test(char)
+
     for (const log of logs) {
-        const [id, ...rest] = log.split(' ')
-        if (isNaN(Number(rest[0]))) {
-            textLogs.push([id, rest.join(' ')])
-        } else {
+        const spaceIndex = log.indexOf(' ')
+        const identifier = log.substring(0, spaceIndex)
+        const content = log.substring(spaceIndex + 1)
+
+        if (isDigit(content[0])) {
             digitLogs.push(log)
+        } else {
+            letterLogs.push(log)
         }
     }
 
-    textLogs.sort((a, b) => {
-        if (a[1] < b[1]) return -1
-        if (a[1] > b[1]) return 1
-        if (a[1] === b[1]) {
-            if (a[0] < b[0]) return -1
-            if (a[0] > b[0]) return 1
-            return 0
+    letterLogs.sort((logA, logB) => {
+        const spaceIndexA = logA.indexOf(' ')
+        const idA = logA.substring(0, spaceIndexA)
+        const contentA = logA.substring(spaceIndexA + 1)
+
+        const spaceIndexB = logB.indexOf(' ')
+        const idB = logB.substring(0, spaceIndexB)
+        const contentB = logB.substring(spaceIndexB + 1)
+
+        const contentComparison = contentA.localeCompare(contentB)
+
+        if (contentComparison !== 0) {
+            return contentComparison
+        } else {
+            return idA.localeCompare(idB)
         }
-        return 0
     })
 
-    const texts = textLogs.map(log => log.join(' '))
-    return [...texts, ...digitLogs]
+    return [...letterLogs, ...digitLogs]
 };
