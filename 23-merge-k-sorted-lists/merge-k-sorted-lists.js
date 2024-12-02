9f2594ebb42956270de3e489375a1be2fbd1914e
Xavier <xavierelon93@gmail.com>
2024-12-02 13:55:19 -0500

    Time: 11 ms (66.73%) | Memory: 55.2 MB (89.77%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -9,33 +9,39 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
+function mergeLists(a, b) {
+    const dummy = new ListNode(0);
+    let temp = dummy;
+     while (a !== null && b !== null) {
+         if (a.val < b.val) {
+             temp.next = a;
+             a = a.next;
+         } else {
+             temp.next = b;
+             b = b.next;
+         }
+         temp = temp.next;
+     }
+    if (a !== null) {
+        temp.next = a;
+    }
+    if (b !== null) {
+        temp.next = b;
+    }
+    return dummy.next;
+}
+
 var mergeKLists = function(lists) {
-    return helper(lists, 0, lists.length-1)
+    if (lists.length === 0 ) {
+        return null;
+    }
+    // two two
+    // priority queue
+    while (lists.length > 1) {
+        let a = lists.shift(); // the head will contains the "less" length list
+        let b = lists.shift(); // acturally, we can use the linkedlist to replace it, the while loop will be the while( list.header.next !== null || lists.length > 0)
+        const h = mergeLists(a, b);
+        lists.push(h);
+    }
+    return lists[0];
 };
-
-function helper(lists, start, end) {
-    if (start === end) {
-        return lists[start]
-    } else if (start < end) {
-        const mid = parseInt((start + end) / 2)
-        const left = helper(lists, start, mid)
-        const right = helper(lists, mid + 1, end)
-        return merge(left, right)
-    } else {
-        return null
-    }
-}
-
-function merge (left, right) {
-    if (!left) {
-        return right
-    } else if (!right) {
-        return left
-    } else if (left.val < right.val) {
-        left.next = merge(left.next, right)
-        return left
-    } else {
-        right.next = merge(left, right.next)
-        return right
-    }
-}
