c3d221c909d20d984d8f0870a27d2ae9ecf7a8d4
Xavier <xavierelon93@gmail.com>
2024-12-03 11:21:28 -0500

    Time: 5 ms (30.56%) | Memory: 50.5 MB (91.84%) - LeetSync

--- 1149-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.js
+++ 1149-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.js
@@ -5,29 +5,31 @@
  * @return {number[]}
  */
 var arraysIntersection = function(arr1, arr2, arr3) {
-    const intersection = []
-    let pointer1 = 0
-    let pointer2 = 0
-    let pointer3 = 0
+    const result = []
 
-    while (pointer1 < arr1.length && pointer2 < arr2.length && pointer3 < arr3.length) {
-        const num1 = arr1[pointer1]
-        const num2 = arr2[pointer2]
-        const num3 = arr3[pointer3]
-
-        if (num1 === num2 && num2 === num3) {
-            intersection.push(num1)
-            pointer1++
-            pointer2++
-            pointer3++
-        } else {
-            const minNum = Math.min(num1, num2, num3)
-
-            if (num1 === minNum) pointer1++
-            if (num2 === minNum) pointer2++
-            if (num3 === minNum) pointer3++
+    for (let num of arr1) {
+        if (binarySearch(arr2, num)) {
+            if (binarySearch(arr3, num)) {
+                result.push(num)
+            }
         }
     }
-
-    return intersection.sort((a, b) => a - b)
+    return result
 };
+
+function binarySearch(arr, target) {
+    let left = 0
+    let right = arr.length - 1
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (arr[mid] === target) {
+            return true
+        } else if (arr[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+}
