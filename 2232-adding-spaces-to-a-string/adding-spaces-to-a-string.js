71199b347f6325db835f86106e92a8037618a4cc
Xavier <xavierelon93@gmail.com>
2024-12-03 10:36:04 -0500

    Time: 109 ms (54.54%) | Memory: 89.2 MB (56.45%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
@@ -4,18 +4,18 @@
  * @return {string}
  */
 var addSpaces = function(s, spaces) {
-    let res = []
+    let ans = ''
     let spacesIndex = 0
     let spacesLength = spaces.length
     let sIndex = 0
 
     for (let i = 0; i < s.length; i++) {
         if (spacesIndex < spacesLength && spaces[spacesIndex] === i) {
-            res.push(' ')
+            ans += ' '
             spacesIndex++
         } 
-        res.push(s[i])
+        ans += s[i]
     }
 
-    return res.join('')
+    return ans
 };
