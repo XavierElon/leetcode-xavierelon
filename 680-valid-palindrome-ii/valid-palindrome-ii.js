83775652c1354f9bdd3099008e6ef1511a2e77a8
Xavier <xavierelon93@gmail.com>
2024-12-02 19:06:02 -0500

    Time: 7 ms (75.10%) | Memory: 57.1 MB (44.22%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,28 +3,26 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    function isPalindrome(s, left, right) {
-        while (left < right) {
-            if (s[left] !== s[right]) {
-                return false
-            }
-            left++
-            right--
-        }
-        return true
-    }
-
     let left = 0
     let right = s.length - 1
 
     while (left < right) {
         if (s[left] !== s[right]) {
-            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+            return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1)
         }
-
         left++
         right--
     }
-
     return true
 };
+
+function isPalindrome(s, left, right) {
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
