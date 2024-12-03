277151466e163945849580baceb87368e55d87f8
Xavier <xavierelon93@gmail.com>
2024-12-02 19:52:41 -0500

    Time: 7 ms (43.37%) | Memory: 52.9 MB (41.93%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const rows = Array(9).fill().map(() => new Set())
-    const cols = Array(9).fill().map(() => new Set())
-    const boxes = Array(9).fill().map(() => new Set())
+    const rows = new Array(9).fill().map(() => new Set())
+    const cols = new Array(9).fill().map(() => new Set())
+    const boxes = new Array(9).fill().map(() => new Set())
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
@@ -14,12 +14,16 @@ var isValidSudoku = function(board) {
 
             const boxIndex = Math.floor(i/3) * 3 + Math.floor(j/3)
 
-            if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) return false
+
+            if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) {
+                return false
+            }
 
             rows[i].add(num)
             cols[j].add(num)
             boxes[boxIndex].add(num)
         }
     }
+
     return true
 };
