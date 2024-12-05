75999f9d7691275aa6ea2438b8790d529a2e9b99
Xavier <xavierelon93@gmail.com>
2024-12-05 15:11:22 -0500

    Time: 15 ms (51.02%) | Memory: 56.1 MB (45.45%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.js
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.js
@@ -4,30 +4,28 @@
  * @return {boolean}
  */
 var canMakeSubsequence = function(str1, str2) {
-    if (str2.length === 0) return true
     if (str1.length === 0) return false
-
+    if (str2.length === 0) return true
+    const m = str1.length
+    const n = str2.length
     let i = 0
     let j = 0
 
-    const n = str1.length
-    const m = str2.length
-
-    const incrementChar = (c) => {
+    const getNextChar = (c) => {
         return c === 'z' ? 'a' : String.fromCharCode(c.charCodeAt(0) + 1)
     }
 
-    while (i < n && j < m) {
+    while (i < m && j < n) {
         if (str1[i] === str2[j]) {
             j++
         } else {
-            const incrementedChar = incrementChar(str1[i])
-            if (incrementedChar === str2[j]) {
+            const nextChar = getNextChar(str1[i])
+            if (nextChar === str2[j]) {
                 j++
             }
         }
         i++
     }
 
-    return j === m
+    return j === n
 };
