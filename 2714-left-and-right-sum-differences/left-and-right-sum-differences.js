35f5433cae03c48799430fe41404d1658475694f
Xavier <xavierelon93@gmail.com>
2024-12-05 16:23:34 -0500

    Time: 2 ms (75.10%) | Memory: 52.7 MB (52.01%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.js
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.js
@@ -4,15 +4,15 @@
  */
 var leftRightDifference = function(nums) {
     const n = nums.length
-    let leftSum = new Array(n).fill(0)
-    let rightSum = new Array(n).fill(0)
-    let differences = new Array(n).fill(0)
+    const leftSum = new Array(n).fill(0)
+    const rightSum = new Array(n).fill(0)
+    const differences = new Array(n).fill(0)
 
     for (let i = 1; i < nums.length; i++) {
         leftSum[i] = leftSum[i-1] + nums[i-1]
     }
 
-    for (let i = nums.length - 2; i>= 0; i--) {
+    for (let i = nums.length - 2; i >= 0; i--) {
         rightSum[i] = rightSum[i+1] + nums[i+1]
     }
 
