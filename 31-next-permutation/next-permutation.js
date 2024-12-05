781f2042d45fda5e059a5115bc3a0c8864592cd9
Xavier <xavierelon93@gmail.com>
2024-12-05 18:46:45 -0500

    Time: 0 ms (100.00%) | Memory: 51.8 MB (34.35%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -3,10 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    for (let i = nums.length -1; i >= 0; i--) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] < nums[i+1]) {
-            const large = nextLarge(i)
-            swap(i, large)
+            const nextLargest = getNextLargest(i)
+            swap(i, nextLargest)
             reverse(i+1)
             return
         }
@@ -19,7 +19,8 @@ var nextPermutation = function(nums) {
     }
 
     function reverse(idx) {
-        let start = idx, end = nums.length - 1
+        let start = idx
+        let end = nums.length - 1
 
         while (start < end) {
             swap(start, end)
@@ -28,9 +29,11 @@ var nextPermutation = function(nums) {
         }
     }
 
-    function nextLarge(idx) {
-        for (let i = nums.length - 1; i > idx; i--) {
-            if (nums[i] > nums[idx]) return i
+    function getNextLargest(idx) {
+        for (let i = nums.length -1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                return i
+            }
         }
     }
 };
