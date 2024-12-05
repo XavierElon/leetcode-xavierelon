35abbb8a624de1e3967f8d54562f008491c9d649
Xavier <xavierelon93@gmail.com>
2024-12-05 15:04:14 -0500

    Time: 23 ms (95.24%) | Memory: 58.2 MB (85.71%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} start
+ * @param {string} target
+ * @return {boolean}
+ */
+var canChange = function(start, target) {
+    let i = 0
+    let j = 0
+    const n = start.length
+
+    while (i < n || j < n) {
+        while (i < n && start[i] === '_') {
+            i++
+        }
+        while (j < n && target[j] === '_') {
+            j++
+        }
+
+        if (i === n || j === n) break
+
+        if (start[i] !== target[j]) {
+            return false
+        }
+
+        if (start[i] === 'L' && i < j) {
+            return false
+        }
+        if (start[i] === 'R' && i > j) {
+            return false
+        }
+        i++
+        j++
+    }
+    return i === n && j === n
+};
