66be14dd1b440c3be80e59e266381bc125423b6d
Xavier <xavierelon93@gmail.com>
2024-12-05 16:36:08 -0500

    Time: 46 ms (89.52%) | Memory: 57 MB (74.02%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.js
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.js
@@ -4,27 +4,46 @@
  * @return {number}
  */
 var numMatchingSubseq = function(s, words) {
-    let res = 0
-
+    // loop over each word in the words array
+    // for a word to be a possible substring
+    // of the relative order permutations of s
+    // each letter in the word must also appear
+    // sequentially, though not adjacently
+    
+    let matches = 0;
+    
     for (const word of words) {
-        let index = -1
-        let match = 0
+        let lastIndex = -1;
+        let match = 0;
+        
+        for (let i = 0; i < word.length; i++) {
+            const char = word.charAt(i);
+            // we can search for the position of the
+            // current char using the lastIndex observed
+            // as our starting point, which JS very
+            // conveniently provides as a parameter of indexOf
+            const currentIndex = s.indexOf(char, lastIndex + 1);
 
-        for (const c of word) {
-            const currentIndex = s.indexOf(c, index + 1)
-
-            if (currentIndex > index) {
-                match++
-                index = currentIndex
-                continue
+            // if the index of current char is greater than the last
+            // index we observed, then the sequence persists and
+            // we can continue
+            if (currentIndex > lastIndex) {
+                match++;
+                lastIndex = currentIndex;
+            } else {
+                // otherwise, no such instance of the current char
+                // exists after the last chars index
+                break;
             }
-            break
         }
-
-        if (match === word.length) {
-            res++
+        
+        // lastly, we know a valid subsequent is found if the
+        // number of local matches found is equal to the length
+        // of our word
+        if (match == word.length) {
+            matches++;
         }
     }
-
-    return res
+    
+    return matches;
 };
