b99f1a746e2e1f272a1ff9298ac11b149973fcd6
Xavier <xavierelon93@gmail.com>
2024-12-05 15:20:25 -0500

    Time: 30 ms (80.95%) | Memory: 58.9 MB (71.43%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.js
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.js
@@ -6,30 +6,22 @@
 var canChange = function(start, target) {
     let i = 0
     let j = 0
-    const n = start.length
+    const n = start.length 
 
     while (i < n || j < n) {
-        while (i < n && start[i] === '_') {
-            i++
-        }
-        while (j < n && target[j] === '_') {
-            j++
-        }
+        while (i < n && start[i] === '_') i++
+        while (j < n && target[j] === '_') j++
 
-        if (i === n || j === n) break
+        if (i === n || j ===n) break
 
-        if (start[i] !== target[j]) {
-            return false
-        }
+        if (start[i] !== target[j]) return false
 
-        if (start[i] === 'L' && i < j) {
-            return false
-        }
-        if (start[i] === 'R' && i > j) {
-            return false
-        }
+        if (start[i] === 'L' && i < j) return false
+        if (start[i] === 'R' && i > j) return false
+
         i++
         j++
     }
+
     return i === n && j === n
 };
