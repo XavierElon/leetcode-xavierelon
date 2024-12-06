dcf80e000ae0466d667f896300b7fe95e2b21f97
Xavier <xavierelon93@gmail.com>
2024-12-06 15:27:23 -0500

    Time: 8 ms (21.63%) | Memory: 52.5 MB (40.40%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -5,18 +5,20 @@
  */
 var nextGreaterElement = function(nums1, nums2) {
     const stack = []
-    const hashmap = {}
+    const map = {}
 
-    for (let num of nums2) {
-        while (stack.length > 0 && num > stack[stack.length - 1]) {
-            hashmap[stack.pop()] = num
+    for (const num of nums2) {
+        while (stack.length > 0 && num > stack[stack.length-1]) {
+            map[stack.pop()] = num
         }
         stack.push(num)
     }
 
     while (stack.length > 0) {
-        hashmap[stack.pop()] = -1
+        const num = stack.pop()
+        map[num] = -1
     }
 
-    return nums1.map(num => hashmap[num] !== undefined ? hashmap[num] : -1)
+    const ans = nums1.map(num => map[num] !== undefined ? map[num] : -1)
+    return ans
 };
