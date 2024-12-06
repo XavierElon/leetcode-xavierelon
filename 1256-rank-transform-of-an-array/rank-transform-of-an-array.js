ad3d3f97c33c4ae80989151dc24072a8eac9d40d
Xavier <xavierelon93@gmail.com>
2024-12-05 19:41:37 -0500

    Time: 42 ms (97.12%) | Memory: 72.2 MB (82.06%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.js
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.js
@@ -4,13 +4,12 @@
  */
 var arrayRankTransform = function(arr) {
     if (arr.length === 0) return []
-
-    const uniqueSorted = Array.from(new Set([...arr])).sort((a, b) => a - b)
+    const uniqueSet = new Set(arr)
+    const sortedUniqueArr = Array.from(uniqueSet).sort((a, b) => a - b)
 
     const rankMap = new Map()
-    uniqueSorted.forEach((num, index) => rankMap.set(num, index + 1))
+    sortedUniqueArr.forEach((num, index) => rankMap.set(num, index + 1))
 
-    const result = arr.map(num =>rankMap.get(num))
-    
-    return result
+    return arr.map(num => rankMap.get(num))
+
 };
