1cb5a004dd7f59a8978fbe0be08943ddbff62e58
Xavier <xavierelon93@gmail.com>
2024-12-06 16:53:44 -0500

    Time: 219 ms (36.84%) | Memory: 61.6 MB (47.83%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.js
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.js
@@ -5,18 +5,35 @@
  * @return {number}
  */
 var maxCount = function(banned, n, maxSum) {
-    const bannedSet = new Set(banned.filter(num => num <= n))
+    banned.sort((a, b) => a - b)
+
     let count = 0
     let sum = 0
 
-    for (let i = 1; i <= n; i++) {
-        if (!bannedSet.has(i)) {
-            sum += i
-            if (sum > maxSum) {
-                return count
+    const binarySearch = (target) => {
+        let left = 0
+        let right = banned.length - 1
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2)
+
+            if (banned[mid] === target) return true
+            if (banned[mid] < target) {
+                left = mid + 1
+            } else {
+                right = mid - 1
             }
+        }
+        return false
+    }
+
+    for (let i = 1; i <= n; i++) {
+        if (!binarySearch(i)) {
+            sum += i
+            if (sum > maxSum) break
             count++
         }
     }
+
     return count
 };
