ebe788ff19b414f935ce972051d2cf6433e5a67b
Xavier <xavierelon93@gmail.com>
2024-12-05 20:25:21 -0500

    Time: 15 ms (96.09%) | Memory: 70.9 MB (78.33%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -4,17 +4,17 @@
  */
 var dailyTemperatures = function(temperatures) {
     const n = temperatures.length
-    let hottest = 0
     const answer = new Array(n).fill(0)
+    let hottest = 0
 
     for (let curr_day = n - 1; curr_day >= 0; curr_day--) {
-        let current_temp = temperatures[curr_day]
+        const current_temp = temperatures[curr_day]
         if (current_temp >= hottest) {
             hottest = current_temp
             continue
         }
         let days = 1
-        while (current_temp >= temperatures[curr_day + days]) {
+        while (temperatures[curr_day] >= temperatures[curr_day + days]) {
             days += answer[curr_day + days]
         }
         answer[curr_day] = days
