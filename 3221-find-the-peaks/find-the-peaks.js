107f4c720e67eb5f783c1d07d3f1f544910c9e0b
Xavier <xavierelon93@gmail.com>
2024-12-08 16:12:06 -0500

    Time: 1 ms (65.69%) | Memory: 51.3 MB (86.52%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.js
+++ 3221-find-the-peaks/find-the-peaks.js
@@ -6,7 +6,7 @@ var findPeaks = function(mountain) {
     const res = []
 
     for (let i = 1; i < mountain.length - 1; i++) {
-        if ((mountain[i-1] < mountain[i]) && (mountain[i] > mountain[i+1])) res.push(i)
+        if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) res.push(i)
     }
 
     return res
