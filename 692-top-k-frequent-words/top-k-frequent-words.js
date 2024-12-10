a64a9fe03f51e2a8f6323fdc3d8afadd0ef67c58
Xavier <xavierelon93@gmail.com>
2024-12-10 15:06:01 -0500

    Time: 18 ms (25.88%) | Memory: 55.4 MB (22.20%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.js
+++ 692-top-k-frequent-words/top-k-frequent-words.js
@@ -4,63 +4,62 @@
  * @return {string[]}
  */
 const topKFrequent = (words, k) => {
-    if(!words || words.length === 0) return [];
+    if (!words || words.length === 0) return []
+
+    const freq = getFrequencies(words)
+    const maxHeap = new Heap(compareMaxFunc)
     
-    const freq = getFrequencies(words);
-    const minHeap = new Heap(compareFunc);
-    
-    for(const word of freq.keys()) {
-        const tuple = [word, freq.get(word)];
-        minHeap.insert(tuple);
-        if(minHeap.size > k) minHeap.extract();
+    for (const word of freq.keys()) {
+        const tuple = [word, freq.get(word)]
+        maxHeap.insert(tuple)
     }
-    
-    const ans = new Array();
-    while(minHeap.size > 0) ans.unshift(minHeap.extract()[0]);
-          
-    return ans;
+
+    const ans = new Array()
+    for (let i = 0; i < k && maxHeap.size > 0; i++) {
+        ans.push(maxHeap.extract()[0])
+    }
+
+    return ans
 };
 
-const compareFunc = (a, b) => {
-    if(a[1] === b[1]) {
-        if(a[0] === b[0]) return 0;
-        if(a[0] < b[0]) return 1;
-        return -1;
+const compareMaxFunc = (a, b) => {
+    if(a[1] !== b[1]) {
+        // Sort by frequency in descending order
+        return b[1] - a[1];
     }
-    
-    return a[1] - b[1];
+    // If frequencies are equal, sort lexicographically in ascending order
+    return a[0].localeCompare(b[0]);
 };
 
 const getFrequencies = (words) => {
-    const freq = new Map();
+    const freq = new Map()
     words.forEach(word => {
-        if(!freq.has(word)) freq.set(word, 0);
-        freq.set(word, freq.get(word) + 1);
-    });
-    
-    return freq;
-};
+        if (!freq.has(word)) freq.set(word, 0)
+        freq.set(word, freq.get(word) + 1)
+    })
+    return freq
+}
 
 class Heap {
     constructor(compareFunc) {
-        this.compareFunc = compareFunc;
-        this.heap = new Array();
+        this.compareFunc = compareFunc
+        this.heap = new Array()
     }
-    
+
     insert(val) {
-        this.heap.push(val);
-        this.heap.sort(this.compareFunc);
+        this.heap.push(val)
+        this.heap.sort(this.compareFunc)
     }
-    
+
     extract() {
-        return this.heap.shift();
+        return this.heap.shift()
     }
-    
+
     peek() {
-        return this.heap[0];
+        return this.heap[0]
     }
-    
+
     get size() {
-        return this.heap.length;
+        return this.heap.length
     }
 }
