dfee4230b6ead50bff5c9fedee53ecfb97212d63
Xavier <xavierelon93@gmail.com>
2024-12-10 18:34:44 -0500

    Time: 21 ms (46.15%) | Memory: 59.1 MB (16.29%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -1,50 +1,25 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
 var mergeKLists = function(lists) {
-    if (lists.length === 0) {
-        return null
-    }
-
-    while (lists.length > 1) {
-        let l1 = lists.shift()
-        let l2 = lists.shift()
-        let merged = mergeLists(l1, l2)
-        lists.push(merged)
-    }
-    return lists[0]
-};
-
-function mergeLists(l1, l2) {
-    const dummy = new ListNode(0)
-    let current = dummy
-
-    while (l1 && l2) {
-        if (l1.val < l2.val) {
-            current.next = l1
-            l1 = l1.next
-        } else {
-            current.next = l2
-            l2 = l2.next
+	// use MinPriorityQueue class from the datastructures-js library that is available in the LeetCode runtime
+    const minHeap = new MinPriorityQueue({ priority: x => x.val });
+    
+    // push head of each list into heap
+    for (const head of lists) {
+        if (head) {
+            minHeap.enqueue(head);
         }
-        current = current.next
     }
-
-    if (l1) {
-        current.next = l1
+    
+	// create head as dummy node
+    let head = new ListNode();
+    let tail = head;
+    
+    while (!minHeap.isEmpty()) {
+        const node = minHeap.dequeue().element;
+        tail.next = node;
+        tail = tail.next;
+        if (node.next) {
+            minHeap.enqueue(node.next);
+        }
     }
-
-    if (l2) {
-        current.next = l2
-    }
-
-    return dummy.next
-}
+    return head.next;
+};
