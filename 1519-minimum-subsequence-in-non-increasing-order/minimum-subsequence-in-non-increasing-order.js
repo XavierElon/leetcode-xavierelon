0b0cc410816348603f2f22d2b7e5a231223ebbd1
Xavier <xavierelon93@gmail.com>
2024-12-10 13:13:03 -0500

    Time: 4 ms (91.67%) | Memory: 51.9 MB (63.38%) - LeetSync

--- 1519-minimum-subsequence-in-non-increasing-order/minimum-subsequence-in-non-increasing-order.js
+++ 1519-minimum-subsequence-in-non-increasing-order/minimum-subsequence-in-non-increasing-order.js
@@ -2,24 +2,19 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var minSubsequence = function(nums) {
-    const res = []
-
-    nums.sort((a, b) => b - a)
-
-    let sum = 0
-    nums.forEach(number => sum += number)
-
-    let current_sum = 0
-
-    for (let i = 0; i < nums.length; i++) {
-        current_sum += nums[i]
-        res.push(nums[i])
-
-        if (current_sum > sum - current_sum) {
-            break;
-        }
-    }
-
-    return res
+const minSubsequence = (nums) => {
+    // Sort the array in descending order
+    nums.sort((a, b) => b - a);
+    
+    // Calculate the total sum of the array
+    const total = nums.reduce((sum, num) => sum + num, 0);
+    
+    // Initialize the current sum
+    let currentSum = 0;
+    
+    // Find the index where currentSum exceeds half of total
+    const index = nums.findIndex(num => (currentSum += num) > total / 2);
+    
+    // Return the subsequence up to the found index
+    return index !== -1 ? nums.slice(0, index + 1) : nums;
 };
