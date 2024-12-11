fad83bc4b5c8e771c25a71f38c13092fdf714730
Xavier <xavierelon93@gmail.com>
2024-12-11 17:55:22 -0500

    Time: 129 ms (19.17%) | Memory: 71.5 MB (8.93%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -5,13 +5,19 @@
  */
 var kClosest = function(points, k) {
     const res = []
-    const minHeap = new MinPriorityQueue()
+    const maxHeap = new MaxPriorityQueue()
 
     points.forEach(p => {
         const distanceFromOrigin = Math.sqrt(p[0] * p[0] + p[1] ** 2)
-        minHeap.enqueue(p, distanceFromOrigin)
+        maxHeap.enqueue(p, distanceFromOrigin)
+        if (maxHeap.size() > k) {
+            maxHeap.dequeue()
+        }
     })
 
-    while (res.length < k) res.push(minHeap.dequeue().element)
+    while (maxHeap.size() > 0) {
+        res.push(maxHeap.dequeue().element)
+    }
+
     return res
 };
