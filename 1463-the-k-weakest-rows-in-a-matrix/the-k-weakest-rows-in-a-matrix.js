d4bd2bd30423c05520fc80e0256480a80d68e535
Xavier <xavierelon93@gmail.com>
2024-12-11 12:02:25 -0500

    Time: 6 ms (20.67%) | Memory: 52.8 MB (10.37%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.js
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.js
@@ -4,21 +4,20 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    const soldiers = mat.map(row => row.reduce((sum, val) => sum + val, 0))
+    const soldiers = mat.map(row => row.reduce((acc, val) => acc + val, 0))
 
-    const minHeap = new MinPriorityQueue()
+    const maxHeap = new MaxPriorityQueue()
 
     for (let i = 0; i < mat.length; i++) {
         const priority = soldiers[i] * mat.length + i
+        maxHeap.enqueue(i, priority)
 
-        minHeap.enqueue(i, priority)
+        if (maxHeap.size() > k) maxHeap.dequeue()
     }
 
     const result = []
-
-    for (let i = 0; i < k; i++) {
-        result.push(minHeap.dequeue().element)
+    while (!maxHeap.isEmpty()) {
+        result.push(maxHeap.dequeue().element)
     }
-
-    return result
+    return result.reverse()
 };
