007cadcc9454ec78184738266ed76d1ad87281b5
Xavier <xavierelon93@gmail.com>
2024-12-10 19:20:46 -0500

    Time: 25 ms (19.93%) | Memory: 57.7 MB (16.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,19 +4,19 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    const freqMap = new Map()
-    for (const num of nums) {
-        freqMap.set(num, (freqMap.get(num) || 0) + 1)
+    const freq = new Map()
+    for (let num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1)
     }
 
-    const buckets = Array(nums.length + 1).fill().map(() => [])
-    for (const [num, freq] of freqMap) {
-        buckets[freq].push(num)
+    const maxHeap = new MaxPriorityQueue()
+    for (let [num, count] of freq) {
+        maxHeap.enqueue(num, count)
     }
 
     const result = []
-    for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
-        result.push(...buckets[i])
+    for (let i = 0; i < k; i++) {
+        result.push(maxHeap.dequeue().element)
     }
 
     return result
