1b7af8cf3d5a2c350646c92a68410be66c30da2b
Xavier <xavierelon93@gmail.com>
2024-12-11 13:13:44 -0500

    Time: 61 ms (9.82%) | Memory: 61.3 MB (15.45%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.js
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.js
@@ -4,14 +4,26 @@
  * @return {number}
  */
 var kthSmallest = function(matrix, k) {
-    const maxHeap = new MaxPriorityQueue()
+    // Provide a priority function that returns the value as the priority
+    const minHeap = new MinPriorityQueue({
+        priority: (entry) => entry.value
+    });
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            maxHeap.enqueue(matrix[i][j])
-            if (maxHeap.size() > k) maxHeap.dequeue()
+    const n = matrix.length
+
+    for (let r = 0; r < Math.min(k, n); r++) {
+        minHeap.enqueue({ value: matrix[r][0], r: r, c: 0 })
+    }
+
+    let element;
+    for (let i = 0; i < k; i++) {
+        element = minHeap.dequeue().element;
+        const { r, c } = element;
+
+        if (c < n - 1) {
+            minHeap.enqueue({ value: matrix[r][c+1], r: r, c: c+1 });
         }
     }
 
-    return maxHeap.front().element
+    return element.value; // element is an object, return element.value if needed.
 };
