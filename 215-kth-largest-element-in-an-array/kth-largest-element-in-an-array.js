d4421240fb95b8a21b765870919382cef8659a5b
Xavier <xavierelon93@gmail.com>
2024-12-10 19:16:14 -0500

    Time: 163 ms (18.95%) | Memory: 74.9 MB (6.72%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const maxHeap = new MaxPriorityQueue()
+
+    for (let num of nums) {
+        maxHeap.enqueue(num)
+    }
+
+    for (let i = 1; i < k; i++) {
+        maxHeap.dequeue()
+    }
+
+    return maxHeap.dequeue().element
+};
