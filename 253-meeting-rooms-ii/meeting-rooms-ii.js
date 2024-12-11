03963bda6177c86018b22491f2107f26c586208b
Xavier <xavierelon93@gmail.com>
2024-12-11 17:20:52 -0500

    Time: 31 ms (9.54%) | Memory: 56.4 MB (12.48%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,18 +3,21 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
+    if (intervals.length === 1) return 1
+
     intervals.sort((a, b) => a[0] - b[0])
     const minHeap = new MinPriorityQueue()
-
     let rooms = 0
 
-    for (const interval of intervals) {
-        if (minHeap.size() === 0) rooms++
-        else {
+    for (interval of intervals) {
+        if (minHeap.size() === 0) {
+            rooms++
+        } else {
             const front = minHeap.front().element
-            if (front <= interval[0]) {
-                minHeap.dequeue()
-            } else rooms++
+            if (front <= interval[0]) minHeap.dequeue()
+            else {
+                rooms++
+            }
         }
         minHeap.enqueue(interval[1])
     }
