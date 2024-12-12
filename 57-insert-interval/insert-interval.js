11fce484188351b2a1bb6b8fc9aa70311cf6e14d
Xavier <xavierelon93@gmail.com>
2024-12-12 15:16:59 -0500

    Time: 4 ms (69.12%) | Memory: 55 MB (24.19%) - LeetSync

--- 57-insert-interval/insert-interval.js
+++ 57-insert-interval/insert-interval.js
@@ -5,24 +5,22 @@
  */
 var insert = function(intervals, newInterval) {
     if (intervals.length === 0) return [newInterval]
+
     const merged = []
 
     let i = 0
-
     while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-        merged.push(intervals[i])
-        i++
+        merged.push(intervals[i++])
     }
 
     let mergedInterval = newInterval
-
-    while (i < intervals.length && intervals[i][0] <= mergedInterval[1]) {
-        mergedInterval = [Math.min(intervals[i][0], mergedInterval[0]),
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        mergedInterval = [
+            Math.min(intervals[i][0], mergedInterval[0]),
             Math.max(intervals[i][1], mergedInterval[1])
         ]
         i++
     }
-
     merged.push(mergedInterval)
 
     while (i < intervals.length) {
