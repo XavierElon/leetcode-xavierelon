35e6159ff0b38d9d2276ec4c6eccba24af3f1ca8
Xavier <xavierelon93@gmail.com>
2024-12-12 17:22:50 -0500

    Time: 22 ms (68.00%) | Memory: 49.9 MB (87.64%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.js
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.js
@@ -6,13 +6,14 @@ var subArrayRanges = function(nums) {
     let sum = 0
 
     for (let i = 0; i < nums.length; i++) {
-        let max = nums[i]
         let min = nums[i]
+        let max = nums[i]
 
         for (let j = i; j < nums.length; j++) {
             max = Math.max(max, nums[j])
             min = Math.min(min, nums[j])
             sum += max - min
+
         }
     }
 
