358a6b7fe43fa5956f3f4de158065a4b669cf2e9
Xavier <xavierelon93@gmail.com>
2024-12-12 14:28:32 -0500

    Time: 0 ms (100.00%) | Memory: 49.3 MB (31.46%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.js
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.js
@@ -4,7 +4,9 @@
  * @return {boolean}
  */
 var haveConflict = function(event1, event2) {
-    if (event1[1] < event2[0]) return false
-    if (event1[1] >= event2[0] && event1[0] <= event2[1]) return true
-    return false
+    if (event1[0] > event2[0]) {
+        [event1, event2] = [event2, event1]
+    }
+    if(event1[1] < event2[0]) return false;
+    return true;
 };
