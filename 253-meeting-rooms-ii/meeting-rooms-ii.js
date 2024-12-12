18113031259792e78af9ce71cd7eb97d375e9cc9
Xavier <xavierelon93@gmail.com>
2024-12-12 15:56:47 -0500

    Time: 5 ms (92.83%) | Memory: 52.2 MB (60.49%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/meeting-rooms-ii.js
@@ -5,15 +5,14 @@
 var minMeetingRooms = function(intervals) {
     if (intervals.length === 1) return 1
 
-    const starts = intervals.map(x => x[0])
-    const ends = intervals.map(x => x[1])
-
+    const starts = intervals.map(start => start[0])
+    const ends = intervals.map(end => end[1])
     starts.sort((a, b) => a - b)
     ends.sort((a, b) => a - b)
 
-    let endPtr = 0
     let rooms = 0
-
+    let endPtr = 0
+    
     for (let startPtr = 0; startPtr < starts.length; startPtr++) {
         if (starts[startPtr] >= ends[endPtr]) {
             rooms--
@@ -21,6 +20,5 @@ var minMeetingRooms = function(intervals) {
         }
         rooms++
     }
-
     return rooms
 };
