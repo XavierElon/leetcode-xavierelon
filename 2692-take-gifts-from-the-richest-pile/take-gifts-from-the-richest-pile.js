2bc79bb23a920527a2a65e7367090b69329ad029
Xavier <xavierelon93@gmail.com>
2024-12-12 11:35:31 -0500

    Time: 127 ms (15.25%) | Memory: 55.8 MB (7.23%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
@@ -7,15 +7,8 @@ var pickGifts = function(gifts, k) {
     let arr = [...gifts]
 
     for (let i = 0; i < k; i++) {
-        let maxIndex = 0
-
-        for (let j = 1; j < arr.length; j++) {
-            if (arr[j] > arr[maxIndex]) {
-                maxIndex = j
-            }
-        }
-
-        arr[maxIndex] = Math.floor(Math.sqrt(arr[maxIndex]))
+        arr.sort((a, b) => b - a)
+        arr[0] = Math.floor(Math.sqrt(arr[0]))
     }
 
     return arr.reduce((sum, val) => sum + val, 0)
