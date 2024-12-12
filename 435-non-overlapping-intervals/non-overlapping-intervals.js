51e708d59c9beb6c050ccd22f7182ab79b80acb2
Xavier <xavierelon93@gmail.com>
2024-12-12 15:32:26 -0500

    Time: 79 ms (59.02%) | Memory: 80.8 MB (33.30%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -6,8 +6,8 @@ var eraseOverlapIntervals = function(intervals) {
     if (intervals.length === 1) return 0
 
     intervals.sort((a, b) => a[0] - b[0])
-    let count = 0
     let prevEnd = intervals[0][1]
+    let count = 0
 
     for (let i = 1; i < intervals.length; i++) {
         if (prevEnd > intervals[i][0]) {
