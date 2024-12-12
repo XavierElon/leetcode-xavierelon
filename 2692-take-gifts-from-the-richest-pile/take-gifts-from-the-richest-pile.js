40f9f319a5e1a618e145482ac9ef9de65ffc8cb3
Xavier <xavierelon93@gmail.com>
2024-12-12 11:30:35 -0500

    Time: 32 ms (44.07%) | Memory: 55.4 MB (19.28%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
@@ -6,16 +6,13 @@
 var pickGifts = function(gifts, k) {
     const maxHeap = new MaxPriorityQueue()
 
-    for (const gift of gifts) {
-        maxHeap.enqueue(gift)
-    }
+    gifts.forEach(gift => maxHeap.enqueue(gift))
 
     for (let i = 0; i < k; i++) {
         const max = maxHeap.dequeue().element
-        const squareRoot = Math.floor(Math.sqrt(max))
-        maxHeap.enqueue(squareRoot)
+        maxHeap.enqueue(Math.floor(Math.sqrt(max)))
     }
-    
+
     let sum = 0
     while (maxHeap.size() > 0) {
         sum += maxHeap.dequeue().element
