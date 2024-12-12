12b803123703461e32f1d034d093eee5e6dfa6ce
Xavier <xavierelon93@gmail.com>
2024-12-12 17:14:19 -0500

    Time: 7 ms (76.67%) | Memory: 58.6 MB (69.09%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.js
+++ 303-range-sum-query-immutable/range-sum-query-immutable.js
@@ -5,9 +5,8 @@ var NumArray = function(nums) {
     this.prefixSum = new Array(nums.length).fill(0)
     this.prefixSum[0] = nums[0]
     for (let i = 1; i < nums.length; i++) {
-        this.prefixSum[i] = nums[i] + this.prefixSum[i-1]
+        this.prefixSum[i] = this.prefixSum[i-1] + nums[i]
     }
-    console.log(this.prefixSum)
 };
 
 /** 
@@ -16,8 +15,9 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(left, right) {
-    if (left === 0) return this.prefixSum[right]
-    else {
+    if (left === 0) {
+        return this.prefixSum[right]
+    } else {
         return this.prefixSum[right] - this.prefixSum[left - 1]
     }
 };
