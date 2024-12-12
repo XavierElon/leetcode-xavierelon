cac4969072dc0f3ea9d85fc6b220403afff42159
Xavier <xavierelon93@gmail.com>
2024-12-12 14:30:40 -0500

    Time: 5 ms (47.76%) | Memory: 51.3 MB (69.62%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.js
+++ 252-meeting-rooms/meeting-rooms.js
@@ -5,10 +5,10 @@
 var canAttendMeetings = function(intervals) {
     if (!intervals || intervals.length === 0) return true
 
-    let sorted = intervals.sort((a, b) => a[0] - b[0])
+    intervals.sort((a, b) => a[0] - b[0])
 
     for (let i = 0; i < intervals.length - 1; i++) {
-        if (sorted[i][1] > sorted[i+1][0]) return false
+        if (intervals[i][1] > intervals[i+1][0]) return false
     }
 
     return true
