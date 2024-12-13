4bdead36600bd169417ed682879f79bc838c8d16
Xavier <xavierelon93@gmail.com>
2024-12-13 18:17:42 -0500

    Time: 1 ms (47.48%) | Memory: 59.7 MB (5.07%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,16 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left = 0
-    let right = s.length - 1
+    function helper(left, right) {
+        if (left >= right) return
 
-    while (left < right) {
-        let temp = s[left]
-        s[left] = s[right]
-        s[right] = temp
-        left++
-        right--
+        [s[left], s[right]] = [s[right], s[left]]
+
+        return helper(left + 1, right - 1)
     }
 
-    return s
+    return helper(0, s.length - 1)
 };
