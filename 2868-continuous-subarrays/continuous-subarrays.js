d20a673a1177ff4ba8e14f8adb3b742b81d6fd70
Xavier <xavierelon93@gmail.com>
2024-12-14 18:54:12 -0500

    Time: 375 ms (10.53%) | Memory: 66.4 MB (47.37%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var continuousSubarrays = function(nums) {
+    let left = 0
+    let count = 0
+    let window = new Map()
+
+    for (let right = 0; right < nums.length; right++) {
+        window.set(nums[right], (window.get(nums[right]) || 0) + 1)
+
+        while (getMax(window) - getMin(window) > 2) {
+            window.set(nums[left], window.get(nums[left]) - 1)
+            if (window.get(nums[left]) === 0) {
+                window.delete(nums[left])
+            }
+            left++
+        }
+
+        count += right - left + 1
+    }
+
+    return count
+};
+
+function getMin(map) {
+    return Math.min(...map.keys())
+}
+
+function getMax(map) {
+    return Math.max(...map.keys())
+}
