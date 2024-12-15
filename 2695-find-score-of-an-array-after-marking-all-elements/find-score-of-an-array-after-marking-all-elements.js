2e7891f96eaf344220cff094428ef50f7a2dccbe
Xavier <xavierelon93@gmail.com>
2024-12-15 13:48:49 -0500

    Time: 302 ms (71.04%) | Memory: 81.7 MB (77.81%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.js
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.js
@@ -3,35 +3,32 @@
  * @return {number}
  */
 var findScore = function(nums) {
-    const n = nums.length;
-    let score = 0;
-    // Create queue with custom comparator for both value and index
+    const n = nums.length
+    let score = 0
     const pq = new MinPriorityQueue({
         compare: (a, b) => {
             if (a.value !== b.value) {
-                return a.value - b.value;
+                return a.value - b.value
             }
-            return a.index - b.index;
+            return a.index - b.index
         }
-    });
-    
-    // Add all elements to queue
+    })
+
     for (let i = 0; i < n; i++) {
-        pq.enqueue({ value: nums[i], index: i });
+        pq.enqueue({ value: nums[i], index: i })
     }
-    
-    let marked = new Array(n).fill(false);
-    
-    // Process queue until empty
+
+    let marked = new Array(n).fill(false)
+
     while (!pq.isEmpty()) {
-        const { value, index } = pq.dequeue();
+        const { value, index } = pq.dequeue()
         if (!marked[index]) {
-            score += value;
-            marked[index] = true;
-            if (index > 0) marked[index - 1] = true;
-            if (index < n - 1) marked[index + 1] = true;
+            score += value
+            marked[index] = true
+            if (index > 0) marked[index - 1] = true
+            if (index < n - 1) marked[index + 1] = true
         }
     }
-    
-    return score;
+
+    return score
 };
