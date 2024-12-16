ab0d277b80298184763a9b002605033fc095ca0f
Xavier <xavierelon93@gmail.com>
2024-12-16 14:28:37 -0500

    Time: 0 ms (100.00%) | Memory: 48.5 MB (78.66%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthGrammar = function(n, k) {
+    if (n === 1) {
+        return 0
+    } 
+
+    const parent = kthGrammar(n - 1, Math.ceil(k / 2))
+    const isOdd = k % 2 === 1
+
+    if (parent === 1) {
+        return isOdd ? 1 : 0
+    } else {
+        return isOdd ? 0 : 1
+    }
+};
