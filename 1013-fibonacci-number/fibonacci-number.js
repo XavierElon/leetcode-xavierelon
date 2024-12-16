2a339d4bd47d27b054145fd8cabd4df46f773e2e
Xavier <xavierelon93@gmail.com>
2024-12-15 20:55:02 -0500

    Time: 260 ms (5.06%) | Memory: 54.4 MB (7.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,9 +3,17 @@
  * @return {number}
  */
 var fib = function(n) {
+    const map = new Map()
+    if (map.has(n)) return map.get(n)
+
+    let result
+
     if (n < 2) {
-        return n
-    }   else {
-        return fib(n-1) + fib(n-2)
+        result = n
+    } else {
+        result = fib(n-1) + fib(n-2)
     }
+
+    map.set(n, result)
+    return result
 };
