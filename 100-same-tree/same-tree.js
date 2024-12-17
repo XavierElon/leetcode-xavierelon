66fb06760845214189eaa821be8ea5b5147865fb
Xavier <xavierelon93@gmail.com>
2024-12-16 19:20:15 -0500

    Time: 10 ms (100.00%) | Memory: 50.8 MB (5.45%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -11,10 +11,31 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    if (p === null && q === null) return true
-    if (p === null || q === null) return false
-    if (p.val !== q.val) return false
+var isSameTree = function(p, q) { 
+    const check = (p, q) => {
+        if (p === null && q === null) return true
+        if (p === null || q === null) return false
+        if (p.val !== q.val) return false
+        return true
+    }
+    
 
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    const queue1 = [p]
+    console.log(queue1)
+    const queue2 = [q]
+    console.log(queue2)
+
+    while (queue1.length || queue2.length) {
+        const p1 = queue1.shift()
+        const q1 = queue2.shift()
+
+        if (!check(p1, q1)) return false
+        if (p1) {
+            queue1.push(p1.left)
+            queue1.push(p1.right)
+            queue2.push(q1.left)
+            queue2.push(q1.right)
+        }
+    }
+    return true
 };
