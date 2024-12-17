5a97c008f4c36b2624221ed07a351af7fdf1951f
Xavier <xavierelon93@gmail.com>
2024-12-17 15:21:35 -0500

    Time: 5 ms (23.17%) | Memory: 54.4 MB (19.03%) - LeetSync

--- 46-permutations/permutations.js
+++ 46-permutations/permutations.js
@@ -3,42 +3,30 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const result = [];
-    
-    // If array has only one element, return it as a single permutation
-    if (nums.length === 1) {
-        return [[...nums]];
-    }
-    
-    // Backtracking function
-    function backtrack(currentPerm, remainingNums) {
-        // Base case: if no numbers left to add, we have a valid permutation
-        if (remainingNums.length === 0) {
-            result.push([...currentPerm]);
-            return;
+    const permutations = []
+
+    if (nums.length === 1) return [[...nums]]
+
+    const backtrack = (perm, remainingNums) => {
+        if (perm.length === nums.length) {
+            permutations.push([...perm])
+            return
         }
-        
-        // Try each remaining number as the next element
+
         for (let i = 0; i < remainingNums.length; i++) {
-            // Add current number to permutation
-            currentPerm.push(remainingNums[i]);
-            
-            // Create new array of remaining numbers (excluding current number)
-            const newRemaining = [
+            perm.push(remainingNums[i])
+
+            newNumbers = [
                 ...remainingNums.slice(0, i),
                 ...remainingNums.slice(i + 1)
-            ];
-            
-            // Recurse with updated arrays
-            backtrack(currentPerm, newRemaining);
-            
-            // Backtrack by removing the number we just added
-            currentPerm.pop();
+            ]
+
+            backtrack(perm, newNumbers)
+
+            perm.pop()
         }
     }
-    
-    // Start backtracking with empty permutation and all numbers available
-    backtrack([], nums);
-    
-    return result;
-}
+
+    backtrack([], nums)
+    return permutations
+};
