999fb49d6e18dfe812e67a5401860350cd94cb9d
Xavier <xavierelon93@gmail.com>
2024-12-17 13:16:52 -0500

    Time: 5 ms (84.62%) | Memory: 51.5 MB (95.65%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.js
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.js
@@ -3,40 +3,22 @@
  * @return {string[]}
  */
 var reorderLogFiles = function(logs) {
-    const letterLogs = []
-    const digitLogs = []
-
-    const isDigit = (char) => /\d/.test(char)
-
+    const digits = [],
+          letters = []
+    
     for (const log of logs) {
-        const spaceIndex = log.indexOf(' ')
-        const identifier = log.substring(0, spaceIndex)
-        const content = log.substring(spaceIndex + 1)
-
-        if (isDigit(content[0])) {
-            digitLogs.push(log)
-        } else {
-            letterLogs.push(log)
-        }
+        if (isFinite(log.split(' ')[1])) digits.push(log)  // digit log
+        else letters.push(log)  // letter log
     }
-
-    letterLogs.sort((logA, logB) => {
-        const spaceIndexA = logA.indexOf(' ')
-        const idA = logA.substring(0, spaceIndexA)
-        const contentA = logA.substring(spaceIndexA + 1)
-
-        const spaceIndexB = logB.indexOf(' ')
-        const idB = logB.substring(0, spaceIndexB)
-        const contentB = logB.substring(spaceIndexB + 1)
-
-        const contentComparison = contentA.localeCompare(contentB)
-
-        if (contentComparison !== 0) {
-            return contentComparison
-        } else {
-            return idA.localeCompare(idB)
-        }
+    
+	// Sorting the letters log based on condition of lexio order without and with key (example: 'let1')
+    letters.sort((a,b) => {
+        let l1 = a.split(' ').slice(1).join(' '),
+            l2 = b.split(' ').slice(1).join(' ')
+        
+        if (l1 === l2) return a > b ? 1 : -1
+        return l1 > l2 ? 1 : -1
     })
-
-    return [...letterLogs, ...digitLogs]
+    
+    return [...letters, ...digits]
 };
