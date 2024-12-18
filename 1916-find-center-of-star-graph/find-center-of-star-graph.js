7c4f27cf50cda84a070fb7ae83515e1ea1361419
Xavier <xavierelon93@gmail.com>
2024-12-18 14:17:17 -0500

    Time: 12 ms (32.66%) | Memory: 68.1 MB (31.66%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.js
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    const firstEdge = edges[0]
-    const secondEdge = edges[1]
+    const n = edges.length + 1
+    const degrees = new Array(n+1).fill(0)
 
-    if (secondEdge.includes(firstEdge[0])) {
-        return firstEdge[0]
-    } else {
-        return firstEdge[1]
+    for (const [u, v] of edges) {
+        degrees[u]++
+        degrees[v]++
     }
+
+    for (let i = 1; i <= n; i++) {
+        if (degrees[i] === n - 1) return i
+    } 
+
+    return -1
 };
