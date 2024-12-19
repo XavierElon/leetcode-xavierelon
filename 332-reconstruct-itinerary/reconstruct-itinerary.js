6d38666744a4cfd43019a85d93127b57df18a5cf
Xavier <xavierelon93@gmail.com>
2024-12-19 16:35:35 -0500

    Time: 4 ms (97.82%) | Memory: 53.9 MB (79.48%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.js
+++ 332-reconstruct-itinerary/reconstruct-itinerary.js
@@ -4,24 +4,21 @@
  */
 var findItinerary = function(tickets) {
     const graph = new Map()
-
     for (const [from, to] of tickets) {
-        if (!graph.has(from)) {
-            graph.set(from, [])
-        }
+        if (!graph.has(from)) graph.set(from, [])
         graph.get(from).push(to)
     }
 
-    for (const [airport, destinations] of graph) {
+    for (const [departures, destinations] of graph) {
         destinations.sort()
     }
 
     const result = []
 
     function dfs(airport) {
-        const destinations = graph.get(airport) || []
+        const destinations = (graph.get(airport) || [])
 
-        while (destinations.length > 0) {
+        while (destinations.length !== 0) {
             const next = destinations.shift()
             dfs(next)
         }
@@ -31,4 +28,4 @@ var findItinerary = function(tickets) {
 
     dfs('JFK')
     return result
-}
+};
