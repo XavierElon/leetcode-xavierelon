10e014321278d2592687dbbc1875003103b72d2c
Xavier <xavierelon93@gmail.com>
2024-12-19 15:04:11 -0500

    Time: 851 ms (43.13%) | Memory: 187.1 MB (32.49%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.js
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.js
@@ -7,25 +7,24 @@
  */
 var validPath = function(n, edges, source, destination) {
     if (source === destination) return true
-
     const graph = new Array(n).fill(null).map(() => [])
     for (let [u, v] of edges) {
         graph[u].push(v)
         graph[v].push(u)
     }
 
-    const visited = new Array(n).fill(false)
+    const visited = new Set()
 
     function dfs(node) {
         if (node === destination) return true
-        visited[node] = true
 
-        for (let neighbor of graph[node]) {
-            if (!visited[neighbor]) {
+        visited.add(node)
+
+        for (const neighbor of graph[node]) {
+            if (!visited.has(neighbor)) {
                 if (dfs(neighbor)) return true
             }
         }
-
         return false
     }
 
