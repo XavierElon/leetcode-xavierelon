0acb985bdacb4916c6a85446290521d87b28ac4d
Xavier <xavierelon93@gmail.com>
2024-12-20 14:08:53 -0500

    Time: 15 ms (33.74%) | Memory: 55.5 MB (61.53%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    const indegree = new Array(numCourses).fill(0)
+    const adjList = new Array(numCourses).fill(null).map(() => [])
+
+    for (const [course, prereq] of prerequisites) {
+        indegree[course]++
+        adjList[prereq].push(course)
+    }
+
+    const queue = []
+    const order = []
+    for (let i = 0; i < numCourses; i++) {
+        if (indegree[i] === 0) {
+            queue.push(i)
+            order.push(i)
+        }
+    }
+
+    while (queue.length > 0) {
+        const course = queue.shift()
+        for (const nextCourse of adjList[course]) {
+            indegree[nextCourse]--
+            if (indegree[nextCourse] === 0) {
+                queue.push(nextCourse)
+                order.push(nextCourse)
+            }
+        }
+    }
+
+    return order.length === numCourses ? order : []
+};
