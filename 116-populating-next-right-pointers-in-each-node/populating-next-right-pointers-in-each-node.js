835722730b5ace5b2e88736e6968d4f524829ce0
Xavier <xavierelon93@gmail.com>
2024-12-20 16:28:34 -0500

    Time: 68 ms (47.32%) | Memory: 54.8 MB (72.02%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+    if (!root) return null
+
+    const queue = [root]
+
+    while (queue.length > 0) {
+        const levelSize = queue.length
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift()
+
+            if (i < levelSize - 1) {
+                node.next = queue[0]
+            } else {
+                node.next = null
+            }
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+    }
+
+    return root
+};
