7912cd10ab0d1438bbc8e70aaf91aa0085e7debc
Xavier <xavierelon93@gmail.com>
2024-12-22 16:35:10 -0500

    Time: 153 ms (5.07%) | Memory: 130.2 MB (6.52%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,45 +12,17 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    if (!inorder.length || !postorder.length) return null;
+    if (inorder.length === 0) return null
 
-    // Build a hash map to store value -> its index relations
-    const inorderIndexMap = new Map();
-    for (let i = 0; i < inorder.length; i++) {
-        inorderIndexMap.set(inorder[i], i);
-    }
+    let rootVal = postorder[postorder.length - 1]
+    let root = new TreeNode(rootVal)
 
-    // Initialize the postorder index to the last element
-    let postIdx = postorder.length - 1;
+    let pivot = inorder.indexOf(rootVal)
 
-    /**
-     * Recursive helper function to build the tree.
-     * @param {number} inLeft - Left boundary in inorder array
-     * @param {number} inRight - Right boundary in inorder array
-     * @returns {TreeNode}
-     */
-    function helper(inLeft, inRight) {
-        // If there are no elements to construct the subtree
-        if (inLeft > inRight) return null;
+    console.log(postorder.slice(pivot, -1))
 
-        // Pick up postIdx element as a root
-        const rootVal = postorder[postIdx];
-        const root = new TreeNode(rootVal);
+    root.left = buildTree(inorder.slice(0, pivot), postorder.slice(0, pivot))
+    root.right = buildTree(inorder.slice(pivot + 1), postorder.slice(pivot, -1))
 
-        // Root splits inorder list into left and right subtrees
-        const inIdx = inorderIndexMap.get(rootVal);
-
-        // Decrement postIdx
-        postIdx--;
-
-        // Build right subtree first
-        root.right = helper(inIdx + 1, inRight);
-
-        // Then build left subtree
-        root.left = helper(inLeft, inIdx - 1);
-
-        return root;
-    }
-
-    return helper(0, inorder.length - 1);
+    return root
 };
