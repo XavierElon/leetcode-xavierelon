61199341c58ce6e013573bdeff32c33165f68dd1
Xavier <xavierelon93@gmail.com>
2024-12-23 12:15:41 -0500

    Time: 64 ms (76.57%) | Memory: 59.5 MB (44.53%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -12,14 +12,14 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (!root) return root
+    if (!root) return null
 
     if (root === p || root === q) return root
 
     const left = lowestCommonAncestor(root.left, p, q)
     const right = lowestCommonAncestor(root.right, p, q)
 
-    if (left !== null & right !== null) return root
+    if (left !== null && right !== null) return root
 
     return left !== null ? left : right
 };
