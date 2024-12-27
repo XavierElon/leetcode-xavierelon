e491713a937285af7991dd793092596dad943ad5
Xavier <xavierelon93@gmail.com>
2024-12-27 11:05:08 -0500

    Time: 4 ms (6.67%) | Memory: 57.1 MB (20.00%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.js
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var maxScoreSightseeingPair = function(values) {
-    let maxScore = -Infinity
-    let maxValue = values[0]
+    const n = values.length
+    const maxValues = new Array(n).fill(0)
 
-    for (let j = 1; j < values.length; j++) {
-        maxScore = Math.max(maxScore, maxValue + values[j] - j)
-        maxValue = Math.max(maxValue, values[j] + j)
+    maxValues[0] = values[0]
+    for (let i = 1; i < n; i++) {
+        maxValues[i] = Math.max(maxValues[i - 1], values[i] + i)
+    }
+
+    let maxScore = -Infinity
+    for (let j = 1; j < n; j++) {
+        maxScore = Math.max(maxScore, maxValues[j - 1] + values[j] - j)
     }
 
     return maxScore
