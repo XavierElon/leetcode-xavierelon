24b1587ee4a8e6b4598f2c817907992a197093c9
Xavier <xavierelon93@gmail.com>
2024-12-30 17:48:07 -0500

    Time: 0 ms (100.00%) | Memory: 50.5 MB (85.52%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.js
+++ 777-toeplitz-matrix/toeplitz-matrix.js
@@ -3,13 +3,13 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    for (let i = 1; i < matrix.length; i++) {
-        for (let j = 1; j < matrix[0].length; j++) {
-            if (matrix[i][j] !== matrix[i-1][j-1]) {
-                return false
+    // Iterate up to second-to-last row and column
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[0].length - 1; j++) {
+            if (matrix[i][j] !== matrix[i+1][j+1]) {
+                return false;
             }
         }
     }
-
-    return true
+    return true;
 };
