52be686c52693348f962f1c4730766bb69e8f06c
Xavier <xavierelon93@gmail.com>
2024-12-31 15:36:19 -0500

    Time: 0 ms (100.00%) | Memory: 52.2 MB (31.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,29 +11,15 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if (list1 === null && list2 === null) return null
-    if (list1 === null) return list2
-    if (list2 === null) return list1
-    
-    let dummyHead = new ListNode(-1)
-    let current = dummyHead
-
-    while (list1 && list2) {
-        if (list1.val <= list2.val) {
-            current.next = list1
-            list1 = list1.next
-        } else {
-            current.next = list2
-            list2 = list2.next
-        }
-        current = current.next
+    if (list1 === null) {
+        return list2
+    } else if (list2 === null) {
+        return list1
+    } else if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
     }
-    if (list1) {
-        current.next = list1
-    }
-    if (list2) {
-        current.next = list2
-    }
-    
-    return dummyHead.next
 };
