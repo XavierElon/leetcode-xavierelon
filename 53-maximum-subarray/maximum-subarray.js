4c0f9da15166e69d796b789498e58539bf5b2a67
Xavier <xavierelon93@gmail.com>
2024-12-31 10:49:04 -0500

    Time: 5 ms (14.64%) | Memory: 59.8 MB (7.75%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    const memo = {}
+    const n = nums.length
+    const dp = Array(n).fill(0)
 
-    const dp = (i) => {
-        if (i === 0) return nums[0]
+    dp[0] = nums[0]
+    let maxSubarray = dp[0]
 
-        if (i in memo) return memo[i]
-
-        memo[i] = Math.max(nums[i], dp(i-1) + nums[i])
-        return memo[i]
-    }
-
-    let maxSubarray = nums[0]
     for (let i = 1; i < nums.length; i++) {
-        const current = dp(i)
-        maxSubarray = Math.max(maxSubarray, current)
+        dp[i] = Math.max(nums[i], dp[i-1] + nums[i])
+        maxSubarray = Math.max(dp[i], maxSubarray)
     }
 
     return maxSubarray
