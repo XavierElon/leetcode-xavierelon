ff86add8aa3de70154ec058afe158050f10d0de9
Xavier <xavierelon93@gmail.com>
2024-12-31 15:41:31 -0500

    Time: 61 ms (74.59%) | Memory: 52.3 MB (94.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,13 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let seen = new Set()
-    let current = head
+    if (head === null ) return false
 
-    while (current !== null) {
-        if (seen.has(current)) return true
-        seen.add(current)
-        current = current.next
-    }
+    let fast = head
+    let slow = head
+
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+        if (fast === slow) {
+            return true
+        }
+    } 
     return false
 };
