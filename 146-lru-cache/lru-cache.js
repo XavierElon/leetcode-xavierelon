e03679de2a8e3e13304cf955159745a5949e8338
Xavier <xavierelon93@gmail.com>
2025-01-04 17:13:56 -0500

    Time: 113 ms (48.09%) | Memory: 106.9 MB (34.38%) - LeetSync

---
+++ 146-lru-cache/lru-cache.js
@@ -0,0 +1,93 @@
+class Node {
+    constructor(key, val) {
+        this.key = key
+        this.val = val
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(key, val) {
+        const newNode = new Node(key, val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return newNode
+    }
+
+    remove(node) {
+        if (!node.next && !node.prev) {
+            this.head = null
+            this.tail = null
+        } else if (!node.next) {
+            this.tail = node.prev
+            this.tail.next = null
+        } else if (!node.prev) {
+            this.head = node.next
+            this.head.prev = null
+        } else {
+            const prev = node.prev
+            const next = node.next
+            prev.next = next
+            next.prev = prev
+        }
+        this.length--
+    }
+}
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity
+    this.map =  {}
+    this.DLL = new DoublyLinkedList()
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.map[key]) return -1
+
+    const value = this.map[key].val
+    this.DLL.remove(this.map[key])
+    this.map[key] = this.DLL.push(key, value)
+    return value
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.map[key]) this.DLL.remove(this.map[key])
+    this.map[key] = this.DLL.push(key, value)
+    if (this.DLL.length > this.capacity) {
+        const currKey = this.DLL.head.key
+        delete this.map[currKey]
+        this.DLL.remove(this.DLL.head)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
