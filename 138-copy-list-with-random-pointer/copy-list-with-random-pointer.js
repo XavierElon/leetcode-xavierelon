78c861a91ef19778d2d3318088f1908176c72ab6
Xavier <xavierelon93@gmail.com>
2025-01-04 16:45:11 -0500

    Time: 52 ms (68.54%) | Memory: 50.7 MB (71.56%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.js
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.js
@@ -12,55 +12,38 @@
  * @return {_Node}
  */
 var copyRandomList = function(head) {
-      if (!head) return null
-    
-    // Create current
+    if (!head) return
     let current = head
 
-    // Create copy nodes and insert them after original nodes
-    while (current !== null) {
-        const copy = new _Node(current.val, current.next)
-        // Insert copy node after current node
+    while (current) {
+        let copy = new _Node(current.val, current.next)
         current.next = copy
-        // Set current to copy's next which is current.next
-        current = copy.next
-    }
-
-    // [Node1] -> [Copy of Node1] -> [Node2] -> [Copy of Node2] -> [Node3] -> [Copy of Node3] -> null
-
-    // Reset current head
-    current = head
-
-    // Set up random 
-    while (current !== null) {
-        if (current.random !== null) {
-            // Assign copy node random to copy of node pointed to by current.random
-            current.next.random = current.random.next
-        }
-        // Move to next original node
         current = current.next.next
     }
 
-    // Reset current head
     current = head
-    // Initialize copyHead and copyCurrent to point to first copy
-    const copyHead = current.next
-    let copyCurrent= current.next
+    while (current) {
+        if (current.random) {
+            current.next.random = current.random.next
+        }
 
-    // While current
-    while (current !== null) {
-        // Restore original list by skipping over copy node
+        current = current.next.next
+    }
+
+    current = head
+
+    const copyHead = current.next
+    let copyCurrent = current.next
+
+    while (current) {
         current.next = current.next.next
         if (copyCurrent.next !== null) {
-            // Update next pointers in copied list to point to next copy node
             copyCurrent.next = copyCurrent.next.next
         }
-        // Move to next Node
+
         current = current.next
-        // Move to next copy node
         copyCurrent = copyCurrent.next
     }
 
-    // Return copyhead
     return copyHead
 };
