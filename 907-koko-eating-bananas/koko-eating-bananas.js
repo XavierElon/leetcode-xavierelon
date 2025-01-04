931d7de3f2fb438ba338d5015bfbc68ce5e9a301
Xavier <xavierelon93@gmail.com>
2025-01-04 15:53:34 -0500

    Time: 8 ms (81.74%) | Memory: 52.6 MB (47.10%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let left = 0
+    let right = Math.max(...piles)
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (canEatAll(mid, piles, h)) {
+            right = mid 
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return left
+};
+
+function canEatAll(k, piles, h) {
+    let hours = 0
+    for (const pile of piles) {
+        hours += Math.ceil(pile / k)
+
+        if (hours > h) return false
+    }
+    return hours <= h
+}
