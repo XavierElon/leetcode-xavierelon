33edb0897930b33283bd0c05cb5ce4c5620efda8
Xavier <xavierelon93@gmail.com>
2025-01-04 13:52:53 -0500

    Time: 2 ms (52.43%) | Memory: 50.7 MB (94.34%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,10 +3,25 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let expectedSum = nums.length * (nums.length + 1) / 2
-    let actualSum = nums.reduce((acc, currentValue) => {
-        return acc + currentValue
-    }, 0)
+    const n = nums.length;
 
-    return expectedSum - actualSum
+    // Place each number in its correct index
+    for (let i = 0; i < n; i++) {
+        // Continue swapping until the current number is in the correct position
+        while (nums[i] < n && nums[i] !== i) {
+            const correctIndex = nums[i];
+            // Swap nums[i] with nums[correctIndex]
+            [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+        }
+    }
+
+    // After rearrangement, find the missing number
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i) {
+            return i;
+        }
+    }
+
+    // If all numbers are in place, the missing number is n
+    return n;
 };
