57a6220a492eb99f82c7f3e35da8cbd2df8ba818
Xavier <xavierelon93@gmail.com>
2025-01-05 18:05:36 -0500

    Time: 6 ms (65.05%) | Memory: 54.5 MB (65.42%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.js
+++ 130-surrounded-regions/surrounded-regions.js
@@ -10,25 +10,12 @@ var solve = function(board) {
 
     for (let col = 0; col < cols; col++) {
         markUncapturable(0, col)
-        markUncapturable(rows - 1, col)
+        markUncapturable(rows-1, col)
     }
 
     for (let row = 0; row < rows; row++) {
         markUncapturable(row, 0)
         markUncapturable(row, cols - 1)
-    }
-
-    function markUncapturable(row, col) {
-        if (row < 0 || col < 0 || row >= rows || col >= cols || board[row][col] !== 'O') {
-            return
-        }
-
-        board[row][col] = '*'
-
-        markUncapturable(row+1, col)
-        markUncapturable(row-1, col)
-        markUncapturable(row, col+1)
-        markUncapturable(row, col-1)
     }
 
     for (let row = 0; row < rows; row++) {
@@ -40,5 +27,17 @@ var solve = function(board) {
             }
         }
     }
-};
 
+    function markUncapturable(row, col) {
+        if (row < 0 || col < 0 || row >= rows || col >= cols || board[row][col] !== 'O') {
+            return
+        }
+
+        board[row][col] = '*'
+
+        markUncapturable(row + 1, col)
+        markUncapturable(row - 1, col)
+        markUncapturable(row, col + 1)
+        markUncapturable(row, col - 1)
+    }
+};
