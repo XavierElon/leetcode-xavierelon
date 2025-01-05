8d5a2a90c3ea7217a5142be597e5d4b049e74bb2
Xavier <xavierelon93@gmail.com>
2025-01-05 15:03:45 -0500

    Time: 1 ms (99.84%) | Memory: 51.3 MB (95.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -4,15 +4,15 @@
  */
 var orangesRotting = function(grid) {
     let freshCount = 0
+    let min = 0
+    const rottingOranges = []
     const rows = grid.length
     const cols = grid[0].length
-    const rottingOranges = []
-    let min = 0
 
-    for (let row = 0; row < grid.length; row++) {
-        for (let col = 0; col < grid[0].length; col++) {
-            if (grid[row][col] === 1) freshCount++
-            else if(grid[row][col] === 2) rottingOranges.push(row * cols + col)
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === 1) freshCount++
+            else if (grid[r][c] === 2) rottingOranges.push(r * cols + c)
         }
     }
 
@@ -27,16 +27,16 @@ var orangesRotting = function(grid) {
             if (row - 1 >= 0 && grid[row-1][col] === 1) {
                 freshCount--
                 grid[row-1][col] = 2
-                rottingOranges.push((row-1) * cols + col)
+                rottingOranges.push((row - 1) * cols + col)
             }
 
             if (row + 1 < rows && grid[row+1][col] === 1) {
                 freshCount--
                 grid[row+1][col] = 2
-                rottingOranges.push((row+1) * cols + col)
+                rottingOranges.push((row + 1) * cols + col)
             }
 
-            if (col - 1 >= 0 && grid[row][col-1] === 1) {
+            if (col -1 >= 0 && grid[row][col-1] === 1) {
                 freshCount--
                 grid[row][col-1] = 2
                 rottingOranges.push(row * cols + col - 1)
