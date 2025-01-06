a5bf450a07cbf103ea240e2c138331a2f34a50fa
Xavier <xavierelon93@gmail.com>
2025-01-06 15:35:56 -0500

    Time: 3 ms (84.76%) | Memory: 53.1 MB (47.35%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    if (!grid || grid.length === 0) {
-        return 0
-    }
+    if (!grid || grid.length === 0) return 0
 
     let maxArea = 0
-    const directions = [[-1, 0],[1,0],[0,1],[0,-1]]
+    const directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
     for (let r = 0; r < grid.length; r++) {
         for (let c = 0; c < grid[0].length; c++) {
@@ -19,6 +17,7 @@ var maxAreaOfIsland = function(grid) {
         }
     }
 
+
     return maxArea
 };
 
@@ -26,15 +25,14 @@ function dfs(grid, directions, r, c) {
     if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] === 0) {
         return 0
     }
-
     grid[r][c] = 0
-
     let area = 1
 
     for (const [dr, dc] of directions) {
-        const newR = dr + r
-        const newC = dc + c
+        const newR = r + dr
+        const newC = c + dc
         area += dfs(grid, directions, newR, newC)
+
     }
 
     return area
