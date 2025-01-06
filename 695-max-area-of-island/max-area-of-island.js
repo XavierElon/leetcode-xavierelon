4b6c158a31814f076d9714bd381ca49cdd2bda03
Xavier <xavierelon93@gmail.com>
2025-01-06 15:29:12 -0500

    Time: 8 ms (43.54%) | Memory: 52.6 MB (54.42%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -3,36 +3,39 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    if (!grid || grid.length === 0) return 0
-    
+    if (!grid || grid.length === 0) {
+        return 0
+    }
+
     let maxArea = 0
-    const directions = [[-1,0], [1,0], [0,-1], [0,1]]
+    const directions = [[-1, 0],[1,0],[0,1],[0,-1]]
 
     for (let r = 0; r < grid.length; r++) {
         for (let c = 0; c < grid[0].length; c++) {
-            if (grid[r][c]) {
-                let area = 0
-                const stack = [[r,c]]
-                grid[r][c] = 0
-
-                while (stack.length > 0) {
-                    const [currentR, currentC] = stack.pop()
-                    area++
-
-                    for (const [dr, dc] of directions) {
-                        const newR = currentR + dr
-                        const newC = currentC + dc
-
-                        if (newR >= 0 && newC >= 0 && newR < grid.length && newC < grid[0].length && grid[newR][newC] === 1) {
-                            stack.push([newR, newC])
-                            grid[newR][newC] = 0
-                        }
-                    }
-                }
-                maxArea = Math.max(maxArea, area)
+            if (grid[r][c] === 1) {
+                const currentArea = dfs(grid, directions, r, c)
+                maxArea = Math.max(maxArea, currentArea)
             }
         }
     }
+
     return maxArea
 };
 
+function dfs(grid, directions, r, c) {
+    if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] === 0) {
+        return 0
+    }
+
+    grid[r][c] = 0
+
+    let area = 1
+
+    for (const [dr, dc] of directions) {
+        const newR = dr + r
+        const newC = dc + c
+        area += dfs(grid, directions, newR, newC)
+    }
+
+    return area
+}
