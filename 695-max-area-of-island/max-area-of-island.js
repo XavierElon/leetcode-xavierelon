2a85a47c859c959e6ee95abe1b520775b512f61e
Xavier <xavierelon93@gmail.com>
2025-01-06 16:17:00 -0500

    Time: 9 ms (38.91%) | Memory: 53.5 MB (44.90%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -4,34 +4,35 @@
  */
 var maxAreaOfIsland = function(grid) {
     if (!grid || grid.length === 0) return 0
-
+    
     const rows = grid.length
     const cols = grid[0].length
-    const directions =[[1,0],[-1,0],[0,1],[0,-1]]
+    const directions = [[1,0],[-1,0],[0,1],[0,-1]]
     let maxArea = 0
-
+    
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             if (grid[r][c] === 1) {
                 let area = 0
-                const queue = [[r, c]]
+                const stack = [[r, c]]
                 grid[r][c] = 0
 
-                while (queue.length > 0) {
-                    const [currentR, currentC] = queue.shift()
+                while (stack.length > 0) {
+                    const [currentR, currentC] = stack.pop()
                     area++
+                    // grid[currentR][currentC] = 0
 
                     for (const [dr, dc] of directions) {
                         const newR = currentR + dr
                         const newC = currentC + dc
 
                         if (newR >= 0 && newC >= 0 && newR < rows && newC < cols && grid[newR][newC] === 1) {
-                            queue.push([newR, newC])
+                            stack.push([newR, newC])
                             grid[newR][newC] = 0
                         }
                     }
                 }
-                maxArea = Math.max(area, maxArea)
+                maxArea = Math.max(maxArea, area)
             }
         }
     }
