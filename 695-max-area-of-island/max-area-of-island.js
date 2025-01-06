34eba004e45a932e2886016aad0ce84142859d8b
Xavier <xavierelon93@gmail.com>
2025-01-06 15:48:11 -0500

    Time: 12 ms (29.66%) | Memory: 54.4 MB (38.64%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -11,8 +11,25 @@ var maxAreaOfIsland = function(grid) {
     for (let r = 0; r < grid.length; r++) {
         for (let c = 0; c < grid[0].length; c++) {
             if (grid[r][c] === 1) {
-                const currentArea = dfs(grid, directions, r, c)
-                maxArea = Math.max(maxArea, currentArea)
+                let area = 0
+                const queue = [[r,c]]
+                grid[r][c] = 0
+
+                while (queue.length > 0) {
+                    const [currentR, currentC] = queue.shift()
+                    area++
+
+                    for (const [dr, dc] of directions) {
+                        const newR = dr + currentR
+                        const newC = dc + currentC
+
+                        if (newR >= 0 && newC >= 0 && newR < grid.length && newC < grid[0].length && grid[newR][newC] === 1) {
+                            queue.push([newR, newC])
+                            grid[newR][newC] = 0
+                        }
+                    }
+                }
+                maxArea = Math.max(maxArea, area)
             }
         }
     }
@@ -21,19 +38,3 @@ var maxAreaOfIsland = function(grid) {
     return maxArea
 };
 
-function dfs(grid, directions, r, c) {
-    if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] === 0) {
-        return 0
-    }
-    grid[r][c] = 0
-    let area = 1
-
-    for (const [dr, dc] of directions) {
-        const newR = r + dr
-        const newC = c + dc
-        area += dfs(grid, directions, newR, newC)
-
-    }
-
-    return area
-}
