93dad46432fd12e3237acdd3faef1a4c8ca45a31
Xavier <xavierelon93@gmail.com>
2025-01-07 11:29:56 -0500

    Time: 32 ms (9.77%) | Memory: 60.1 MB (27.48%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -3,20 +3,23 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
+    if (intervals.length <= 1) return intervals
+
     intervals.sort((a, b) => a[0] - b[0])
 
-    const merged = [intervals[0]]
+    let writeIndex = 0
 
     for (let i = 1; i < intervals.length; i++) {
-        const lastMerged = merged[merged.length - 1]
-        const currentInterval = intervals[i]
-
-        if (lastMerged[1] >= currentInterval[0]) {
-            lastMerged[1] = Math.max(lastMerged[1], currentInterval[1])
+        if (intervals[writeIndex][1] >= intervals[i][0]) {
+            intervals[writeIndex][1] = Math.max(intervals[writeIndex][1], intervals[i][1])
         } else {
-            merged.push(currentInterval)
+            writeIndex++
+            intervals[writeIndex] = intervals[i]
         }
     }
 
-    return merged
+    console.log(intervals)
+    console.log(writeIndex)
+    intervals.length = writeIndex + 1
+    return intervals
 };
