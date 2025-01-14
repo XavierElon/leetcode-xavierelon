9eb7de6fdff29982767591f49d15ac7dc5e1ed66
Xavier <xavierelon93@gmail.com>
2025-01-13 20:19:27 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (16.28%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -9,8 +9,8 @@ class Solution:
         while curr <= p2:
             if nums[curr] == 0:
                 nums[curr], nums[p0] = nums[p0], nums[curr]
-                p0 += 1
                 curr += 1
+                p0 += 1
             elif nums[curr] == 2:
                 nums[curr], nums[p2] = nums[p2], nums[curr]
                 p2 -= 1
