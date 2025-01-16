53743d150ead2979c2c437ba0671832fd692c453
Xavier <xavierelon93@gmail.com>
2025-01-16 11:05:00 -0500

    Time: 6 ms (45.20%) | Memory: 17.9 MB (23.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row_sets = [set() for _ in range(9)]
-        column_sets = [set() for _ in range(9)]
+        col_sets = [set() for _ in range(9)]
         subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
 
         for r in range(9):
@@ -9,17 +9,16 @@ class Solution:
                 num = board[r][c]
                 if num == '.':
                     continue
-
+                
                 if num in row_sets[r]:
                     return False
-                
-                if num in column_sets[c]:
+                if num in col_sets[c]:
+                    return False
+                if num in subgrid_sets[r // 3][c // 3]:
                     return False
 
-                if num in subgrid_sets[r // 3][ c // 3]:
-                    return False
-                
                 row_sets[r].add(num)
-                column_sets[c].add(num)
+                col_sets[c].add(num)
                 subgrid_sets[r // 3][c // 3].add(num)
+
         return True
