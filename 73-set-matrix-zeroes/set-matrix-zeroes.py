e8c5e71c7480e366925a90ca70afa3776ed14f4d
Xavier <xavierelon93@gmail.com>
2025-01-16 15:26:57 -0500

    Time: 3 ms (72.95%) | Memory: 18.6 MB (11.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -7,15 +7,34 @@ class Solution:
             return
 
         m, n = len(matrix), len(matrix[0])
-        zero_rows, zero_cols = set(), set()
+
+        first_row_has_zero = False
+        for c in range(n):
+            if matrix[0][c] == 0:
+                first_row_has_zero = True
+                break
+        first_col_has_zero = False
+         
+        for r in range(m):
+            if matrix[r][0] == 0:
+                first_col_has_zero = True
+                break
 
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 0:
-                    zero_rows.add(r)
-                    zero_cols.add(c)
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
 
-        for r in range(m):
-            for c in range(n):
-                if r in zero_rows or c in zero_cols:
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
+
+        if first_row_has_zero:
+            for c in range(n):
+                matrix[0][c] = 0
+
+        if first_col_has_zero:
+            for r in range(m):
+                matrix[r][0] = 0
