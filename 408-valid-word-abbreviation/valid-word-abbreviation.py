15b9a9e76c6b3f2f2c598eba74df7ac6fda7ed4e
Xavier <xavierelon93@gmail.com>
2025-01-19 16:11:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.92%) - LeetSync

---
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        p1 = p2 = 0
+
+        while p1 < len(word) and p2 < len(abbr):
+            if abbr[p2].isdigit():
+                if abbr[p2] == '0':
+                    return False
+                shift = 0
+                while p2 < len(abbr) and abbr[p2].isdigit():
+                    shift = (shift * 10) + int(abbr[p2])
+                    p2 += 1
+                p1 += shift
+            else:
+                if word[p1] != abbr[p2]:
+                    return False
+                p1 += 1
+                p2 += 1
+
+        return p1 == len(word) and p2 == len(abbr)
