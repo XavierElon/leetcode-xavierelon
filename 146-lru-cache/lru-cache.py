2882fdacf3fe594b476ccc5f9b0be43bf601efad
Xavier <xavierelon93@gmail.com>
2025-01-22 12:33:10 -0500

    Time: 109 ms (86.89%) | Memory: 78.3 MB (43.84%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,52 @@
+class DoublyLinkedListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap ={}
+        self.head = DoublyLinkedListNode(-1, -1)
+        self.tail = DoublyLinkedListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.hashmap:
+            return -1
+
+        self.remove_node(self.hashmap[key])
+        self.add_to_tail(self.hashmap[key])
+        return self.hashmap[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.remove_node(self.hashmap[key])
+        node = DoublyLinkedListNode(key, value)
+        self.hashmap[key] = node
+
+        if len(self.hashmap) > self.capacity:
+            del self.hashmap[self.head.next.key]
+            self.remove_node(self.head.next)
+        self.add_to_tail(node)
+        
+    def add_to_tail(self, node: DoublyLinkedListNode) -> None:
+        prev_node = self.tail.prev
+        node.prev = prev_node
+        node.next = self.tail
+        prev_node.next = node
+        self.tail.prev = node
+
+    def remove_node(self, node: DoublyLinkedListNode) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
