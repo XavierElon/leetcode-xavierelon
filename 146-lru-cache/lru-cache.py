589d3a2ae4e3d95756c56fcff3e3255334d4ca88
Xavier <xavierelon93@gmail.com>
2025-01-22 12:35:38 -0500

    Time: 168 ms (27.14%) | Memory: 78.2 MB (64.45%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -9,28 +9,28 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.hashmap ={}
+        self.cache: Dict[int, DoublyLinkedList] = {}
         self.head = DoublyLinkedListNode(-1, -1)
         self.tail = DoublyLinkedListNode(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key not in self.hashmap:
+        if key not in self.cache:
             return -1
 
-        self.remove_node(self.hashmap[key])
-        self.add_to_tail(self.hashmap[key])
-        return self.hashmap[key].val
+        self.remove_node(self.cache[key])
+        self.add_to_tail(self.cache[key])
+        return self.cache[key].val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hashmap:
-            self.remove_node(self.hashmap[key])
+        if key in self.cache:
+            self.remove_node(self.cache[key])
         node = DoublyLinkedListNode(key, value)
-        self.hashmap[key] = node
+        self.cache[key] = node
 
-        if len(self.hashmap) > self.capacity:
-            del self.hashmap[self.head.next.key]
+        if len(self.cache) > self.capacity:
+            del self.cache[self.head.next.key]
             self.remove_node(self.head.next)
         self.add_to_tail(node)
         
