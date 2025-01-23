dcd2c464e8e35219fd0cc30c5a26ea892c7f4a7d
Xavier <xavierelon93@gmail.com>
2025-01-22 20:01:42 -0500

    Time: 3 ms (43.76%) | Memory: 18.3 MB (41.46%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,31 +1,19 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        visited = [False] * len(graph)
-        node_color = [False] * len(graph)  # False and True represent two colors
-
-        queue = deque()
+        colors = [0] * len(graph)
 
         for i in range(len(graph)):
-            # If the node is already visited or has no neighbors, skip it
-            if visited[i] or not graph[i]:
-                continue
+            if colors[i] == 0 and not self.dfs(i, 1, graph, colors):
+                return False
+        return True
 
-            # Start BFS from node i
-            queue.append(i)
-            visited[i] = True
-            node_color[i] = True  # Assign the first color
+    def dfs(self, node: int, color: int, graph: List[List[int]], colors: List[int]) -> bool:
+        colors[node] = color
 
-            while queue:
-                node = queue.popleft()
-                current_color = node_color[node]
-
-                for neighbor in graph[node]:
-                    if not visited[neighbor]:
-                        queue.append(neighbor)
-                        visited[neighbor] = True
-                        node_color[neighbor] = not current_color  # Assign opposite color
-                    else:
-                        if node_color[neighbor] == current_color:
-                            return False  # Found same color on adjacent nodes
-
-        return True  # All nodes processed without conflicts
+        for neighbor in graph[node]:
+            if colors[neighbor] == color:
+                return False
+            
+            if (colors[neighbor] == 0 and not self.dfs(neighbor, -color, graph, colors)):
+                return False
+        return True
