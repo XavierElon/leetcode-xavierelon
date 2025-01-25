fc5966deff32b8635024b5f0c67ef029312aced6
Xavier <xavierelon93@gmail.com>
2025-01-25 08:41:11 -0800

    Time: 11 ms (14.07%) | Memory: 18.8 MB (60.62%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        if not self.isBalanced(root.left) or not self.isBalanced(root.right):
+            return False
+
+        left_height = self.get_height(root.left)
+        right_height = self.get_height(root.right)
+        return abs(left_height - right_height) <= 1
+    
+    def get_height(self, node):
+        if not node:
+            return 0
+        
+        return 1 + max(self.get_height(node.left), self.get_height(node.right))
