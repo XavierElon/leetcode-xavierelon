fd04cdb0874185d1fbf3a8a69643550aceda6ec0
Xavier <xavierelon93@gmail.com>
2025-01-27 14:18:22 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (56.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,8 +5,10 @@ class Solution:
 
         dp = [0] * (n + 1)
 
-        dp[1], dp[2] = 1, 2
+        one_before, two_before = 2, 1
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+            current = one_before + two_before
+            two_before = one_before
+            one_before = current
+        return one_before
