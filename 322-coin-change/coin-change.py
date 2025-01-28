2829dab155ec72a8c00cc632ac231c1345e2a325
Xavier <xavierelon93@gmail.com>
2025-01-27 16:58:03 -0800

    Time: 708 ms (80.16%) | Memory: 18.3 MB (50.52%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,20 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res = self.top_down_dp(coins, amount, {})
-        return -1 if res == float('inf') else res
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
 
-    def top_down_dp(self, coins: List[int], target: int, memo: Dict[int, int]) -> int:
-        if target == 0:
-            return 0
+        for t in range(1, amount + 1):
+            for coin in coins:
+                if coin <= t:
+                    dp[t] = min(dp[t], 1 + dp[t-coin])
 
-        if target in memo:
-            return memo[target]
-
-        min_coins = float('inf')
-
-        for coin in coins:
-            if coin <= target:
-                min_coins = min(min_coins, 1 + self.top_down_dp(coins, target - coin, memo))
-        
-        memo[target] = min_coins
-        return memo[target]
+        return dp[amount] if dp[amount] != float('inf') else -1
