8f8d292ded6b2181d7d4910d18076e06f2294071
Xavier <xavierelon93@gmail.com>
2025-01-27 16:44:38 -0800

    Time: 6 ms (24.93%) | Memory: 18 MB (28.11%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,19 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def minimum_cost(i):
-            # Base case, we are allowed to start at either step 0 or step 1
-            if i <= 1:
-                return 0
+        minimum_cost = [0] * (len(cost) + 1)
 
-            # Check if we have already calculated minimum_cost(i)
-            if i in memo:
-                return memo[i]
+        for i in range(2, len(cost) + 1):
+            take_one_step = minimum_cost[i-1] + cost[i-1]
+            take_two_steps = minimum_cost[i-2] + cost[i-2]
+            minimum_cost[i] = min(take_one_step, take_two_steps)
 
-            # If not, cache the result in our hash map and return it
-            down_one = cost[i - 1] + minimum_cost(i - 1)
-            down_two = cost[i - 2] + minimum_cost(i - 2)
-            memo[i] = min(down_one, down_two)
-            return memo[i]
-
-        memo = {}
-        return minimum_cost(len(cost))
+        return minimum_cost[-1]
