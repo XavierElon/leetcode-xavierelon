93c6f02299d7d59d5a9520467498467010482284
Xavier <xavierelon93@gmail.com>
2025-01-29 12:49:37 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.55%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -4,16 +4,18 @@ class Solution:
         self.backtrack(nums, [], set(), res)
         return res
 
-    def backtrack(self, nums: List[int], candidate: List[int], used: Set[int], res: List[List[int]]) -> None:
-        if len(candidate) == len(nums):
-            res.append(candidate.copy())
+    def backtrack(self, nums: List[int], current: List[int], used: Set[int], res: List[List[int]]) -> None:
+        if len(current) == len(nums):
+            res.append(current.copy())
             return
 
         for num in nums:
             if num not in used:
-                candidate.append(num)
                 used.add(num)
-                self.backtrack(nums, candidate, used, res)
-                candidate.pop()
+                current.append(num)
+
+                self.backtrack(nums, current, used, res)
+
                 used.remove(num)
-                
+                current.pop()
+        
