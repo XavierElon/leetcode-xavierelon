e06ff1261a12be9b301f03ac0281a2ea91e9b5c4
Xavier <xavierelon93@gmail.com>
2025-01-30 18:42:08 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (62.01%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -9,17 +9,8 @@ class Solution:
         if not root:
             return None
 
-        stack = [root]
+        root.left, root.right = root.right, root.left
 
-        while stack:
-            node = stack.pop()
-
-            node.left, node.right = node.right, node.left
-
-            if node.right:
-                stack.append(node.right)
-            
-            if node.left:
-                stack.append(node.left)
-
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
