bdab804b2adc1a3d3920ae298b0ebb7830922783
Xavier <xavierelon93@gmail.com>
2025-02-03 15:25:20 -0800

    Time: 3 ms (19.24%) | Memory: 17.8 MB (50.08%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -23,7 +23,6 @@ class Solution:
 
             if node.left:
                 queue.append((node.left, column - 1))
-
             if node.right:
                 queue.append((node.right, column + 1))
 
