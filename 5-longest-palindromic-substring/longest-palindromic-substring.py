c13762985197d83ccce3f6cecc4164bc5247458c
Xavier <xavierelon93@gmail.com>
2025-02-03 14:36:31 -0800

    Time: 318 ms (54.82%) | Memory: 17.7 MB (73.39%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,7 +4,7 @@ class Solution:
         resLen = 0
 
         for i in range(len(s)):
-            # Odd Length
+            # Odd length
             l, r = i, i
 
             while l >= 0 and r < len(s) and s[l] == s[r]:
@@ -16,7 +16,7 @@ class Solution:
                 r += 1
 
             # Even Length
-            l, r = i , i + 1
+            l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
                     res = s[l:r+1]
@@ -24,5 +24,5 @@ class Solution:
 
                 l -= 1
                 r += 1
-        
+
         return res
