fb8930a4416977176022da3789bab12f891ec270
Xavier <xavierelon93@gmail.com>
2025-02-04 09:59:30 -0800

    Time: 7 ms (59.94%) | Memory: 18 MB (67.09%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
         def backtrack(counter, path):
             if len(path) == len(nums):
                 res.append(path.copy())
                 return
+
             for num in counter:
                 if counter[num] > 0:
-                    counter[num] -= 1
                     path.append(num)
+                    counter[num] -= 1
                     backtrack(counter, path)
                     path.pop()
                     counter[num] += 1
