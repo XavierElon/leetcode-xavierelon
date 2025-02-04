ff61370b788457b793db14910e376c4c426afb81
Xavier <xavierelon93@gmail.com>
2025-02-04 10:26:41 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,24 +2,24 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-            
+
         keypad_map = {
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl','6': 'mno','7': 'pqrs','8': 'tuv', '9': 'wxyz'
         }
 
         res = []
-        
-        def backtrack(i: int, curr_combination: List[str], digits: str, keypad_map: Dict[str, str], res: List[str]) -> None:
-            if len(curr_combination) == len(digits):
-                res.append("".join(curr_combination))
+
+        def backtrack(i: int, current: List[str], digits: str, kepad_map: Dict[str, str], res: List[str]) -> None:
+            if len(current) == len(digits):
+                res.append("".join(current))
                 return
 
             for letter in keypad_map[digits[i]]:
-                curr_combination.append(letter)
+                current.append(letter)
 
-                backtrack(i+1, curr_combination, digits, keypad_map, res)
+                backtrack(i + 1, current, digits, keypad_map, res)
 
-                curr_combination.pop()
-        
+                current.pop()
+
         backtrack(0, [], digits, keypad_map, res)
         return res
