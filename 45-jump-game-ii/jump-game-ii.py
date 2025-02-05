7105b9cb7e7e3992b93739b309aba3b55c6f66d8
Xavier <xavierelon93@gmail.com>
2025-02-05 15:05:25 -0800

    Time: 11 ms (47.08%) | Memory: 18.7 MB (31.85%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -5,9 +5,9 @@ class Solution:
         if n == 1:
             return 0
         
+        farthest = 0
         jumps = 0
         current_end = 0
-        farthest = 0
 
         for i in range(n - 1):
             farthest = max(farthest, i + nums[i])
@@ -15,7 +15,8 @@ class Solution:
             if i == current_end:
                 jumps += 1
                 current_end = farthest
-                if current_end >= n - 1:
+
+                if current_end >= n-1:
                     break
-                    
         return jumps
+
