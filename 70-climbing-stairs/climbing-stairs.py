1e39c14bfcdc1e6457f52cde7b67ea55d402b716
Xavier <xavierelon93@gmail.com>
2025-02-05 13:30:37 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (41.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,11 +3,11 @@ class Solution:
         if n == 1:
             return 1
         
-        first = 1
-        second = 2
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        dp[2] = 2
 
         for i in range(3, n + 1):
-            third = first + second
-            first = second
-            second = third
-        return second
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
