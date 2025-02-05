97525813d0ef8268b0f03a57b5835c51d4d5b354
Xavier <xavierelon93@gmail.com>
2025-02-05 13:39:46 -0800

    Time: 1 ms (84.26%) | Memory: 17.7 MB (77.59%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,8 +3,8 @@ class Solution:
         minimum_cost = [0] * (len(cost) + 1)
 
         for i in range(2, len(cost) + 1):
-            take_one_step = minimum_cost[i-1] + cost[i-1]
-            take_two_steps = minimum_cost[i-2] + cost[i-2]
-            minimum_cost[i] = min(take_one_step, take_two_steps)
+            one_step = minimum_cost[i-1] + cost[i-1]
+            two_step = minimum_cost[i-2] + cost[i-2]
+            minimum_cost[i] = min(one_step, two_step)
 
         return minimum_cost[-1]
