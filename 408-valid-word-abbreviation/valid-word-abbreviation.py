f88279d0e67fd6fb08ab26b2e91e8d3612f631d9
Xavier <xavierelon93@gmail.com>
2025-02-10 08:41:51 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.75%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -4,18 +4,22 @@ class Solution:
 
         while p1 < len(word) and p2 < len(abbr):
             if abbr[p2].isdigit():
+                shift = 0
                 if abbr[p2] == '0':
                     return False
-                shift = 0
+
                 while p2 < len(abbr) and abbr[p2].isdigit():
                     shift = (shift * 10) + int(abbr[p2])
                     p2 += 1
+
                 p1 += shift
+
+
+            elif word[p1] != abbr[p2]:
+                return False
+
             else:
-                if word[p1] != abbr[p2]:
-                    return False
-                else:
-                    p1 += 1
-                    p2 += 1
-        
+                p1 += 1
+                p2 += 1
+            
         return p1 == len(word) and p2 == len(abbr)
