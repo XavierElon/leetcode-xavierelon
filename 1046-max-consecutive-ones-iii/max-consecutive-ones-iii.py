10557f99563a4d112d5f78b046d614672f1c73e2
Xavier <xavierelon93@gmail.com>
2025-02-11 09:55:09 -0800

    Time: 72 ms (29.95%) | Memory: 18.4 MB (38.35%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -12,7 +12,7 @@ class Solution:
                 if nums[left] == 0:
                     zero_count -= 1
                 left += 1
-            
-            max_length = max(max_length, right- left + 1)
+
+            max_length = max(max_length, right - left + 1)
 
         return max_length
