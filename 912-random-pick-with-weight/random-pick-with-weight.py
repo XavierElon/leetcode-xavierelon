ba2d98fa0536cecfb04f328301e6c19e3f9cc265
Xavier <xavierelon93@gmail.com>
2025-02-10 18:57:32 -0800

    Time: 82 ms (43.69%) | Memory: 23.3 MB (78.17%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.prefixSums = []
+        prefix_sum = 0
+
+        for weight in w:
+            prefix_sum += weight
+            self.prefixSums.append(prefix_sum)
+
+        self.totalSum = prefix_sum
+
+    def pickIndex(self) -> int:
+        rand_target = random.randint(1, self.totalSum)
+
+        left, right = 0, len(self.prefixSums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if self.prefixSums[mid] < rand_target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
