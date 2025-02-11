951c3c878d3c419c79f63ccd2d2b33a2f60425e4
Xavier <xavierelon93@gmail.com>
2025-02-10 16:59:04 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.17%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,21 +1,15 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        freq = [0] * 26
-
-        for char in s:
-            freq[ord(char) - ord('a')] += 1
+        freq = Counter(s)
 
         result = []
 
         for char in order:
-            index = ord(char) - ord('a')
+            if char in freq:
+                result.append(char * freq[char])
+                del freq[char]
 
-            if freq[index] > 0:
-                result.append(char * freq[index])
-                freq[index] = 0
-
-        for i in range(26):
-            if freq[i] > 0:
-                result.append(chr(i + ord('a')) * freq[i])
+        for char, count in freq.items():
+            result.append(char * count)
 
         return "".join(result)
