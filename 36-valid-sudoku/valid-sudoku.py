c573320ce6b351d44cc3e71c554a3f98efa1f5bf
Xavier <xavierelon93@gmail.com>
2025-02-12 12:41:49 -0800

    Time: 2 ms (87.05%) | Memory: 17.9 MB (42.28%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,24 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_sets = [set() for _ in range(9)]
-        col_sets = [set() for _ in range(9)]
-        subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
+        row_arr = [set() for _ in range(9)]
+        col_arr = [set() for _ in range(9)]
+        box_arr = [[set() for _ in range(3)] for _ in range(3)]
 
-        for r in range(9):
-            for c in range(9):
-                num = board[r][c]
-                if num == '.':
-                    continue
-                
-                if num in row_sets[r]:
-                    return False
-                if num in col_sets[c]:
-                    return False
-                if num in subgrid_sets[r // 3][c // 3]:
-                    return False
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] != '.':
+                    num = board[row][col]
 
-                row_sets[r].add(num)
-                col_sets[c].add(num)
-                subgrid_sets[r // 3][c // 3].add(num)
+                    if num in row_arr[row] or num in col_arr[col] or num in box_arr[row//3][col//3]:
+                        return False
+
+                    row_arr[row].add(num)
+                    col_arr[col].add(num)
+                    box_arr[row//3][col//3].add(num)
 
         return True
