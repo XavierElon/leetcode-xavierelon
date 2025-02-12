4f3ddc7c66dfd7287ed466596e19732b4b5115bd
Xavier <xavierelon93@gmail.com>
2025-02-12 12:57:00 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (35.95%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,15 +1,14 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         freq = Counter(s)
-
-        result = []
+        res = []
 
         for char in order:
             if char in freq:
-                result.append(char * freq[char])
+                res.append(char * freq[char])
                 del freq[char]
 
         for char, count in freq.items():
-            result.append(char * count)
+            res.append(char * count)
 
-        return "".join(result)
+        return "".join(res)
