0ffd72002ff9957b865b666cbd8f94747151fa02
Xavier <xavierelon93@gmail.com>
2025-02-13 10:29:23 -0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.29%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,36 +1,44 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        lower_bound = self.lower_bound_bs(nums, target)
-        upper_bound = self.upper_bound_bs(nums, target)
+        lower_bound = self.lower_bound(nums, target)
+        upper_bound = self.upper_bound(nums, target)
 
         return [lower_bound, upper_bound]
 
-    def lower_bound_bs(self, nums, target):
+    def lower_bound(self, nums, target):
         left, right = 0, len(nums) - 1
 
         while left < right:
             mid = (left + right) // 2
-
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
                 right = mid
 
         return left if nums and nums[left] == target else -1
 
-    def upper_bound_bs(self, nums, target):
+
+    def upper_bound(self, nums, target):
         left, right = 0, len(nums) - 1
 
         while left < right:
             mid = (left + right) // 2 + 1
 
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
                 left = mid
 
         return right if nums and nums[right] == target else -1
+
+
+'''
+[5,8,8,8,8,8,10]
+ L     M       R
+mid = 1
+n = 7
+'''
