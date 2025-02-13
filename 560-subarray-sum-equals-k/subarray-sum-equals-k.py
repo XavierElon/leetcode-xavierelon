55a109cc410a04504ee88230afb9ea7b942f163f
Xavier <xavierelon93@gmail.com>
2025-02-13 12:32:38 -0800

    Time: 34 ms (47.50%) | Memory: 20.2 MB (89.89%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
-        prefix_sum = 0
-        prefix_freq_map = {0: 1}
+        prefix_sum_map = {0: 1}
+        curr_prefix_sum = 0
 
         for num in nums:
-            prefix_sum += num
-            if prefix_sum - k in prefix_freq_map:
-                count += prefix_freq_map[prefix_sum - k]
-            prefix_freq_map[prefix_sum] = prefix_freq_map.get(prefix_sum, 0) + 1
+            curr_prefix_sum += num
+
+            if curr_prefix_sum - k in prefix_sum_map:
+                count += prefix_sum_map[curr_prefix_sum - k]
+            freq = prefix_sum_map.get(curr_prefix_sum, 0)
+            prefix_sum_map[curr_prefix_sum] = freq + 1
 
         return count
