52a4468c29dabbbc296757d24c0b90de5a55066e
Xavier <xavierelon93@gmail.com>
2025-02-15 11:30:13 -0800

    Time: 1296 ms (95.90%) | Memory: 21.6 MB (72.55%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,34 +1,10 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.idx_val = [(idx, val) for idx, val in enumerate(nums)]
+        self.values = {i: val for i, val in enumerate(nums) if val != 0}
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        res = 0
-        for pair in self.idx_val:
-            val = self.binary_search(vec.idx_val, pair[0])
-            res += val * pair[1] if val != float('inf') else 0
-        return res
-
-    def binary_search(self, vec_pair, target_idx):
-        start, end = 0, len(vec_pair) - 1
-
-        while start + 1 < end:
-            mid = (start + end) // 2
-
-            if vec_pair[mid][0] == target_idx:
-                return vec_pair[mid][1]
-            elif vec_pair[mid][0] > target_idx:
-                end = mid
-            else:
-                start = mid
-
-        if vec_pair[start][0] == target_idx:
-            return vec_pair[start][1]
-        if vec_pair[end][0] == target_idx:
-            return vec_pair[end][1]
-
-        return floatt('inf')
+        return sum(self.values[i] * vec.values[i] for i in self.values.keys() & vec.values.keys())
         
 
 # Your SparseVector object will be instantiated and called as such:
