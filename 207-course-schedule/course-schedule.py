df9a95cfd16a53a0b2865515b621207291f81823
Xavier <xavierelon93@gmail.com>
2025-02-15 10:39:02 -0800

    Time: 1 ms (93.56%) | Memory: 19 MB (82.90%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,19 +3,21 @@ class Solution:
         indegree = [0] * numCourses
         adj_list = [[] for _ in range(numCourses)]
         count = 0
-         
+
         for course, prereq in prerequisites:
             indegree[course] += 1
             adj_list[prereq].append(course)
 
-        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        queue = deque(i for i in range(numCourses) if indegree[i] == 0)
 
         while queue:
             current = queue.popleft()
             count += 1
-            for nextCourse in adj_list[current]:
-                indegree[nextCourse] -= 1
-                if indegree[nextCourse] == 0:
-                    queue.append(nextCourse)
-        
+
+            for next_course in adj_list[current]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+            
         return count == numCourses
