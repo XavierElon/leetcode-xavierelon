87af125b3821db2a305e450f38705a6abe5111ad
Xavier <xavierelon93@gmail.com>
2025-02-17 13:19:28 -0800

    Time: 27 ms (96.59%) | Memory: 19 MB (81.23%) - LeetSync

--- 737-sentence-similarity-ii/sentence-similarity-ii.py
+++ 737-sentence-similarity-ii/sentence-similarity-ii.py
@@ -1,21 +1,25 @@
 class Solution:
     def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+
         parent = {}
-        
+
         def find(p):
             if p not in parent:
                 parent[p] = p
-            while p != parent[p]:
-                p = parent[p]
-            return p    
-        
+            if p != parent[p]:
+                parent[p] = find(parent[p])
+            return parent[p]
+
         def union(p, q):
-            i = find(p)        
-            j = find(q)
-            if i != j:
-                parent[i] = j
-                
+            root1 = find(p)
+            root2 = find(q)
+
+            if root1 != root2:
+                parent[root1] = root2
+
         for p, q in similarPairs:
             union(p, q)
-            
-        return all(find(a) == find(b) for a, b in zip_longest(sentence1, sentence2))
+
+        return all(find(a) == find(b) for a, b in zip(sentence1, sentence2))
