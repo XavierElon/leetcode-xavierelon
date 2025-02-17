3e3376a66e07ba5f83792f9b137cb2852b57fb00
Xavier <xavierelon93@gmail.com>
2025-02-17 15:10:03 -0800

    Time: 15 ms (66.45%) | Memory: 19.8 MB (70.18%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -7,10 +7,8 @@ class Solution:
             if ratings[i] > ratings[i-1]:
                 candies[i] = candies[i-1] + 1
 
-        for i in range(n - 2, -1, -1):
+        for i in range(n-2, -1, -1):
             if ratings[i] > ratings[i+1]:
                 candies[i] = max(candies[i], candies[i + 1] + 1)
 
         return sum(candies)
-
-        
