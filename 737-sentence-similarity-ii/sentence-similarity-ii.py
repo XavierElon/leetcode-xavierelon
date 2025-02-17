23ab8aa29e727df72284e8749b57f59f73fdb4cf
Xavier <xavierelon93@gmail.com>
2025-02-17 13:11:46 -0800

    Time: 36 ms (83.62%) | Memory: 19.2 MB (63.14%) - LeetSync

--- 737-sentence-similarity-ii/sentence-similarity-ii.py
+++ 737-sentence-similarity-ii/sentence-similarity-ii.py
@@ -1,36 +1,21 @@
 class Solution:
     def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
-        len_s1 = len(sentence1)
-        len_s2 = len(sentence2)
-
-        if len_s1 != len_s2:
-            return False
-
-        graph = defaultdict(list)
-        for w1, w2 in similarPairs:
-            graph[w1].append(w2)
-            graph[w2].append(w1)
-
-        for w1, w2 in zip(sentence1, sentence2):
-            q = deque([])
-            q.append(w1)
-
-            seen = set()
-
-            while q:
-                word = q.popleft()
-
-                if word == w2:
-                    break
-
-                for new_word in graph[word]:
-                    if new_word not in seen:
-                        q.append(new_word)
-                        seen.add(new_word)
-
-            else:
-                return False
-
-        return True
-
+        parent = {}
         
+        def find(p):
+            if p not in parent:
+                parent[p] = p
+            while p != parent[p]:
+                p = parent[p]
+            return p    
+        
+        def union(p, q):
+            i = find(p)        
+            j = find(q)
+            if i != j:
+                parent[i] = j
+                
+        for p, q in similarPairs:
+            union(p, q)
+            
+        return all(find(a) == find(b) for a, b in zip_longest(sentence1, sentence2))
