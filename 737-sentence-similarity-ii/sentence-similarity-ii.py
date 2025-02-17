6f4462868136cdb052bd0678982eca3e53131071
Xavier <xavierelon93@gmail.com>
2025-02-17 13:51:26 -0800

    Time: 77 ms (26.28%) | Memory: 19 MB (93.52%) - LeetSync

--- 737-sentence-similarity-ii/sentence-similarity-ii.py
+++ 737-sentence-similarity-ii/sentence-similarity-ii.py
@@ -1,40 +1,35 @@
 class Solution:
-    def areSentencesSimilarTwo(self, sentence1: List[str],
-                            sentence2: List[str],
-                            similarPairs: List[List[str]]) -> bool:
+    def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         len_s1 = len(sentence1)
         len_s2 = len(sentence2)
+
         if len_s1 != len_s2:
             return False
 
         graph = defaultdict(list)
-        
-        for pair in similarPairs:
-            word1 = pair[0]
-            word2 = pair[1]
-            graph[word1].append(word2)
-            graph[word2].append(word1)
 
-        for index in range(len(sentence1)):
-            word1 = sentence1[index]
-            word2 = sentence2[index]
+        for w1, w2 in similarPairs:
+            graph[w1].append(w2)
+            graph[w2].append(w1)
 
-            if word1 == word2:
-                continue
-            if not self.dfs(graph, set(), word1, word2):
+        for w1, w2 in zip(sentence1, sentence2):
+            q = deque([])
+            q.append(w1)
+
+            seen = set()
+
+            while q:
+                word = q.popleft()
+
+                if word == w2:
+                    break
+
+                for new_word in graph[word]:
+                    if new_word not in seen:
+                        q.append(new_word)
+                        seen.add(new_word)
+
+            else:
                 return False
 
         return True
-
-    def dfs(self, graph, seen, currWord, targetWord):
-        if targetWord == currWord:
-            return True
-        if currWord in seen:
-            return False
-        seen.add(currWord)
-
-        for neighbor in graph[currWord]:
-            if self.dfs(graph, seen, neighbor, targetWord):
-                return True
-            
-        return False
