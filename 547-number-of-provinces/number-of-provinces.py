4805b99cb72e4f2050c4be53faf3d0eadab8ad08
Xavier <xavierelon93@gmail.com>
2025-02-17 14:32:20 -0800

    Time: 3 ms (90.52%) | Memory: 19 MB (69.65%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,36 +1,26 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        parent = list(range(n))
-        rank = [1] * n
-
-        def find(a):
-            if parent[a] != a:
-                parent[a] = find(parent[a])
-            return parent[a]
-
-        def union(a, b):
-            root_a = find(a)
-            root_b = find(b)
-
-            if root_a == root_b:
-                return
-
-            if rank[root_a] > rank[root_b]:
-                parent[root_b] = root_a
-            elif rank[root_a] > rank[root_b]:
-                parent[root_a] = root_b
-            else:
-                parent[root_b] = root_a
-                rank[root_a] += 1 
+        numberOfComponents = 0
+        visited = [False] * n 
 
         for i in range(n):
-            for j in range(i + 1, n):
-                if isConnected[i][j] == 1:
-                    union(i, j)
+            if not visited[i]:
+                numberOfComponents += 1
+                self.bfs(i, isConnected, visited)
 
-        provinces = set()
-        for city in range(n):
-            provinces.add(find(city))
+        return numberOfComponents       
 
-        return len(provinces)
+    def bfs(self, node, isConnected, visited):
+        queue = deque([node])
+        visited[node] = True
+
+        while queue:
+            node = queue.popleft()
+
+            for i in range(len(isConnected)):
+                if isConnected[node][i] == 1 and not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+
+        
