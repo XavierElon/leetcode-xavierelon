8624bf4e7f2b219ffd49407385f04a93dde2d97c
Xavier <xavierelon93@gmail.com>
2025-02-20 15:54:31 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.20%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,15 @@
 class Solution:
-    def rob(self, houses: List[int]) -> int:
-        if not houses:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
             return 0
-        if len(houses) == 1:
-            return houses[0]
-        
-        dp = [0] * len(houses)
-        dp[0] = houses[0]
-        dp[1] = max(houses[0], houses[1])
+        if len(nums) == 1:
+            return nums[0]
 
-        for i in range(2, len(houses)):
-            dp[i] = max(dp[i-1], houses[i] + dp[i-2])
-        
-        return dp[len(houses) - 1]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[-1]
