ff0c5df8eff6f76be6502097ec1a0b702378e03c
Xavier <xavierelon93@gmail.com>
2025-02-20 11:39:25 -0800

    Time: 27 ms (45.92%) | Memory: 19.6 MB (10.09%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -4,16 +4,16 @@ class Solution:
         visited = set()
 
         def dfs(r, c):
-            if (r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0 or (r, c) in visited):
+            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in visited or grid[r][c] == 0:
                 return 0
 
-            visited.add((r, c))
+            visited.add((r,c))
 
             return (1 + dfs(r + 1, c) +
                         dfs(r - 1, c) +
                         dfs(r, c + 1) +
                         dfs(r, c - 1))
-
+        
         max_area = 0
 
         for r in range(rows):
