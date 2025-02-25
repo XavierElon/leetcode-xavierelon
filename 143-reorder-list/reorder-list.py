4b57a8e12f8f0a326651b16a8a4a3815e7020dd8
Xavier <xavierelon93@gmail.com>
2025-02-25 13:36:43 -0800

    Time: 0 ms (100.00%) | Memory: 23.3 MB (80.47%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,23 +8,45 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next:
-            return head
-
+        if not head or not head.next or not head.next.next:
+         return  # Lists with 0, 1, or 2 nodes don't need reordering
+        
+        # Step 1: Find the middle of the linked list
         slow = fast = head
         while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-
-        prev, curr = None, slow
-        while curr:
-            next_ = curr.next
-            curr.next = prev
-            prev, curr = curr, next_
-
-        first, second = head, prev
-        while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
-
-        return head
+            slow = slow.next       # Moves one step at a time
+            fast = fast.next.next  # Moves two steps at a time
+        
+        # Now 'slow' points to the middle node
+        
+        # Step 2: Reverse the second half of the linked list
+        second_half = slow.next  # Start of second half
+        slow.next = None         # Break the list into two halves
+        
+        # Reverse the second half
+        prev = None
+        current = second_half
+        while current:
+            next_temp = current.next  # Store next node
+            current.next = prev       # Reverse the pointer
+            prev = current            # Move prev forward
+            current = next_temp       # Move current forward
+        
+        reversed_second_half = prev   # New head of the reversed second half
+        
+        # Step 3: Merge the two halves in alternating fashion
+        first = head
+        second = reversed_second_half
+        
+        while second:  # Second half could be shorter (or equal) if list has odd length
+            # Save next pointers
+            first_next = first.next
+            second_next = second.next
+            
+            # Interweave the nodes
+            first.next = second
+            second.next = first_next
+            
+            # Move to the next pair of nodes
+            first = first_next
+            second = second_next
