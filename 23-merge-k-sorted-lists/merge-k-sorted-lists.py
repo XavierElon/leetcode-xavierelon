66fe5e8612e96bb3c15c826a8058f0c0c68f13e9
Xavier <xavierelon93@gmail.com>
2025-02-25 15:47:43 -0800

    Time: 8 ms (79.57%) | Memory: 20.3 MB (35.41%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,33 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
+        dummy = ListNode(0)
+        current = dummy
+        heap = []
 
-        while len(lists) > 1:
-            mergedLists = []
-
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
-        return lists[0]
-
-    def mergeList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(heap, (l.val, i, l))
         
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            current.next = node
+            current = node
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
         return dummy.next
