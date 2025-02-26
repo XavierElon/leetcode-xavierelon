af491ea6af31d5994d1d96b90e863903676681b1
Xavier <xavierelon93@gmail.com>
2025-02-26 13:55:27 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.09%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -11,9 +11,11 @@ class Solution:
                 for row in matrix:
                     result.append(row.pop())
 
+            # Process the bottom row in reverse (if it exists)
             if matrix:
                 result += matrix.pop()[::-1]
 
+            # Process the leftmost column from bottom to top (if it exists)
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
                     result.append(row.pop(0))
