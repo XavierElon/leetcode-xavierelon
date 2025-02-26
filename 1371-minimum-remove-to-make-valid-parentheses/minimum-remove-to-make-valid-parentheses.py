5ad70b0eb395fd8b445667122959bb9430d28784
Xavier <xavierelon93@gmail.com>
2025-02-26 12:12:21 -0800

    Time: 57 ms (24.62%) | Memory: 20.4 MB (14.32%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,16 +1,43 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        to_remove = set()
-
-        for index, char in enumerate(s):
-            if char == '(':
-                stack.append(index)
-            elif char == ')':
-                if stack:
-                    stack.pop()
-                else:
-                    to_remove.add(index)
+        # First pass: Remove extra closing parentheses
+        extra_opens = 0
+        total_opens = 0
+        j = 0
+        result = [''] * len(s)  # Pre-allocate list for in-place modification
         
-        to_remove.update(stack)
-        return "".join([s[i] for i in range(len(s)) if i not in to_remove])
+        for ch in s:
+            if ch == ')':
+                if extra_opens == 0:
+                    continue  # Skip if no open parenthesis to match
+                extra_opens -= 1
+                result[j] = ch
+                j += 1
+            elif ch == '(':
+                total_opens += 1
+                extra_opens += 1
+                result[j] = ch
+                j += 1
+            else:
+                result[j] = ch
+                j += 1
+
+        # Second pass: Remove extra opening parentheses
+        length = j
+        j = 0
+        keep = total_opens - extra_opens  # Number of '(' to keep
+        final_result = [''] * length  # Pre-allocate for second pass
+        
+        for i in range(length):
+            ch = result[i]
+            if ch == '(':
+                if keep == 0:
+                    continue  # Skip extra '('
+                final_result[j] = ch
+                j += 1
+                keep -= 1
+            else:
+                final_result[j] = ch
+                j += 1
+
+        return ''.join(final_result[:j])
