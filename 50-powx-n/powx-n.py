c685ce4a0305a93e35a78a4bb8a5ea4e1f386a05
Xavier <xavierelon93@gmail.com>
2025-02-27 11:42:32 -0800

    Time: 1 ms (12.40%) | Memory: 17.8 MB (75.05%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,9 +3,11 @@ class Solution:
         def helper(x, n):
             if x == 0: return 0
             if n == 0: return 1
+            if n == 1: return x
 
             res = helper(x * x, n // 2)
             return res * x if n % 2 else res
 
+        
         res = helper(x, abs(n))
         return res if n >= 0 else 1 / res
