5e59ca146755583878dec0576439ed43248d1827
Xavier <xavierelon93@gmail.com>
2025-02-27 12:49:12 -0800

    Time: 3 ms (35.33%) | Memory: 19.6 MB (89.88%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -9,9 +9,10 @@ class Solution:
         def isValid(node, low, high):
             if not node:
                 return True
+            
             if node.val <= low or node.val >= high:
                 return False
-            
+
             return isValid(node.left, low, node.val) and isValid(node.right, node.val, high)
 
         return isValid(root, float('-inf'), float('inf'))
