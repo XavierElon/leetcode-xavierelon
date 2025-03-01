3650a50f75f31539fdc1f163c771864fca5591c8
Xavier <xavierelon93@gmail.com>
2025-03-01 09:18:19 -0800

    Time: 0 ms (100.00%) | Memory: 18.7 MB (45.10%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -2,9 +2,8 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
 
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
@@ -12,4 +11,4 @@ class Solution:
             else:
                 right = mid - 1
 
-        return -1
+        return left if nums[left] == target else -1
