713cda61dc3edd5849f6068191b7b0ff06244e7b
Xavier <xavierelon93@gmail.com>
2025-03-01 11:20:42 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.79%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,26 +1,35 @@
 class Solution:
-    def nextPermutation(self, nums):
-        def swap(i, j):
-            nums[i], nums[j] = nums[j], nums[i]
-
-        def reverse(start):
-            end = len(nums) - 1
-            while start < end:
-                swap(start, end)
-                start += 1
-                end -= 1
-
-        def getNextLargest(idx):
-            for i in range(len(nums) - 1, idx, -1):
-                if nums[i] > nums[idx]:
-                    return i
-
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Step 1: Find the first number from right that's bigger than the next number
+        valley = -1
         for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                next_largest = getNextLargest(i)
-                swap(i, next_largest)
-                reverse(i + 1)
-                return
-
-        reverse(0)
-    
+            num = nums[i]
+            num_after = nums[i + 1]
+            
+            if num < num_after:
+                valley = i
+                break
+        
+        # If no valley found, reverse the entire array
+        if valley == -1:
+            nums.reverse()
+            return
+        
+        # Find the number just larger than nums[valley]
+        next_higher = len(nums) - 1
+        while nums[next_higher] <= nums[valley]:
+            next_higher -= 1
+        
+        # Swap the valley element with next_higher
+        nums[valley], nums[next_higher] = nums[next_higher], nums[valley]
+        
+        # Reverse the subarray after valley
+        left = valley + 1
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
