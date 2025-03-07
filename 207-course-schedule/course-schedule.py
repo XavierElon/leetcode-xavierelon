5c25a7b087f152b29e0143e55b4f54aeb5c0927d
Xavier <xavierelon93@gmail.com>
2025-03-07 09:11:49 -0800

    Time: 5 ms (60.61%) | Memory: 19 MB (73.34%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,7 +1,7 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         indegree = [0] * numCourses
-        adj_list =[[] for _ in range(numCourses)]
+        adj_list = [[] for _ in range(numCourses)]
         count = 0
 
         for prereq, course in prerequisites:
@@ -12,8 +12,8 @@ class Solution:
 
         for i in range(numCourses):
             if indegree[i] == 0:
-                count += 1
                 queue.append(i)
+                count += 1
 
         while queue:
             current = queue.popleft()
@@ -21,7 +21,7 @@ class Solution:
             for course in adj_list[current]:
                 indegree[course] -= 1
                 if indegree[course] == 0:
-                    count += 1
                     queue.append(course)
+                    count += 1
 
         return count == numCourses
