3fb3179d6c91c466cb003929ddb7881f251eb2d7
Xavier <xavierelon93@gmail.com>
2025-03-10 09:48:56 -0700

    Time: 0 ms (100.00%) | Memory: 18.8 MB (98.57%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,40 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1, -1]
-        if target < nums[0] or target > nums[-1]:
-            return [-1, -1]
-        
-        # Initialize result list with two elements
-        result = [0, 0]
-        
-        # Find leftmost occurrence
-        left = 0
-        right = len(nums) - 1
+        lower = self.lower(nums, target, True)
+        upper = self.lower(nums, target, False)
+
+        return [lower, upper]
+
+    def lower(self, nums, target, left_sided):
+        left, right = 0, len(nums) - 1
+        result = -1
+
         while left <= right:
-            mid = (right - left) // 2 + left
-            
-            if nums[mid] >= target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        
-        # If target not found
-        if nums[left] != target:
-            return [-1, -1]
-        result[0] = left
-        
-        # Find rightmost occurrence
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (right - left) // 2 + left
-            
-            if nums[mid] <= target:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                result = mid
+                if left_sided:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
-        
-        result[1] = right
         
         return result
