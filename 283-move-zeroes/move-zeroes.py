e290acea21c8d0b589f7b8d1cb18d07ce517a05b
Xavier <xavierelon93@gmail.com>
2025-03-10 16:38:01 -0700

    Time: 4 ms (64.38%) | Memory: 18.8 MB (78.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -7,8 +7,5 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[last_non_zero] = nums[i]
+                nums[i], nums[last_non_zero] = nums[last_non_zero], nums[i]
                 last_non_zero += 1
-
-        for i in range(last_non_zero, len(nums)):
-            nums[i] = 0
