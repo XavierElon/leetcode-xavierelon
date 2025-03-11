59deb50bacc4c7bead7faa1689b451f9571f5157
Xavier <xavierelon93@gmail.com>
2025-03-11 12:22:25 -0700

    Time: 53 ms (27.33%) | Memory: 19.1 MB (81.07%) - LeetSync

--- 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.py
+++ 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.py
@@ -12,20 +12,30 @@ class Solution:
         :type root: 'Node'
         :rtype: int
         """
-        def dfs(node):
-            if not node:
+        diameter = 0
+
+        def height(node):
+            nonlocal diameter
+
+            if node is None:
                 return 0
 
-            max_depth = 0
+            max_height = 0
+            second_max_height = 0
 
             for child in node.children:
-                depth = 1 + dfs(child)
-                self.diameter = max(self.diameter, depth + max_depth)
-                max_depth = max(max_depth, depth)
+                child_height = height(child)
 
-            return max_depth
-        
-        self.diameter = 0
-        dfs(root)
+                if child_height > max_height:
+                    second_max_height = max_height
+                    max_height = child_height
+                elif child_height > second_max_height:
+                    second_max_height = child_height
 
-        return self.diameter
+            diameter = max(diameter, max_height + second_max_height)
+
+            return max_height + 1
+
+        if root:
+            height(root)
+        return diameter
