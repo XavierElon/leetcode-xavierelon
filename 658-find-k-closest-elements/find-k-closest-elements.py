f7208df3b3cdd231537541ffeb7a1165fc50c11d
Xavier <xavierelon93@gmail.com>
2025-03-12 14:13:58 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (82.81%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -3,11 +3,20 @@ class Solution:
         left, right = 0, len(nums) - k
 
         while left < right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
             if target - nums[mid] > nums[mid + k] - target:
                 left = mid + 1
             else:
                 right = mid
         return nums[left:left + k]
 
-        
+"""
+length = 9
+        0.      4.      8
+nums = [1,1,2,2,2,2,2,3,3], target = 3, k = 3
+                L    R     
+left = 0
+right = 6
+mid = 3
+3 - 2 > 2 - 3
+"""
