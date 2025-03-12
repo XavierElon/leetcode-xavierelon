f32c090644eb223c82592bf7035e4e9edaca0941
Xavier <xavierelon93@gmail.com>
2025-03-12 12:10:45 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (14.82%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,33 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Step 1: Find the first number from right that's bigger than the next number
-        valley = -1
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        def reverse(start):
+            end = len(nums) - 1
+            while start < end:
+                swap(start, end)
+                start += 1
+                end -= 1
+
+        def getNextLargest(idx):
+            for i in range(len(nums) - 1, idx, -1):
+                if nums[i] > nums[idx]:
+                    return i
+
         for i in range(len(nums) - 2, -1, -1):
-            num = nums[i]
-            num_after = nums[i + 1]
-            
-            if num < num_after:
-                valley = i
-                break
-        
-        # If no valley found, reverse the entire array
-        if valley == -1:
-            nums.reverse()
-            return
-        
-        # Find the number just larger than nums[valley]
-        next_higher = len(nums) - 1
-        while nums[next_higher] <= nums[valley]:
-            next_higher -= 1
-        
-        # Swap the valley element with next_higher
-        nums[valley], nums[next_higher] = nums[next_higher], nums[valley]
-        
-        # Reverse the subarray after valley
-        left = valley + 1
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+            if nums[i] < nums[i + 1]:
+                next_largest = getNextLargest(i)
+                swap(i, next_largest)
+                reverse(i + 1)
+                return
+
+        reverse(0)
