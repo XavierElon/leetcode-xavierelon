fae1bd75c31dac3b1ed373eb76f27d8d355ef77f
Xavier <xavierelon93@gmail.com>
2025-03-12 13:02:30 -0700

    Time: 43 ms (53.48%) | Memory: 28.2 MB (20.29%) - LeetSync

--- 398-random-pick-index/random-pick-index.py
+++ 398-random-pick-index/random-pick-index.py
@@ -5,9 +5,9 @@ class Solution:
         for i, num in enumerate(nums):
             self.num_to_indices[num].append(i)
 
+
     def pick(self, target: int) -> int:
         return random.choice(self.num_to_indices[target])
-        
 
 
 # Your Solution object will be instantiated and called as such:
