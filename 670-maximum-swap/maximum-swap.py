465259b509e55169ec0dbc7e1a0c8aa6ed9f6a8e
Xavier <xavierelon93@gmail.com>
2025-03-14 11:12:33 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.23%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -6,7 +6,7 @@ class Solution:
         for i, digit in enumerate(num_str):
             rightmost_index[int(digit)] = i
 
-        for i, digit in enumerate(num_str):
+        for i , digit in enumerate(num_str):
             for j in range(9, int(digit), -1):
                 if rightmost_index[j] > i:
                     num_str[i], num_str[rightmost_index[j]] = num_str[rightmost_index[j]], num_str[i]
