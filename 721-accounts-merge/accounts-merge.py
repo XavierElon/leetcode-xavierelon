ac9888c19082adaad6438346912475a54313517b
Xavier <xavierelon93@gmail.com>
2025-03-16 16:29:17 -0700

    Time: 22 ms (82.17%) | Memory: 23 MB (33.10%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -6,26 +6,26 @@ class Solution:
             emails = account[1:]
             canonical = emails[0]
             for email in emails[1:]:
-                adj_list[email].append(canonical)
                 adj_list[canonical].append(email)
+                adj_list[email].append(canonical)
 
         visited = set()
 
-        def dfs(email, same_emails):
+        def dfs(email, email_list):
             visited.add(email)
-            same_emails.append(email)
+            email_list.append(email)
             for neighbor in adj_list[email]:
                 if neighbor not in visited:
-                    dfs(neighbor, same_emails)
-
+                    dfs(neighbor, email_list)
+        
         merged = []
 
         for account in accounts:
             emails = account[1:]
             if emails[0] in visited:
                 continue
-            same_emails = []
-            dfs(emails[0], same_emails)
-            merged.append([account[0]] + sorted(same_emails))
+            email_list = []
+            dfs(emails[0], email_list)
+            merged.append([account[0]] + sorted(email_list))
 
         return merged
