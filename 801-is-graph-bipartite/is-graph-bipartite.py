2bc2b4c60af5f587ebd7a2a84da6980618974144
Xavier <xavierelon93@gmail.com>
2025-03-15 18:41:16 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (35.55%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -5,15 +5,18 @@ class Solution:
         for i in range(n):
             if i not in color:
                 color[i] = 1
-                queue = collections.deque([i])
+                queue = deque([i])
 
                 while queue:
                     u = queue.popleft()
+
                     for v in graph[u]:
                         if v not in color:
                             color[v] = -color[u]
                             queue.append(v)
-                        elif color[v] == color[u]:
+                        elif color[u] == color[v]:
                             return False
+
+                            
 
         return True
