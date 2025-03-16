41f0fa4963f4dccaada5541452c504d265a0c3da
Xavier <xavierelon93@gmail.com>
2025-03-16 13:52:51 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.93%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,26 +1,23 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:           
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if image[sr][sc] == color:
             return image
-        
-        original_color = image[sr][sc]
-        directions = [[-1,0],[1,0],[0,-1],[0,1]]
-        rows, cols = len(image), len(image[0])
 
+        rows, cols = len(image), len(image[0])
+        original_color = image[sr][sc]
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
         queue = deque([(sr, sc)])
-        image[sr][sc] = color
 
         while queue:
             row, col = queue.popleft()
             image[row][col] = color
+
             for dr, dc in directions:
                 new_r, new_c = row + dr, col + dc
                 if self.is_within_bounds(new_r, new_c, rows, cols) and image[new_r][new_c] == original_color:
-                    # image[new_r][new_c] = color
                     queue.append((new_r, new_c))
 
         return image
 
     def is_within_bounds(self, r, c, rows, cols):
         return 0 <= r < rows and 0 <= c < cols
-        
