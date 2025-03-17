d9fae31981b9f991484a13977a6a2f44bb3d14f0
Xavier <xavierelon93@gmail.com>
2025-03-17 16:08:08 -0700

    Time: 3 ms (51.15%) | Memory: 19.2 MB (11.46%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,5 @@ class Solution:
         for i, num in enumerate(nums):
             complement = target - num
             if complement in hashmap:
-                return [hashmap[complement], i]
+                return [i, hashmap[complement]]
             hashmap[num] = i
-
-            
