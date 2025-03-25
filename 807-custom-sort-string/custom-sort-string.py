76a13df0080527261d81cdf2ea6eaae817a058d1
Xavier <xavierelon93@gmail.com>
2025-03-25 16:10:48 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.34%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,16 +1,17 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        freq_map = {}
-        res = []
-
+        freq_map = defaultdict(int)
+        
         for char in s:
-            freq_map[char] = freq_map.get(char, 0) + 1
+            freq_map[char] += 1
+
+        res = []
 
         for char in order:
             if char in freq_map:
-                res.append(char * freq_map.get(char))
+                res.append(char * freq_map[char])
                 del freq_map[char]
-        
+
         for char in freq_map:
             res.append(char * freq_map[char])
 
