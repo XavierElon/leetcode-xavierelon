9c50007ae403b238d8b3d4425b9fd218c1edb7db
Xavier <xavierelon93@gmail.com>
2025-03-28 12:06:49 -0700

    Time: 1344 ms (36.83%) | Memory: 22.1 MB (25.24%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,29 +1,28 @@
 class SparseVector:
-    def __init__(self, nums):
-        # Convert dense vector to sparse representation (index, value) for non-zero elements
-        self.index_to_num = [(i, num) for i, num in enumerate(nums) if num != 0]
+    def __init__(self, nums: List[int]):
+        self.nonzeros = {}
+        for i, num in enumerate(nums):
+            if num != 0:
+                self.nonzeros[i] = num
 
-    def binary_search(self, index_to_num, target_index):
-        # Binary search to find value at target_index, return 0 if not found
-        left = 0
-        right = len(index_to_num) - 1
-        while left <= right:
-            mid = (right + left) // 2
-            if index_to_num[mid][0] == target_index:
-                return index_to_num[mid][1]
-            if index_to_num[mid][0] < target_index:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return 0
-
-    def dotProduct(self, vec):
-        # Calculate dot product using binary search on the larger vector
-        sum = 0
-        if len(self.index_to_num) < len(vec.index_to_num):
-            for index, value in self.index_to_num:
-                sum += value * self.binary_search(vec.index_to_num, index)
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        if len(self.nonzeros) > len(vec.nonzeros):
+            smaller = vec.nonzeros
+            larger = self.nonzeros
         else:
-            for index, value in vec.index_to_num:
-                sum += value * self.binary_search(self.index_to_num, index)
-        return sum
+            smaller = self.nonzeros
+            larger = vec.nonzeros
+
+        res = 0
+        for index, value in smaller.items():
+            if index in larger:
+                res += value * larger[index]
+
+        return res
+        
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
