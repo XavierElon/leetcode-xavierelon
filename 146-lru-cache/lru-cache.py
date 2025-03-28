f4d2f55cb26b0ad6eae4b2b0302d9f559c2feb0b
Xavier <xavierelon93@gmail.com>
2025-03-28 11:29:19 -0700

    Time: 162 ms (33.79%) | Memory: 78.8 MB (13.51%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,5 +1,5 @@
 class DoublyLinkedListNode:
-    def __init__(self, key, value):
+    def __init__(self, key=-1, value=-1):
         self.key = key
         self.value = value
         self.next = self.prev = None
@@ -9,11 +9,11 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.hashmap = defaultdict(None)
-        self.head = DoublyLinkedListNode(-1,-1)
-        self.tail = DoublyLinkedListNode(-1,-1)
-        self.head.next = self.tail
+        self.head = DoublyLinkedListNode()
+        self.tail = DoublyLinkedListNode()
         self.tail.prev = self.head
-
+        self.head.nexxt = self.tail
+        
     def get(self, key: int) -> int:
         if key in self.hashmap:
             node = self.hashmap[key]
@@ -29,26 +29,28 @@ class LRUCache:
             node.value = value
             self.remove(node)
             self.add(node)
-            self.hashmap[key] = node
+            return node.value
         else:
             if len(self.hashmap) >= self.capacity:
                 lru = self.head.next
-                del self.hashmap[lru.key]
                 self.remove(lru)
+                del self.hashmap[lru.key]
             new_node = DoublyLinkedListNode(key, value)
             self.add(new_node)
             self.hashmap[key] = new_node
 
     def add(self, node):
-        prev = self.tail.prev
-        prev.next = node
+        prev_node = self.tail.prev
+        prev_node.next = node
         node.next = self.tail
         self.tail.prev = node
-        node.prev = prev
+        node.prev = prev_node
 
     def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
         
 
