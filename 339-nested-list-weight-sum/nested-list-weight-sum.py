bf08090e22a14f7b91f2740be9a5d941928683fb
Xavier <xavierelon93@gmail.com>
2025-03-28 08:33:44 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (13.02%) - LeetSync

---
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -0,0 +1,59 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        curr_depth = 1
+        weight_sum = 0
+        queue = deque([(nestedList, curr_depth)])
+
+        while queue:
+            curr_nested_list, curr_depth = queue.popleft()
+
+            for element in curr_nested_list:
+                if element.isInteger():
+                    weight_sum += element.getInteger() * curr_depth
+                else:
+                    queue.append((element.getList(), curr_depth + 1))
+
+        return weight_sum
