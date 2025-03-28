1f719d2b6989979c7bf3197ecb0fa7ae15695926
Xavier <xavierelon93@gmail.com>
2025-03-28 11:18:09 -0700

    Time: 140 ms (58.57%) | Memory: 78.3 MB (76.67%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,33 +2,33 @@ class DoublyLinkedListNode:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.head = self.prev = None
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.hashmap = {}
-        self.tail = DoublyLinkedListNode(-1,-1)
+        self.hashmap = defaultdict(None)
         self.head = DoublyLinkedListNode(-1,-1)
-        self.tail.prev = self.head
+        self.tail = DoublyLinkedListNode(-1,-1)
         self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
         if key in self.hashmap:
             node = self.hashmap[key]
             self.remove(node)
-            self.add_to_back(node)
+            self.add(node)
             return node.value
         else:
             return -1
-
+        
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
             node = self.hashmap[key]
             node.value = value
             self.remove(node)
-            self.add_to_back(node)
+            self.add(node)
             self.hashmap[key] = node
         else:
             if len(self.hashmap) >= self.capacity:
@@ -36,19 +36,20 @@ class LRUCache:
                 del self.hashmap[lru.key]
                 self.remove(lru)
             new_node = DoublyLinkedListNode(key, value)
-            self.add_to_back(new_node)
+            self.add(new_node)
             self.hashmap[key] = new_node
 
-    def add_to_back(self, node):
-        prev_node = self.tail.prev
-        prev_node.next = node
+    def add(self, node):
+        prev = self.tail.prev
+        prev.next = node
         node.next = self.tail
         self.tail.prev = node
-        node.prev = prev_node
+        node.prev = prev
 
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
+
         
 
 
