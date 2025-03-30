62f87adb5475f0d3c76744e01776ae9254a7ecb1
Xavier <xavierelon93@gmail.com>
2025-03-29 17:06:38 -0700

    Time: 27 ms (92.11%) | Memory: 18.9 MB (88.49%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,30 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        first_pass = []
-        open_count = 0
+        s = list(s)
+        stack = []
 
-        # First pass: remove invalid closing parentheses
-        for char in s:
+        for i, char in enumerate(s):
             if char == '(':
-                open_count += 1
-                first_pass.append(char)
+                stack.append(i)
             elif char == ')':
-                if open_count > 0:
-                    open_count -= 1
-                    first_pass.append(char)
-            else:
-                first_pass.append(char)
-        
-        # Short-circuit if there are no unmatched opening parentheses
-        if open_count == 0:
-            return ''.join(first_pass)
-        
-        # Second pass: remove excess opening parentheses
-        result = []
-        for char in reversed(first_pass):
-            if char == '(' and open_count > 0:
-                open_count -= 1
-            else:
-                result.append(char)
-        
-        return ''.join(reversed(result))
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+
+        for i in stack:
+            s[i] = ''
+
+        return ''.join(s)
