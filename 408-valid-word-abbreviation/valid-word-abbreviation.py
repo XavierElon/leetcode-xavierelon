ab2290dbac4c48250f4fc4f475940b31fec12d6d
Xavier <xavierelon93@gmail.com>
2025-03-31 06:47:11 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (18.89%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -4,17 +4,16 @@ class Solution:
 
         while p1 < len(word) and p2 < len(abbr):
             if abbr[p2].isdigit():
-                shift = 0
-
                 if abbr[p2] == '0':
                     return False
 
+                shift = 0
                 while p2 < len(abbr) and abbr[p2].isdigit():
-                    shift = (shift * 10) + int(abbr[p2])
+                    shift = shift * 10 + int(abbr[p2])
                     p2 += 1
-                
-                p1 += shift
 
+                p1 += shift
+            
             elif word[p1] != abbr[p2]:
                 return False
 
@@ -23,11 +22,3 @@ class Solution:
                 p2 += 1
 
         return p1 == len(word) and p2 == len(abbr)
-
-'''
-p1 = 13
-p2 = 2
-word[p1] = n
-abbr[p2] = 12
-shift = 12
-'''
