f77ac9302a432a27089bb450c188a7f038e4b60a
Xavier <xavierelon93@gmail.com>
2025-03-31 06:21:24 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (62.37%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -7,6 +7,7 @@ class Solution:
                 shift = 0
                 if abbr[p2] == '0':
                     return False
+
                 while p2 < len(abbr) and abbr[p2].isdigit():
                     shift = (shift * 10) + int(abbr[p2])
                     p2 += 1
@@ -19,5 +20,5 @@ class Solution:
             else:
                 p1 += 1
                 p2 += 1
-
+                
         return p1 == len(word) and p2 == len(abbr)
