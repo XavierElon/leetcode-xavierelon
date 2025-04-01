f34ca6c4b23d581aafe9d0aa18bd995459fc94b3
Xavier <xavierelon93@gmail.com>
2025-04-01 16:04:19 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (31.74%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -9,16 +9,8 @@ class Solution:
         if not root:
             return None
 
-        queue = deque([root])
-
-        while queue:
-            node = queue.popleft()
-
-            node.left, node.right = node.right, node.left
-
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
 
         return root
+
+            
