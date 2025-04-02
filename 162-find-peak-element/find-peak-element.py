3f0aeb1f48d0b95244e444acf60d7664ba70e363
Xavier <xavierelon93@gmail.com>
2025-04-02 14:25:07 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (19.48%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,9 +4,8 @@ class Solution:
 
         while l < r:
             mid = (l + r) // 2
-
             if nums[mid] > nums[mid + 1]:
-                r = mid 
+                r = mid
             else:
                 l = mid + 1
 
