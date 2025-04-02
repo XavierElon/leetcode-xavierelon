6ef1581ca072c0c5c18c1a8c11050308fabdb64c
Xavier <xavierelon93@gmail.com>
2025-04-01 17:30:52 -0700

    Time: 88 ms (32.94%) | Memory: 18.9 MB (92.29%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,43 +1,38 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s
         i = 0
         cur = prev = res = 0
         n = len(s)
         operation = '+'
 
         while i < n:
-            curr_char = s[i]
-            if curr_char.isdigit():
+            
+            if s[i].isdigit():
                 while i < n and s[i].isdigit():
-                    cur = cur * 10 + int(s[i])
+                    cur = cur * 10 + int(s[i]) 
                     i += 1
+                
                 i -= 1
                 
                 if operation == '+':
                     res += cur
                     prev = cur
                 elif operation == '-':
-                    res -= cur 
+                    res -= cur
                     prev = -cur
                 elif operation == '*':
                     res -= prev
-
-                    res += cur * prev
-
-                    prev = cur * prev
-
+                    res += prev * cur
+                    prev = prev * cur
                 else:
                     res -= prev
-
                     res += int(prev/cur)
                     prev = int(prev/cur)
-                
-                cur = 0
 
-            elif curr_char != ' ':
-                operation = curr_char
+            elif s[i] != ' ':
+                operation = s[i]
 
             i += 1
+            cur = 0
 
         return res
