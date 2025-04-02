e0062d7663f2a47b6367db7f0f01f869992949e4
Xavier <xavierelon93@gmail.com>
2025-04-02 14:42:26 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (39.46%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -6,7 +6,8 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] > nums[mid + 1]:
                 r = mid
-            else: 
+            else:
                 l = mid + 1
 
         return l
+
