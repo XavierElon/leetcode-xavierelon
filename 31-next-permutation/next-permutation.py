d3dc6bfd43698855bf505810ee99939a046d7321
Xavier <xavierelon93@gmail.com>
2025-04-01 18:33:10 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (57.34%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,12 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         pivot = None
-
         for i in range(len(nums) - 1, 0, -1):
             if nums[i] > nums[i-1]:
-                pivot = i-1
+                pivot = i - 1
                 break
-
         else:
             nums.reverse()
             return
@@ -17,7 +15,9 @@ class Solution:
         swap = len(nums) - 1
         while nums[swap] <= nums[pivot]:
             swap -= 1
-
+        
         nums[pivot], nums[swap] = nums[swap], nums[pivot]
 
         nums[pivot+1:] = reversed(nums[pivot+1:])
+            
+
