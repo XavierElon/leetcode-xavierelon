58e0b016b14ac9707b83b1acd8e2efca44e9a73d
Xavier <xavierelon93@gmail.com>
2025-04-01 18:20:16 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.45%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -5,21 +5,22 @@ class Solution:
         """
         pivot = None
 
-        for i in range(len(nums) - 1, 0, -1):
+        for i in range(len(nums) -1, 0, -1):
             if nums[i] > nums[i - 1]:
                 pivot = i - 1
 
                 break
+
         else:
             nums.reverse()
 
             return
 
-        swap = len(nums) - 1 
-        
+        swap = len(nums) - 1
+
         while nums[swap] <= nums[pivot]:
             swap -= 1
-        
+
         nums[pivot], nums[swap] = nums[swap], nums[pivot]
 
-        nums[pivot+1:] = reversed(nums[pivot + 1:])
+        nums[pivot+1:] = reversed(nums[pivot+1:])
