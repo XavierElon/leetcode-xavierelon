1198225ca9d56af491df92374edc9a24c929e3d8
Xavier <xavierelon93@gmail.com>
2025-04-02 11:51:36 -0700

    Time: 23 ms (74.68%) | Memory: 18.3 MB (63.54%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -12,23 +12,24 @@ class Solution:
                 num = num * 10 + int(c)
             elif c == '+':
                 res += num * sign
-                num = 0
                 sign = 1
+                num = 0
             elif c == '-':
                 res += num * sign
-                num = 0
                 sign = -1
+                num = 0
             elif c == '(':
                 stack.append(res)
                 stack.append(sign)
                 res = 0
                 sign = 1
+
             elif c == ')':
-                res += sign * num
-                num = 0
+                res += num * sign
                 prev_sign = stack.pop()
                 prev_res = stack.pop()
-                res = prev_res + prev_sign * res
-        res += sign * num
-        return res                
+                res =  prev_res + res * prev_sign
+                num = 0
 
+        res += num * sign
+        return res
