66c0b21f80ec9b82c38b4f92e50b06103b285545
Xavier <xavierelon93@gmail.com>
2025-04-03 16:20:53 -0700

    Time: 7 ms (73.97%) | Memory: 20.9 MB (89.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,10 +3,11 @@ class Solution:
         if not intervals:
             return []
 
+        n = len(intervals)
         intervals.sort(key=lambda x: x[0])
         merged = [intervals[0]]
 
-        for i in range(1, len(intervals)):
+        for i in range(1, n):
             prev_start, prev_end = merged[-1]
             curr_start, curr_end = intervals[i]
 
