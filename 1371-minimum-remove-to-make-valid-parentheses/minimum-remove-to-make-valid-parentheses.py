fc84cc5b0416d6e338342765aba94e04c0d04542
Xavier <xavierelon93@gmail.com>
2025-04-04 12:28:13 -0700

    Time: 39 ms (72.60%) | Memory: 18.9 MB (88.71%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,11 +1,12 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         s = list(s)
+        n = len(s)
         count = 0
 
-        for i in range(len(s)):
+        for i in range(n):
             if s[i] == '(':
-                count += 1
+                count +=1
             elif s[i] == ')':
                 if count > 0:
                     count -= 1
@@ -14,13 +15,13 @@ class Solution:
         
         count = 0
 
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(n-1, -1, -1):
             if s[i] == ')':
                 count += 1
             elif s[i] == '(':
                 if count > 0:
-                    count -=1 
+                    count -= 1
                 else:
                     s[i] = ''
-
+        
         return ''.join(s)
