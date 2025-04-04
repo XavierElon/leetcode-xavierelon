e669ca28d4af62aa10919341e08eb7fba3422dd5
Xavier <xavierelon93@gmail.com>
2025-04-03 19:00:49 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.30%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -7,20 +7,20 @@ class Solution:
             if abbr[p2].isdigit():
                 if abbr[p2] == '0':
                     return False
-
+                
                 shift = 0
 
                 while p2 < n and abbr[p2].isdigit():
                     shift = shift * 10 + int(abbr[p2])
                     p2 += 1
-                
+
                 p1 += shift
 
-            elif word[p1] != abbr[p2]:
+            elif abbr[p2] != word[p1]:
                 return False
-
             else:
                 p1 += 1
                 p2 += 1
-
+        
         return p1 == m and p2 == n
+                
