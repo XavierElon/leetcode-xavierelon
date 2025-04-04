1eb2a7c1d7bfc996446c7881c6c6c77ca10a16d6
Xavier <xavierelon93@gmail.com>
2025-04-04 09:28:53 -0700

    Time: 53 ms (80.53%) | Memory: 31.8 MB (94.56%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        res = []
+        queue = deque([(0,0)])
+
+        while queue:
+            row, col = queue.popleft()
+            res.append(nums[row][col])
+
+            if col == 0 and row + 1 < len(nums):
+                queue.append((row + 1, col))
+
+            if col + 1 < len(nums[row]):
+                queue.append((row, col + 1))
+
+        return res
