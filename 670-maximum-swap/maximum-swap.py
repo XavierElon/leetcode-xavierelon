39ef84d5c7f4d95300e27cf542c8899463e6fbfe
Xavier <xavierelon93@gmail.com>
2025-04-04 10:40:00 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (13.86%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -8,12 +8,11 @@ class Solution:
         for i in range(n):
             current_digit_char = s[i]
 
-            for d_char in '987654321':
-                if d_char <= current_digit_char:
+            for char in '987654321':
+                if char <= current_digit_char:
                     break
-
-                if d_char in last_occurrence and last_occurrence[d_char] > i:
-                    swap_idx = last_occurrence[d_char]
+                if char in last_occurrence and last_occurrence[char] > i:
+                    swap_idx = last_occurrence[char]
                     s[i], s[swap_idx] = s[swap_idx], s[i]
 
                     return int(''.join(s))
