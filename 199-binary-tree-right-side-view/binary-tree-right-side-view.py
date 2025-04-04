212a25aad30b888a9ed63509df0b355d17d880dc
Xavier <xavierelon93@gmail.com>
2025-04-03 17:19:45 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.08%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -9,17 +9,15 @@ class Solution:
         if not root:
             return []
 
-        queue = deque([root])
         res = []
+        queue = deque([root])
 
         while queue:
             level_size = len(queue)
             for i in range(level_size):
                 node = queue.popleft()
-                
                 if i == level_size - 1:
                     res.append(node.val)
-                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
