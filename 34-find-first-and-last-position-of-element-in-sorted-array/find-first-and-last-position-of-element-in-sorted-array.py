e588c411154c1794b27ce336b2f0378571c01d35
Xavier <xavierelon93@gmail.com>
2025-04-03 19:46:05 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (74.60%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         lower = self.lower(nums, target)
-        upper = self.upper(nums, target)
+        higher = self.higher(nums, target)
 
-        return [lower, upper]
+        return [lower, higher]
 
     def lower(self, nums, target):
         l, r = 0, len(nums) - 1
@@ -13,16 +13,14 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] == target:
                 res = mid
-                r = mid - 1 
+                r = mid - 1
             elif nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
-            
-
         return res
 
-    def upper(self, nums, target):
+    def higher(self, nums, target):
         l, r = 0, len(nums) - 1
         res = -1
 
@@ -31,9 +29,9 @@ class Solution:
             if nums[mid] == target:
                 res = mid
                 l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
+            elif nums[mid] < target:
                 l = mid + 1
+            else:
+                r = mid - 1
 
         return res
