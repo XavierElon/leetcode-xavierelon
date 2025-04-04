2290e2fec530f59045177d9f8541f73fff2ca29b
Xavier <xavierelon93@gmail.com>
2025-04-04 12:05:15 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (74.75%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -4,15 +4,15 @@ class Solution:
             return 0
         if n == 0:
             return 1
-
+        
         abs_n = abs(n)
-        result = 1.0
         base = x
+        result = 1.0
 
         while abs_n > 0:
             if abs_n % 2 == 1:
                 result *= base
             base *= base
             abs_n //= 2
-        
-        return result if n >= 0 else 1 / result
+
+        return result if n >=0 else 1 / result
