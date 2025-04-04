3b6eadcda1957cb54429c4bf954d44e2cfb33577
Xavier <xavierelon93@gmail.com>
2025-04-04 12:17:03 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.67%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,6 +1,6 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        p1 = p2 =0
+        p1 = p2 = 0
         m, n = len(word), len(abbr)
 
         while p1 < m and p2 < n:
@@ -12,10 +12,11 @@ class Solution:
                     shift = shift * 10 + int(abbr[p2])
                     p2 += 1
                 p1 += shift
+
             elif abbr[p2] != word[p1]:
                 return False
             else:
                 p1 += 1
                 p2 += 1
-            
+
         return p1 == m and p2 == n
