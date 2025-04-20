01b7125ceacf63525d64a6431504c8d3e9c9b0a6
Xavier <xavierelon93@gmail.com>
2025-04-20 16:23:20 -0700

    Time: 47 ms (5.97%) | Memory: 21.2 MB (47.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
+        base = ord('a')
 
         for s in strs:
             counts = [0] * 26
 
             for ch in s:
-                counts[ord(ch) - 97] += 1
+                counts[ord(ch) - base] += 1
             
             key = '#'.join(map(str, counts))
             groups[key].append(s)
