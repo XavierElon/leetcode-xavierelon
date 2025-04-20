8f4bcb11b64cd405a79aec319b1b50cbc4b5062a
Xavier <xavierelon93@gmail.com>
2025-04-20 15:31:38 -0700

    Time: 3 ms (98.36%) | Memory: 17.9 MB (64.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,16 +3,7 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        char_count = {}
+        freq_s_counter = Counter(s)
+        freq_t_counter = Counter(t)
 
-        for char in s:
-            char_count[char] = char_count.get(char, 0) + 1
-
-        for char in t:
-            if char not in char_count:
-                return False
-            char_count[char] -= 1
-            if char_count[char] == 0:
-                del char_count[char]
-
-        return len(char_count) == 0
+        return freq_s_counter == freq_t_counter
