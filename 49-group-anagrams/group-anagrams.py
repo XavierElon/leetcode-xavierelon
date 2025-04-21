c9837162056ddc17d479495ad92326cfd7eb3dc6
Xavier <xavierelon93@gmail.com>
2025-04-21 10:08:42 -0700

    Time: 44 ms (6.58%) | Memory: 21 MB (50.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -7,8 +7,8 @@ class Solution:
             counts = [0] * 26
             for ch in s:
                 counts[ord(ch) - base] += 1
-
+            
             key = '#'.join(map(str, counts))
             groups[key].append(s)
-        
+
         return list(groups.values())
