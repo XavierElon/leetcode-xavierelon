933df5ebd877291017aab55bedfffb2109f2f01c
Xavier <xavierelon93@gmail.com>
2025-04-21 17:29:27 -0700

    Time: 7 ms (51.65%) | Memory: 22 MB (7.03%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -11,13 +11,12 @@ class MinStack:
 
     def pop(self) -> None:
         self.stack.pop()
-        
+
     def top(self) -> int:
         return self.stack[-1][0]
 
     def getMin(self) -> int:
         return self.stack[-1][1]
-        
 
 
 # Your MinStack object will be instantiated and called as such:
