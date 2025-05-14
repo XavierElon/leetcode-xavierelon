5df2fbee9825f1310a8079c7f5b1277883eb2a1e
Xavier <xavierelon93@gmail.com>
2025-05-14 11:36:48 -0700

    Time: 11 ms (95.31%) | Memory: 18 MB (34.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,8 +7,7 @@ class Solution:
         for right, char in enumerate(s):
             if char in char_index and char_index[char] >= left:
                 left = char_index[char] + 1
-
-            max_len = max(max_len, right-left+1)
+            max_len = max(max_len, right - left + 1)
 
             char_index[char] = right
 
