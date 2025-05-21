81f6e66d8262ce7dfdb39d6a5330b9b55172f81d
Xavier <xavierelon93@gmail.com>
2025-05-21 10:44:24 -0700

    Time: 0 ms (100.00%) | Memory: 23.4 MB (12.83%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -9,44 +9,27 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         if not head or not head.next or not head.next.next:
-         return  # Lists with 0, 1, or 2 nodes don't need reordering
-        
-        # Step 1: Find the middle of the linked list
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next       # Moves one step at a time
-            fast = fast.next.next  # Moves two steps at a time
-        
-        # Now 'slow' points to the middle node
-        
-        # Step 2: Reverse the second half of the linked list
-        second_half = slow.next  # Start of second half
-        slow.next = None         # Break the list into two halves
-        
-        # Reverse the second half
-        prev = None
-        current = second_half
-        while current:
-            next_temp = current.next  # Store next node
-            current.next = prev       # Reverse the pointer
-            prev = current            # Move prev forward
-            current = next_temp       # Move current forward
-        
-        reversed_second_half = prev   # New head of the reversed second half
-        
-        # Step 3: Merge the two halves in alternating fashion
-        first = head
-        second = reversed_second_half
-        
-        while second:  # Second half could be shorter (or equal) if list has odd length
-            # Save next pointers
-            first_next = first.next
-            second_next = second.next
-            
-            # Interweave the nodes
+            return
+
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, curr = None, slow.next
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        slow.next = None
+
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+
             first.next = second
-            second.next = first_next
-            
-            # Move to the next pair of nodes
-            first = first_next
-            second = second_next
+            second.next = temp1
+
+            first = temp1
+            second = temp2
