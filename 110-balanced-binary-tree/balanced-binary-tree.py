0809e58559e599eeeab6e59435f587f130487d2c
Xavier <xavierelon93@gmail.com>
2025-06-03 09:27:45 -0700

    Time: 7 ms (31.70%) | Memory: 18.8 MB (89.55%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -13,9 +13,9 @@ class Solution:
             left = height(node.left)
             right = height(node.right)
 
-            if left == -1 or right == -1 or abs(left - right) > 1:
+            if left == -1 or right == -1 or abs(right - left) > 1:
                 return -1
-            
+
             return max(left, right) + 1
 
         return height(root) != -1
