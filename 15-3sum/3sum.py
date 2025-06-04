e0f116978d81608e9cbe1cb9023294ff4e4b8014
Xavier <xavierelon93@gmail.com>
2025-06-04 15:21:09 -0700

    Time: 543 ms (73.62%) | Memory: 20.7 MB (37.64%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,7 +6,7 @@ class Solution:
         for i, val in enumerate(nums):
             if i > 0 and val == nums[i-1]:
                 continue
-            
+
             l, r = i + 1, len(nums) - 1
 
             while l < r:
@@ -25,4 +25,5 @@ class Solution:
                         l += 1
                     while l < r and nums[r] == nums[r+1]:
                         r -= 1
+
         return res
