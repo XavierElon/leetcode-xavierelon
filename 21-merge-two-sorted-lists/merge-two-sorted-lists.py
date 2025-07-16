7dc67a7778b7e06652b285e56574117b9256dc97
Xavier <xavierelon93@gmail.com>
2025-07-16 16:09:35 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.83%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,14 +10,14 @@ class Solution:
         current = prehead
 
         while list1 and list2:
-            if list1.val <= list2.val:
+            if list1.val < list2.val:
                 current.next = list1
                 list1 = list1.next
             else:
                 current.next = list2
-                list2 = list2.next 
+                list2 = list2.next
+
             current = current.next
 
         current.next = list1 if list1 else list2
-
         return prehead.next
