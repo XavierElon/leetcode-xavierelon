ad31f19def80e61740e73b511bd4cb609ae8cb39
Xavier <xavierelon93@gmail.com>
2025-07-23 10:58:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 0:
-            return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        if n <= 2:
+            return n
 
-        one_step_before = 2
-        two_steps_before = 1
+        one_step_before, two_steps_before = 2, 1
 
         for i in range(3, n + 1):
             current = one_step_before + two_steps_before
@@ -16,4 +11,3 @@ class Solution:
             one_step_before = current
 
         return one_step_before
-        
