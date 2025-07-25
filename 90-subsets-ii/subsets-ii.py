c6dad07c3e2231e2317e93ba4966c075356452f1
Xavier <xavierelon93@gmail.com>
2025-07-25 15:13:26 -0700

    Time: 2 ms (28.69%) | Memory: 17.9 MB (57.38%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -5,10 +5,11 @@ class Solution:
 
         def backtrack(start, curr_subset):
             res.append(curr_subset[:])
+
             for i in range(start, len(nums)):
                 if i > start and nums[i] == nums[i-1]:
                     continue
-                
+
                 curr_subset.append(nums[i])
                 backtrack(i + 1, curr_subset)
                 curr_subset.pop()
