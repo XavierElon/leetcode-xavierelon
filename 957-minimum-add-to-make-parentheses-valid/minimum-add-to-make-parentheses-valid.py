c8b355ee792882e066c697a48eb04178d7f1429e
Xavier <xavierelon93@gmail.com>
2025-07-25 12:27:35 -0700

    Time: 3 ms (13.95%) | Memory: 17.6 MB (81.59%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -2,13 +2,13 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         unmatched_open_count = unmatched_close_count = 0
 
-        for i in range(len(s)):
-            if s[i] == '(':
+        for ch in s:
+            if ch == '(':
                 unmatched_open_count += 1
-            elif s[i] == ')':
+            elif ch == ')':
                 if unmatched_open_count > 0:
                     unmatched_open_count -= 1
                 else:
                     unmatched_close_count += 1
-                    
+
         return unmatched_open_count + unmatched_close_count
