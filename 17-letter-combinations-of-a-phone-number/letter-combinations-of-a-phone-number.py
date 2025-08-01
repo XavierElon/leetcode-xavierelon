5685836a19aa20491b74dc4812ccb81579ceb86f
Xavier <xavierelon93@gmail.com>
2025-08-01 15:56:33 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.01%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -7,19 +7,16 @@ class Solution:
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl','6': 'mno','7': 'pqrs','8': 'tuv', '9': 'wxyz'
         }
 
-        res = []
-
-        def backtrack(i: int, current: List[str]) -> None:
-            if len(current) == len(digits):
-                res.append("".join(current))
+        def backtrack(index: int, path: List[str]):
+            if len(path) == len(digits):
+                res.append("".join(path))
                 return
 
-            for letter in keypad_map[digits[i]]:
-                current.append(letter)
-
-                backtrack(i + 1, current)
-
-                current.pop()
-
+            for letter in keypad_map[digits[index]]:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+        
+        res = []
         backtrack(0, [])
         return res
