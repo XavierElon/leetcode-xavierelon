7dc48e6380c8b3c782ae220f590aa5b9be1ab761
Xavier <xavierelon93@gmail.com>
2025-08-01 16:42:51 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (33.35%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,22 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(current, used) -> None:
-            if len(current) == len(nums):
-                res.append(current.copy())
+    def permute(self, nums):
+        
+
+        def backtrack(nums, candidate, used, res):
+            if len(candidate) == len(nums):
+                res.append(candidate[:])
                 return
 
             for num in nums:
                 if num not in used:
-                    current.append(num)
+                    candidate.append(num)
                     used.add(num)
-                    backtrack(current, used)
-                    current.pop()
+
+                    backtrack(nums, candidate, used, res)
+
+                    candidate.pop()
                     used.remove(num)
 
         res = []
-        backtrack([], set())
+        backtrack(nums, [], set(), res)
         return res
