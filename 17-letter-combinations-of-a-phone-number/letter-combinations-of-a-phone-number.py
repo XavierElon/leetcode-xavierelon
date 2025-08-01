27fa530fc65688b7a8d396b032288a18d6a224b6
Xavier <xavierelon93@gmail.com>
2025-08-01 16:11:36 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.96%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -7,7 +7,7 @@ class Solution:
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl','6': 'mno','7': 'pqrs','8': 'tuv', '9': 'wxyz'
         }
 
-        def backtrack(index: int, path: List[str]):
+        def backtrack(index, path):
             if len(path) == len(digits):
                 res.append("".join(path))
                 return
@@ -16,7 +16,7 @@ class Solution:
                 path.append(letter)
                 backtrack(index + 1, path)
                 path.pop()
-        
+
         res = []
         backtrack(0, [])
         return res
