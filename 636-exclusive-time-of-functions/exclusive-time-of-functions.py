a030298e644f85566e50860b50703815edef167a
Xavier <xavierelon93@gmail.com>
2025-08-06 12:53:40 -0700

    Time: 7 ms (68.72%) | Memory: 17.9 MB (43.01%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,6 +1,6 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        times = [0] * n
+        exc_times = [0] * n
         stack = []
         prev_time = 0
 
@@ -12,11 +12,13 @@ class Solution:
 
             if event == 'start':
                 if stack:
-                    times[stack[-1]] += time - prev_time
+                    exc_times[stack[-1]] += time - prev_time
                 stack.append(func_id)
                 prev_time = time
             else:
                 curr_id = stack.pop()
-                times[curr_id] += (time - prev_time) + 1
+                elapsed_time = time - prev_time + 1
+                exc_times[curr_id] += elapsed_time
                 prev_time = time + 1
-        return times
+
+        return exc_times
