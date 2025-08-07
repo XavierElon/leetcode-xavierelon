5f0a465a2ec6556eab1237f479f2aabcbed7956c
Xavier <xavierelon93@gmail.com>
2025-08-07 12:31:07 -0700

    Time: 3 ms (81.83%) | Memory: 18 MB (5.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,15 +1,16 @@
 class Solution:
-    def orangesRotting(self, matrix: List[List[int]]) -> int:
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+
         queue = deque()
         ones = seconds = 0
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == 1:
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
                     ones += 1
-                elif matrix[r][c] == 2:
-                    queue.append((r, c))
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
 
         while queue and ones > 0:
             seconds += 1
@@ -20,12 +21,13 @@ class Solution:
                 for dr, dc in dirs:
                     new_r, new_c = r + dr, c + dc
 
-                    if self.is_within_bounds(new_r, new_c, matrix) and matrix[new_r][new_c] == 1:
-                        matrix[new_r][new_c] = 2
+                    if self.is_within_bounds(new_r, new_c, grid) and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
                         ones -= 1
                         queue.append((new_r, new_c))
 
         return seconds if ones == 0 else -1
+
     
-    def is_within_bounds(self, r, c, matrix):
-        return 0 <= r < len(matrix) and 0 <= c < len(matrix[0])
+    def is_within_bounds(self, r, c, grid):
+        return 0 <= r and r < len(grid) and 0 <= c and c < len(grid[0])
