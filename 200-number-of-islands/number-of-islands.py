9838346e93951d47c8573cf5a495da8938b11bc6
Xavier <xavierelon93@gmail.com>
2025-08-07 11:27:06 -0700

    Time: 263 ms (37.26%) | Memory: 20.3 MB (57.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,27 @@
 class Solution:
-    def numIslands(self, matrix: List[List[str]]) -> int:
-        if not matrix:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
             return 0
 
         count = 0
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == '1':
-                    self.dfs(r, c, matrix)
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    self.dfs(r, c, grid)
                     count += 1
 
         return count
-        
 
-    def dfs(self, r, c, matrix):
-        matrix[r][c] = '0'
+    def dfs(self, r, c, grid):
+        grid[r][c] = '0'
         dirs = [(-1,0),(1,0),(0,-1),(0,1)]
 
         for dr, dc in dirs:
             new_r, new_c = r + dr, c + dc
-            
-            if (self.is_within_bounds(new_r, new_c, matrix) and matrix[new_r][new_c] == '1'):
-                self.dfs(new_r, new_c, matrix)
 
-    def is_within_bounds(self, r, c, matrix):
-        return 0 <= r < len(matrix) and 0 <= c < len(matrix[0])
+            if (self.is_within_bounds(new_r, new_c, grid) and grid[new_r][new_c] == '1'):
+                self.dfs(new_r, new_c, grid)
+
+    def is_within_bounds(self, r, c, grid):
+        return 0 <= r and r < len(grid) and 0 <= c and c < len(grid[0])
