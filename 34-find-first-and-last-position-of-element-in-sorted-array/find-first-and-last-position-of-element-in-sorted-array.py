36e2fbe4120a091cdc22fa52a79cdf68c03618c4
Xavier <xavierelon93@gmail.com>
2025-08-08 10:57:11 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (93.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -8,7 +8,6 @@ class Solution:
     def lower(self, nums, target):
         l, r = 0, len(nums) - 1
         res = -1
-
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target:
@@ -17,8 +16,8 @@ class Solution:
             elif nums[mid] < target:
                 l = mid + 1
             else:
-                r = mid -1
-        
+                r = mid - 1 
+
         return res
 
     def higher(self, nums, target):
@@ -31,7 +30,7 @@ class Solution:
                 res = mid
                 l = mid + 1
             elif nums[mid] > target:
-                r = mid - 1
+                r = mid -1
             else:
                 l = mid + 1
 
