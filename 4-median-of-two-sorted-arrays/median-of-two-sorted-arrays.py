c6e3a2d60a1db8691ea8675f43be3ffa60b243a3
Xavier <xavierelon93@gmail.com>
2025-08-08 12:29:40 -0700

    Time: 16 ms (5.62%) | Memory: 18.3 MB (19.00%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -9,19 +9,21 @@ class Solution:
             return (sorted_array[mid-1] + sorted_array[mid]) / 2
         else:
             return sorted_array[total // 2]
-    
+
     def build_sorted(self, nums1, nums2):
         sorted_array = []
 
         p1, p2 = 0, 0
-        while p1 < len(nums1) or p2 < len(nums2):
-            p1_val = float('inf') if p1 >= len(nums1) else nums1[p1]
-            p2_val = float('inf') if p2 >= len(nums2) else nums2[p2]
 
-            if p1_val < p2_val:
-                sorted_array.append(p1_val)
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] < nums2[p2]:
+                sorted_array.append(nums1[p1])
                 p1 += 1
             else:
-                sorted_array.append(p2_val)
+                sorted_array.append(nums2[p2])
                 p2 += 1
+
+        sorted_array.extend(nums1[p1:])
+        sorted_array.extend(nums2[p2:])
+
         return sorted_array
