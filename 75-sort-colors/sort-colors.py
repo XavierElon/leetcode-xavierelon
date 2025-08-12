3b3c5d80e451d3c55bbc881088ee8360751ddc73
Xavier <xavierelon93@gmail.com>
2025-08-12 13:01:53 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.57%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, left, right = 0, 0, len(nums) -1
+        i, left, right = 0, 0, len(nums) - 1
 
         while i <= right:
             if nums[i] == 0:
@@ -13,6 +13,5 @@ class Solution:
             elif nums[i] == 2:
                 nums[i], nums[right] = nums[right], nums[i]
                 right -= 1
-
             else:
                 i += 1
