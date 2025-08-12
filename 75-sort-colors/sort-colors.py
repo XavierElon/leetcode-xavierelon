dffedb3dbcfbba44a76f9cd33f07d525763b09ec
Xavier <xavierelon93@gmail.com>
2025-08-12 13:06:09 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.51%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -8,10 +8,11 @@ class Solution:
         while i <= right:
             if nums[i] == 0:
                 nums[i], nums[left] = nums[left], nums[i]
-                i += 1
                 left += 1
+                i += 1
             elif nums[i] == 2:
                 nums[i], nums[right] = nums[right], nums[i]
                 right -= 1
             else:
                 i += 1
+                
