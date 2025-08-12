ab91db61e315b1f2bdfe513f65a9c2ad8816e087
Xavier <xavierelon93@gmail.com>
2025-08-12 12:49:31 -0700

    Time: 0 ms (100.00%) | Memory: 18.5 MB (59.40%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -9,22 +9,23 @@ class Solution:
         if not root:
             return []
 
-        queue = deque([root])
-        
         res = []
+        levels = []
+        queue = deque([root])
 
         while queue:
-            level_size = len(queue)
             level = []
+            n = len(queue)
 
-            for i in range(level_size):
+            for _ in range(n):
                 node = queue.popleft()
+
                 level.append(node.val)
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            
-            res.append(level)
 
-        return res
+            levels.append(level)
+
+        return levels
