a3e2d6ce8b6a48871e764b081cbcbe3160327554
Xavier <xavierelon93@gmail.com>
2025-08-12 12:53:53 -0700

    Time: 25 ms (50.89%) | Memory: 23.3 MB (79.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,12 +5,12 @@ class Solution:
 
         left_product = 1
         for i in range(n):
-            res[i] = left_product
+            res[i] *= left_product
             left_product *= nums[i]
 
         right_product = 1
-        for i in range(n - 1, -1, -1):
+        for i in range(n-1, -1, -1):
             res[i] *= right_product
             right_product *= nums[i]
-        
+
         return res
