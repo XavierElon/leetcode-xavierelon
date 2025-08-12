1ad672fbb304863ff6b7bdda0a27421690c3f1f1
Xavier <xavierelon93@gmail.com>
2025-08-12 11:02:43 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (69.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,12 +3,12 @@ class Solution:
         def backtrack(open_rem, close_rem, cur):
             if open_rem == 0 and close_rem == 0:
                 res.append("".join(cur))
-            
+
             if open_rem > 0:
                 cur.append('(')
                 backtrack(open_rem - 1, close_rem, cur)
                 cur.pop()
-            
+
             if close_rem > open_rem:
                 cur.append(')')
                 backtrack(open_rem, close_rem - 1, cur)
