796004cd2e9b051062dccae65fbd196bc4fd0649
Xavier <xavierelon93@gmail.com>
2025-08-12 13:32:59 -0700

    Time: 179 ms (42.91%) | Memory: 31.9 MB (30.24%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -10,10 +10,10 @@ class Solution:
         for i in range(len(nums)):
             if queue and queue[0] == i - k:
                 queue.popleft()
-
+            
             while queue and nums[queue[-1]] <= nums[i]:
                 queue.pop()
-
+            
             queue.append(i)
 
             if i >= k - 1:
