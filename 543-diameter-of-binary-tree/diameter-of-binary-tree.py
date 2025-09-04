9d56adf98ed12ab7f5437c9364b3ff7e2fac9bf2
Xavier <xavierelon93@gmail.com>
2025-09-04 07:52:26 -0700

    Time: 4 ms (73.03%) | Memory: 21.1 MB (6.55%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,12 +7,11 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
-            if not node:
-                return 0, 0
+            if not node: return 0, 0
 
             left_height, left_diam = dfs(node.left)
             right_height, right_diam = dfs(node.right)
- 
+
             current_height = max(left_height, right_height) + 1
             current_diam = max(left_diam, right_diam, left_height + right_height)
 
