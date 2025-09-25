5c4ce69484c66b8dc43a630acf822fcc1e46a829
Xavier <xavierelon93@gmail.com>
2025-09-25 07:25:32 -0700

    Time: 1 ms (13.66%) | Memory: 17.8 MB (37.88%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,23 +6,22 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root: return []
+        if not root:
+            return []
 
         queue = deque([root])
         res = []
 
         while queue:
             level_size = len(queue)
-
             for i in range(level_size):
                 node = queue.popleft()
-
                 if i == level_size - 1:
                     res.append(node.val)
-
+                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-        
+
         return res
