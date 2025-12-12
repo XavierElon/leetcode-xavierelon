b92c143b319a262e39927f2ef67db86e0dbb6ebb
Xavier <xavierelon93@gmail.com>
2025-12-11 16:34:29 -0800

    Time: 7 ms (90.02%) | Memory: 18 MB (47.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -9,5 +9,5 @@ class Solution:
         for cs, ct in zip(s, t):
             counts[ord(cs) - base] += 1
             counts[ord(ct) - base] -= 1
-        
+
         return all(c == 0 for c in counts)
