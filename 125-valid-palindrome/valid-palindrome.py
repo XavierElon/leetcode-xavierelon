73683ffb16a75ecabfaf8b644bbc54445ebe81de
Xavier <xavierelon93@gmail.com>
2025-12-11 17:45:04 -0800

    Time: 7 ms (82.69%) | Memory: 18.1 MB (67.94%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -7,11 +7,11 @@ class Solution:
                 l += 1
             while l < r and not s[r].isalnum():
                 r -= 1
-
+            
             if s[l].lower() != s[r].lower():
                 return False
+
             l += 1
             r -= 1
 
         return True
-            
