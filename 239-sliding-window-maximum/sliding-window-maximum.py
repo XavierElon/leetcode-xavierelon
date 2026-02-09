3a29f9029e123ef3c0c80abdf9c1f2d7c5e837de
Xavier <xavierelon93@gmail.com>
2026-02-08 19:14:58 -0800

    Time: 181 ms (59.54%) | Memory: 34.5 MB (62.61%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -4,6 +4,7 @@ class Solution:
             return []
 
         res = []
+
         queue = deque()
 
         for i in range(len(nums)):
@@ -12,7 +13,7 @@ class Solution:
 
             while queue and nums[queue[-1]] <= nums[i]:
                 queue.pop()
-            
+
             queue.append(i)
 
             if i >= k - 1:
