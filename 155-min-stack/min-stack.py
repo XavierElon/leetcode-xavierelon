b37959c23f6b8429d6cdae1b88de99719e5664c5
Xavier <xavierelon93@gmail.com>
2026-04-21 08:11:21 -0700

    Time: 4 ms (68.51%) | Memory: 22.7 MB (32.26%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -7,7 +7,7 @@ class MinStack:
         if not self.stack:
             self.stack.append((val, val))
         else:
-            self.stack.append((val, min(val, self.stack[-1][1])))
+            self.stack.append((val, min(val,self.stack[-1][1])))
 
     def pop(self) -> None:
         self.stack.pop()
