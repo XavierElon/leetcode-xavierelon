816d553e49f082aba015f0781768e7ada3310be5
Xavier <xavierelon93@gmail.com>
2026-07-21 08:59:29 -0700

    Time: 253 ms (37.62%) | Memory: 21.5 MB (71.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -10,18 +10,17 @@ class Solution:
                 if grid[r][c] == '1':
                     self.dfs(r, c, grid)
                     count += 1
-
         return count
-        
+
     def dfs(self, r, c, grid):
         grid[r][c] = '0'
-        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+        dirs = [(-1,0), (1,0), (0,-1), (0,1)]
 
         for dr, dc in dirs:
             new_r, new_c = r + dr, c + dc
 
-            if (self.is_within_bounds(new_r, new_c, grid) and grid[new_r][new_c] == '1'):
+            if (self.isWithinBounds(new_r, new_c, grid) and grid[new_r][new_c] == '1'):
                 self.dfs(new_r, new_c, grid)
 
-    def is_within_bounds(self, r, c, grid):
+    def isWithinBounds(self, r, c, grid):
         return 0 <= r and r < len(grid) and 0 <= c and c < len(grid[0])
