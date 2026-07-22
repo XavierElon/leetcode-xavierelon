638fb3a7eae63f0a29e574b0fe0268a25c1d4a2d
Xavier <xavierelon93@gmail.com>
2026-07-22 11:13:33 -0700

    Time: 63 ms (26.40%) | Memory: 29.6 MB (67.31%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,7 +3,7 @@ class Solution:
         l, r = 0, len(height) - 1
         max_water = 0
 
-        while l < r:
+        while l < r: 
             water = min(height[l], height[r]) * (r - l)
             max_water = max(max_water, water)
 
@@ -11,5 +11,5 @@ class Solution:
                 l += 1
             else:
                 r -= 1
-
+        
         return max_water
