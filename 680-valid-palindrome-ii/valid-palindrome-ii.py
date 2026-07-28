655f8e9ecdb159e31f1fc6cb418c5db91668336d
Xavier <xavierelon93@gmail.com>
2026-07-28 08:03:35 -0700

    Time: 55 ms (65.11%) | Memory: 19.5 MB (79.71%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -3,30 +3,20 @@ class Solution:
         l, r = 0, len(s) - 1
 
         while l < r:
-            while l < r and not s[l].isalpha():
-                l += 1
-            while l < r and not s[r].isalpha():
-                r -= 1
-
-            if s[l].lower() == s[r].lower():
+            if s[l] == s[r]:
                 l += 1
                 r -= 1
             else:
-                return self.check(s, l + 1, r) or self.check(s, l, r - 1)
+                return self.isPalindrome(s, l + 1, r) or self.isPalindrome(s, l, r - 1)
 
         return True
 
-    def check(self, s, l, r):
+    def isPalindrome(self, s, l, r):
         while l < r:
-            while l < r and not s[l].isalpha():
-                l += 1
-            while l < r and not s[r].isalpha():
-                r -= 1
-            
-            if s[l].lower() != s[r].lower():
+            if s[l] != s[r]:
                 return False
+            else:
+                l += 1
+                r -= 1
 
-            l += 1
-            r -= 1
-        
         return True
